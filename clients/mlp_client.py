9648fb1a02da926f1f9d6f3aa455f67552f0e87b
Andrew Rechnitzer <andrewr@math.ubc.ca>
2018-06-20 11:49:11 -0700

    mlp_client now remembers which sever etc you entered last time

--- clients/mlp_client.py
+++ clients/mlp_client.py
@@ -1,5 +1,6 @@
 import sys
-import tempfile
+import os
+import json
 
 global tempDirectory, directoryPath
 
@@ -8,6 +9,27 @@ import mlp_identifier
 
 from PyQt5.QtWidgets import QApplication, QWidget
 from uiFiles.ui_chooser import Ui_Chooser
+
+lastTime = {}
+
+def readLastTime():
+    global lastTime
+    if os.path.isfile("lastTime.json"):
+        with open('lastTime.json') as data_file:
+            lastTime = json.load(data_file)
+    else:
+        lastTime["user"] = "adr"
+        lastTime["server"] = "localhost"
+        lastTime["mport"] = "41984"
+        lastTime["wport"] = "41985"
+        lastTime["pg"] = 1
+        lastTime["v"] = 1
+
+def writeLastTime():
+    fh = open("lastTime.json", 'w')
+    fh.write(json.dumps(lastTime, indent=4, sort_keys=True))
+    fh.close()
+
 
 class Chooser(QWidget):
     def __init__(self):
@@ -18,11 +40,21 @@ class Chooser(QWidget):
 
         self.ui = Ui_Chooser()
         self.ui.setupUi(self)
+        self.setLastTime()
 
         self.ui.markButton.clicked.connect(self.runMarker)
         self.ui.identifyButton.clicked.connect(self.runIDer)
         self.ui.closeButton.clicked.connect(self.closeWindow)
         self.ui.startButton.clicked.connect(self.validate)
+
+    def setLastTime(self):
+        readLastTime()
+        self.ui.userLE.setText(lastTime["user"])
+        self.ui.serverLE.setText(lastTime["server"])
+        self.ui.mportSB.setValue(int(lastTime["mport"]))
+        self.ui.wportSB.setValue(int(lastTime["wport"]))
+        self.ui.pgSB.setValue(int(lastTime["pg"]))
+        self.ui.vSB.setValue(int(lastTime["v"]))
 
     def validate(self):
         if (not self.ui.userLE.text().isalnum()) or (not self.ui.userLE.text()):
@@ -44,7 +76,7 @@ class Chooser(QWidget):
         self.ui.serverGBox.setEnabled(False)
         self.ui.userGBox.setEnabled(False)
         self.ui.markGBox.setEnabled(False)
-        
+
         self.ui.startButton.clicked.disconnect(self.validate)
         self.ui.startButton.clicked.connect(self.closeWindow)
         self.ui.startButton.setText("Finished Task")
@@ -70,6 +102,15 @@ class Chooser(QWidget):
         self.ui.userLE.setFocus()
 
     def closeWindow(self):
+        lastTime["user"] = self.ui.userLE.text()
+        lastTime["server"] = self.ui.serverLE.text()
+        lastTime["mport"] = self.ui.mportSB.value()
+        lastTime["wport"] = self.ui.wportSB.value()
+        lastTime["pg"] = self.ui.pgSB.value()
+        lastTime["v"] = self.ui.vSB.value()
+
+        writeLastTime()
+
         self.close()
 
 app = QApplication(sys.argv)
