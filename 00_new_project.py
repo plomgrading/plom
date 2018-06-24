d32a634072f69c281d25b52a1f926841ecdf99f4
Andrew Rechnitzer <andrew@MacBook-Pro.hitronhub.home>
2018-06-23 22:03:34 -0700

    new project script now transfers pythong files + generates ssl keys/certs

--- 00_new_project.py
+++ 00_new_project.py
@@ -1,7 +1,39 @@
 import sys
 import os
+import locale
 from PyQt5.QtWidgets import QApplication, QFileDialog, QWidget
 from ui_launcher import Ui_Launcher
+
+files = ['build', 'finishing', 'imageServer', 'resources', 'scanAndGroup']
+files += ['build/01_construct_a_specification.py', 'build/cleanAll.py', 'build/02_build_tests_from_spec.py', 'build/merge_and_code_pages.py', 'build/buildTestPDFs.py', 'build/testspecification.py']
+files += [
+'scanAndGroup/03_scans_to_page_images.py', 'scanAndGroup/cleanAll.py', 'scanAndGroup/04_decode_images.py', 'scanAndGroup/extract_qr_and_orient.py', 'scanAndGroup/05_missing_pages.py', 'scanAndGroup/manualPageIdentifier.py', 'scanAndGroup/06_group_pages.py', 'scanAndGroup/testspecification.py']
+files += ['imageServer/authenticate.py', 'imageServer/mark_manager.py', 'imageServer/examviewwindow.py', 'imageServer/mark_storage.py', 'imageServer/id_storage.py', 'imageServer/testspecification.py', 'imageServer/identify_manager.py', 'imageServer/userManager.py', 'imageServer/image_server.py']
+files += [
+'finishing/07_check_completed.py', 'finishing/coverPageBuilder.py', 'finishing/08_build_cover_pages.py', 'finishing/testReassembler.py', 'finishing/09_reassemble.py', 'finishing/testspecification.py'
+]
+
+def buildTar():
+    flist = " ".join(files)
+    os.system("tar -cnf mlp.tar {}".format(flist))
+
+def unTar(projPath):
+    os.system("tar -xf mlp.tar --directory {}".format(projPath))
+
+def buildKey(projPath):
+    print("Building new ssl key/certificate for server")
+    # Command to generate the self-signed key:
+    #     openssl req -x509 -newkey rsa:2048 -keyout selfsigned.key -nodes -out selfsigned.cert -sha256 -days 1000
+    sslcmd = "openssl req -x509 -sha256 -newkey rsa:2048 -keyout {}/mlp.key -nodes -out {}/mlp-selfsigned.crt -days 1000".format(projPath, projPath)
+    sslcmd += " -subj \'/C={}/ST=./L=./CN=localhost\'".format(locale.getdefaultlocale()[0][-2:])
+    print(sslcmd)
+    os.system(sslcmd)
+
+
+def doThings(projPath):
+    buildTar()
+    unTar(projPath)
+    buildKey(projPath)
 
 class ProjectLauncher(QWidget):
     def __init__(self):
@@ -20,7 +52,8 @@ class ProjectLauncher(QWidget):
             self.ui.directoryLE.setText(dir)
 
     def createProject(self):
-        pass
+        doThings(self.ui.directoryLE.text())
+        self.close()
 
 app = QApplication(sys.argv)
 window = ProjectLauncher()
