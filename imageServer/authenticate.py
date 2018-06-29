f07f4db1bbeccc50da7d90cdc12f87aa3874cfad
Andrew Rechnitzer <andrew@MacBook-Pro.hitronhub.home>
2018-06-28 19:06:42 -0700

    image server will now reload user when told by usermanager list and add / delete as appropriate. Still need to fix server details.

--- imageServer/authenticate.py
+++ imageServer/authenticate.py
@@ -4,37 +4,41 @@ import uuid
 
 class Authority:
     def __init__(self, userList):
-        self.ctx = CryptContext( schemes=["pbkdf2_sha256", "bcrypt"], deprecated="auto" )
-        self.userList=userList
-        self.tokenList={}
+        self.ctx = CryptContext(schemes=["pbkdf2_sha256", "bcrypt"], deprecated="auto")
+        self.userList = userList
+        self.tokenList = {}
+
+    def addUser(self, user, password):
+        if user not in self.tokenList:
+            self.userList[user]=password
 
     def checkPassword(self, user, password):
-        if(user not in self.userList):
-            return(False)
-        if( self.ctx.verify(password, self.userList[user]) ):
-            return(True)
+        if user not in self.userList:
+            return False
+        if self.ctx.verify(password, self.userList[user]):
+            return True
         else:
-            return(False)
+            return False
 
     def allocateToken(self, user):
-        self.tokenList[user]=uuid.uuid4().hex
+        self.tokenList[user] = uuid.uuid4().hex
 
     def getToken(self, user):
-        return( self.tokenList[user] )
+        return self.tokenList[user]
 
-    def authoriseUser(self,user,password):
-        if(self.checkPassword(user,password)):
+    def authoriseUser(self, user, password):
+        if self.checkPassword(user, password):
             self.allocateToken(user)
-            return(True)
+            return True
         else:
-            return(False)
+            return False
 
     def detoken(self, user):
-        if(user in self.tokenList):
+        if user in self.tokenList:
             del self.tokenList[user]
 
     def validateToken(self, user, token):
-        if(user in self.tokenList):
-            if( token == self.tokenList[user] ):
-                return(True)
-        return(False)
+        if user in self.tokenList:
+            if token == self.tokenList[user]:
+                return True
+        return False
