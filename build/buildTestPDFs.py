bf168bad76cbb964a9046d826c19f2417173fc3f
andrewr <andrewr@math.ubc.ca>
2018-12-05 14:23:05 -0800

    Added some internal doc to builttest and mergecode scripts

--- build/buildTestPDFs.py
+++ build/buildTestPDFs.py
@@ -7,19 +7,23 @@ from resources.testspecification import TestSpecification
 
 
 def readExams():
+    """Read the list of exams produced from datafile"""
     global exams
     with open("../resources/examsProduced.json") as data_file:
         exams = json.load(data_file)
 
 
 def scriptBuild():
+    """Rerun mergeandcode script to rebuild all the exams
+    Build a list of commands and pipe through gnu-parallel
+    to take advantage of multiple processors
+    """
     fh = open("./commandlist.txt", "w")
     for x in exams:
         fh.write("python3 mergeAndCodePages.py {} {} {} {} \"{}\"\n".format(
             spec.Name, spec.Length, spec.Versions, x, exams[x]))
     fh.close()
     os.system("parallel --bar <commandlist.txt")
-
 
 spec = TestSpecification()
 spec.readSpec()
