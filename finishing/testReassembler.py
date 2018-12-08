a7a0b7f246b14bd622d6407cb596a428eeae741a
andrewr <andrewr@math.ubc.ca>
2018-12-07 16:03:24 -0800

    Added documentation to the 09,09alt scripts.

--- finishing/testReassembler.py
+++ finishing/testReassembler.py
@@ -1,22 +1,29 @@
+import fitz
 import os
 import sys
-import fitz
 import tempfile
-
+# takes StudentID and list of group image files as args.
+# 0th item on list is the coverpage.
+# other items are the groupimage files.
 sid = eval(sys.argv[1])
 imgl = eval(sys.argv[2])
-
+# output as test_<StudentID>.pdf
 outname = "reassembled/test_{}.pdf".format(sid)
-
+# work on a tempfile
 with tempfile.NamedTemporaryFile(suffix='.pdf') as tf:
-    tfn = tf.name
+    # use imagemagick to glob the group-images together into a pdf.
+    # first build the imagemagick command.
     cmd = "convert -quality 100"
     for X in imgl[1:]:
         cmd += " {}".format(X)
-    cmd += " {}".format(tfn)
+    cmd += " {}".format(tf.name)
+    # run the command.
     os.system(cmd)
-
-    exam = fitz.open(tfn)
+    # Now attach the coverpage to the front using fitz / pymupdf
+    exam = fitz.open(tf.name)
+    # grab the coverpage
     cover = fitz.open(imgl[0])
+    # insert the coverpage as the 0th page.
     exam.insertPDF(cover, from_page=0, to_page=0, start_at=0)
+    # save the output.
     exam.save(outname)
--- finishing/testReassembler_only_ided.py
+++ finishing/testReassembler_only_ided.py
@@ -1,18 +1,21 @@
 import os
 import sys
 import tempfile
-from shutil import copyfile
-
+import shutil
+# takes StudentID and list of group image files as args.
 sid = eval(sys.argv[1])
 imgl = eval(sys.argv[2])
-
+# output to indicate file
 outname = "reassembled_ID_but_not_marked/test_{}.pdf".format(sid)
-
+# work on a tempfile
 with tempfile.NamedTemporaryFile(suffix='.pdf') as tf:
-    tfn = tf.name
+    # use imagemagick to glob together the groupimages
+    # build the command
     cmd = "convert -quality 100"
     for X in imgl[0:]:
         cmd += " {}".format(X)
-    cmd += " {}".format(tfn)
+    cmd += " {}".format(tf.name)
+    # run the command
     os.system(cmd)
-    copyfile(tfn, outname)
+    # copy the tempfile into place.
+    shutil.copyfile(tf.name, outname)
