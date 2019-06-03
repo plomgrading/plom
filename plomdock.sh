a9a0e031c75f9a5dc03b5741a5b1e91e84be02ec
Colin B. Macdonald <cbm@m.fsf.org>
2019-06-03 10:23:18 -0700

    initial work on pulling up a docker image with plom

---
+++ plomdock.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+export BRANCH=dev
+
+#before_install:
+# TODO: on gitlab CI, we would have the appropriate commit checked out in `MLP`?
+git clone https://gitlab.math.ubc.ca/andrewr/MLP.git plom
+cd plom
+git checkout $BRANCH
+
+# TODO: use `ubuntu:latest` or `ubuntu:18.04` instead
+# TODO: mtmiller/octave has some our deps already, so faster pull
+docker pull mtmiller/octave
+docker run --name=plom0 --detach --init --env=LC_ALL=C.UTF-8 --volume=$PWD:/plom:z mtmiller/octave sleep inf
+
+# TODO: could also clone within the docker image:
+#docker exec plom0 git clone https://gitlab.math.ubc.ca/andrewr/MLP.git
+#docker exec plom0 cd MLP; git check dev
+
+#install:
+docker exec plom0 apt-get update
+docker exec plom0 apt-get --no-install-recommends --yes install  \
+    parallel zbar-tools \
+    python3-passlib python3-seaborn python3-pandas python3-pyqt5 \
+    python3-pyqt5.qtsql python3-peewee python3-pyqrcode python3-png
+# TODO: `dpkg` is whinging and trying to interactively configure timezones (how to do this unattended?)
+# something like `sudo docker exec plom0 dpkg --configure -a`
+
+# TODO: cmake dep missing in lapsolver: file upstream?
+docker exec plom0 pip3 install wsgidav easywebdav2 pymupdf \
+                               weasyprint imutils lapsolver
+
+#script:
+# TODO: something like:
+docker exec plom0 cd /plomsrc/build; python3 editMeToBuildASpec.py
+
+#after_script:
+docker stop plom0
+docker rm plom0
