4a6d65b341293cb9737abeaced59e17a1e29e927
Colin B. Macdonald <cbm@m.fsf.org>
2019-06-04 13:58:05 -0700

    docker: pull up the server stuff

--- plomdock.sh
+++ plomdock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export BRANCH=dev
+export BRANCH=master
 
 #before_install:
 # TODO: on gitlab CI, we would have the appropriate commit checked out in `MLP`?
@@ -10,7 +10,7 @@ git checkout $BRANCH
 
 # TODO: use `ubuntu:latest` or `ubuntu:18.04` instead
 # TODO: mtmiller/octave has some our deps already, so faster pull
-docker pull mtmiller/octave
+docker pull ubuntu:18.04
 docker run --name=plom0 --detach --init --env=LC_ALL=C.UTF-8 --volume=$PWD:/plom:z mtmiller/octave sleep inf
 
 # TODO: could also clone within the docker image:
@@ -24,14 +24,30 @@ docker exec plom0 env DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 docker exec plom0 apt-get --no-install-recommends --yes install  \
     parallel zbar-tools cmake \
     python3-passlib python3-seaborn python3-pandas python3-pyqt5 \
-    python3-pyqt5.qtsql python3-peewee python3-pyqrcode python3-png
+    python3-pyqt5.qtsql python3-pyqrcode python3-png \
+    python3-pip python3-setuptools imagemagick
 
-docker exec plom0 pip3 install wsgidav easywebdav2 pymupdf \
-                               weasyprint imutils lapsolver
+
+docker exec plom0 pip3 install --upgrade \
+       wsgidav easywebdav2 pymupdf weasyprint imutils \
+       lapsolver peewee cheroot
 
 #script:
-# TODO: something like:
-docker exec plom0 cd /plomsrc/build; python3 editMeToBuildASpec.py
+mkdir plom/scanAndGroup/scannedExams/
+mkdir plom/imageServer/markingComments
+mkdir plom/imageServer/markedPapers
+cp -fa ~/share/minTestServerData/resources/* plom/resources/
+cp -a ~/share/minTestServerData/*.pdf plom/scanAndGroup/scannedExams/
+docker exec plom0 bash -c "cd plom/scanAndGroup; python3 03_scans_to_page_images.py"
+docker exec plom0 bash -c "cd plom/scanAndGroup; python3 04_decode_images.py"
+docker exec plom0 bash -c "cd plom/scanAndGroup; python3 05_missing_pages.py"
+docker exec plom0 bash -c "cd plom/scanAndGroup; python3 06_group_pages.py"
+# TODO: add IDing NN later?  this replaces prediction list and classlist
+
+#
+# TODO: chmod 644 mlp.key?
+# TODO: permission error on server.log on 18k1-cbm1.math.ubc.ca
+docker exec plom0 bash -c "cd plom/imageServer; python3 image_server.py"
 
 #after_script:
 docker stop plom0
