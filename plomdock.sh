620e4ca5222e624781232bbd2559bf4717ae0aa9
Colin B. Macdonald <cbm@m.fsf.org>
2019-06-06 16:10:46 -0700

    Andrew's changes to plomdock

--- plomdock.sh
+++ plomdock.sh
@@ -3,8 +3,9 @@
 # Instructions: must run this in the directory above "plom"
 
 export BRANCH=master
-#export BASEIMG=ubuntu:18.04
 export BASEIMG=mtmiller/octave
+export MINTESTDATA=/home/andrew/tmp/dockerHack/minTestServerData
+export GID=`id -g`
 
 #before_install:
 # TODO: on gitlab CI, we would have the appropriate commit checked out in `MLP`?
@@ -14,7 +15,8 @@ git checkout $BRANCH
 cd ..
 
 docker pull ${BASEIMG}
-docker run --name=plom0 --detach --init --env=LC_ALL=C.UTF-8 --volume=$PWD/plom:/plom:z ${BASEIMG} sleep inf
+docker run -p 41984:41984 -p 41985:41985 --name=plom0 --detach --init --env=LC_ALL=C.UTF-8 --volume=$PWD/plom:/plom:z ${BASEIMG} sleep inf
+docker exec plom0 adduser -u $UID --no-create-home --disabled-password --gecos "" $USER
 
 # TODO: could also clone within the docker image:
 #docker exec plom0 git clone https://gitlab.math.ubc.ca/andrewr/MLP.git plom
@@ -28,8 +30,8 @@ docker exec plom0 apt-get --no-install-recommends --yes install  \
     parallel zbar-tools cmake \
     python3-passlib python3-seaborn python3-pandas python3-pyqt5 \
     python3-pyqt5.qtsql python3-pyqrcode python3-png \
-    python3-pip python3-setuptools imagemagick
-
+    python3-pip python3-setuptools imagemagick \
+    texlive-latex-extra dvipng
 
 docker exec plom0 pip3 install --upgrade \
        wsgidav easywebdav2 pymupdf weasyprint imutils \
@@ -39,12 +41,12 @@ docker exec plom0 pip3 install --upgrade \
 mkdir plom/scanAndGroup/scannedExams/
 mkdir plom/imageServer/markingComments
 mkdir plom/imageServer/markedPapers
-/bin/cp -fa ~/share/minTestServerData/resources/* plom/resources/
-/bin/cp -a ~/share/minTestServerData/*.pdf plom/scanAndGroup/scannedExams/
-docker exec plom0 bash -c "cd plom/scanAndGroup; python3 03_scans_to_page_images.py"
-docker exec plom0 bash -c "cd plom/scanAndGroup; python3 04_decode_images.py"
-docker exec plom0 bash -c "cd plom/scanAndGroup; python3 05_missing_pages.py"
-docker exec plom0 bash -c "cd plom/scanAndGroup; python3 06_group_pages.py"
+/bin/cp -fa $MINTESTDATA/resources/* plom/resources/
+/bin/cp -a $MINTESTDATA/*.pdf plom/scanAndGroup/scannedExams/
+docker exec --user $USER plom0 bash -c "cd plom/scanAndGroup; python3 03_scans_to_page_images.py"
+docker exec --user $USER plom0 bash -c "cd plom/scanAndGroup; python3 04_decode_images.py"
+docker exec --user $USER plom0 bash -c "cd plom/scanAndGroup; python3 05_missing_pages.py"
+docker exec --user $USER plom0 bash -c "cd plom/scanAndGroup; python3 06_group_pages.py"
 # TODO: add IDing NN later?  this replaces prediction list and classlist
 
 # Server stuff
@@ -52,8 +54,9 @@ IP=`docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"
 sed -i "s/127.0.0.1/${IP}/" plom/resources/serverDetails.json
 # TODO: chmod 644 mlp.key?
 # TODO: permission error on server.log on 18k1-cbm1.math.ubc.ca
-docker exec plom0 bash -c "cd plom/imageServer; python3 image_server.py"
+echo "Server IP is ${IP}"
+docker exec --user $USER plom0 bash -c "cd plom/imageServer; python3 image_server.py"
 
 #after_script:
-docker stop plom0
-docker rm plom0
+# docker stop plom0
+# docker rm plom0
