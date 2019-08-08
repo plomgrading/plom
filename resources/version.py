233fae6496a88e11f7f7b0b8277d46c128f099ee
andrewr <andrewr@math.ubc.ca>
2019-08-08 13:38:55 -0700

    Forgot to upload the version.py file. <sigh> sorry.

---
+++ resources/version.py
@@ -0,0 +1,7 @@
+__author__ = "Andrew Rechnitzer"
+__copyright__ = "Copyright (C) 2018-2019 Andrew Rechnitzer"
+__credits__ = ["Andrew Rechnitzer", "Colin Macdonald"]
+__license__ = "AGPLv3"
+
+
+_Release_Version_ = "0.2 'Pickle'"
