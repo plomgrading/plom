8b98d90d661c2201d37527e655f3c709225d0ace
Colin B. Macdonald <cbm@m.fsf.org>
2019-08-08 23:25:42 -0700

    Removing this comment, which I agree with
    It will become stale, and we're not going to retrofit API versions
    into older versions.

--- resources/version.py
+++ resources/version.py
@@ -5,7 +5,4 @@ __license__ = "AGPLv3"
 
 
 Release_Version = "0.1.0+"
-# I'm going to assume the very initial pre 0.1 was "0"
-# and that 0.1 was "1"... and that we are now somewhere
-# around 2
 PLOM_API_Version = "2"
