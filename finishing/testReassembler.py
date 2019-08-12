0a2d14ecf3338ff9194838e8b7cbbb40b6017edf
Colin B. Macdonald <cbm@m.fsf.org>
2019-08-12 11:17:53 -0700

    Reassemble each page as sheet of letter paper
    We make each page landscape or portrait by specifying the "page"
    size as "792 x 612 pts" or "612 x 792 pts".  This is different from
    rotating the image because we want the landscape pages to appear
    sideways in PDF viewers, but still print properly.
    
    Fixes #201.
    
    Imagemagick is a bit finicky about all this.  Its not clear to me
    how to center the image on the page without messing around with
    density or resize.  For now, leave a TODO, and let it attach to
    one edge: good enough for now.

--- finishing/testReassembler.py
+++ finishing/testReassembler.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 __author__ = "Andrew Rechnitzer"
 __copyright__ = "Copyright (C) 2018-2019 Andrew Rechnitzer"
 __credits__ = ["Andrew Rechnitzer", "Colin Macdonald", "Elvis Cai"]
@@ -7,29 +9,53 @@ import fitz
 import os
 import sys
 import tempfile
+import subprocess
 
-# takes StudentID and list of group image files as args.
-# 0th item on list is the coverpage.
-# other items are the groupimage files.
-sid = eval(sys.argv[1])
-imgl = eval(sys.argv[2])
-# output as test_<StudentID>.pdf
-outname = "reassembled/test_{}.pdf".format(sid)
-# work on a tempfile
-with tempfile.NamedTemporaryFile(suffix=".pdf") as tf:
-    # use imagemagick to glob the group-images together into a pdf.
-    # first build the imagemagick command.
-    cmd = "convert -quality 100"
-    for X in imgl[1:]:
-        cmd += " {}".format(X)
-    cmd += " {}".format(tf.name)
-    # run the command.
-    os.system(cmd)
-    # Now attach the coverpage to the front using fitz / pymupdf
-    exam = fitz.open(tf.name)
-    # grab the coverpage
-    cover = fitz.open(imgl[0])
-    # insert the coverpage as the 0th page.
-    exam.insertPDF(cover, from_page=0, to_page=0, start_at=0)
-    # save the output.
-    exam.save(outname)
+# hardcoded for letter, https://gitlab.math.ubc.ca/andrewr/MLP/issues/276
+sizeportrait = "612x792"
+sizelandscape = "792x612"
+
+
+def iswider(f):
+    """True if image is wider than it is high"""
+    ratio = subprocess.check_output(["identify", "-format",
+                                     "%[fx:w/h]", f]).decode().rstrip()
+    return float(ratio) > 1
+
+
+if __name__ == '__main__':
+    # takes StudentID and list of group image files as args.
+    # 0th item on list is the coverpage.
+    # other items are the groupimage files.
+    sid = eval(sys.argv[1])
+    imgl = eval(sys.argv[2])
+    coverfname, imgl = imgl[0], imgl[1:]
+    # output as test_<StudentID>.pdf
+    outname = "reassembled/test_{}.pdf".format(sid)
+
+
+    # use imagemagick to convert each group-image into a temporary pdf.
+    pdfpages = [tempfile.NamedTemporaryFile(suffix=".pdf") for x in imgl]
+    for img, TF in zip(imgl, pdfpages):
+        cmd = ["convert", img, "-quality", "100"]
+        # TODO: want to center the image but then it doesn't fit page
+        #cmd += ["-gravity", "center", "-background", "white"]
+        # Rotate page not the image: we want landscape on screen
+        if iswider(img):
+            cmd += ["-page", sizelandscape]
+        else:
+            cmd += ["-page", sizeportrait]
+        cmd += ["pdf:{}".format(TF.name)]
+        subprocess.check_call(cmd)
+
+    # open the coverpage and append the temporary pdf pages
+    exam = fitz.open(coverfname)
+    for pg in pdfpages:
+        exam.insertPDF(fitz.open(pg.name))
+
+    # clean up temp files
+    for pg in pdfpages:
+        pg.close()
+
+    with tempfile.NamedTemporaryFile(suffix=".pdf") as tf:
+        exam.save(outname)
