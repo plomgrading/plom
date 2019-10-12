f7f1b1a07327d61e0f4072c905a897e62af249e6
andrewr <andrewr@math.ubc.ca>
2019-10-12 11:22:41 -0700

    Towards a "build a canned set of users" script.

---
+++ imageServer/buildCannedUsers.py
@@ -0,0 +1,28 @@
+__author__ = "Andrew Rechnitzer"
+__copyright__ = "Copyright (C) 2019 Andrew Rechnitzer"
+__credits__ = ["Andrew Rechnitzer", "Colin Macdonald"]
+__license__ = "AGPLv3"
+
+import json
+import os
+import random
+import subprocess
+import sys
+
+
+# Stuff for hashing and verifying passwords
+from passlib.hash import pbkdf2_sha256
+from passlib.context import CryptContext
+
+# Fire up password stuff
+mlpctx = CryptContext(schemes=["pbkdf2_sha256", "bcrypt"], deprecated="auto")
+
+# get canned user list
+lst = eval(
+    subprocess.run(
+        ["python3", "../resources/aliceBob.py"], stdout=subprocess.PIPE
+    ).stdout.decode("utf-8")
+)
+
+for (n, p) in lst:
+    print(n, p, mlpctx.encrypt(p))
