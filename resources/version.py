cc99139a173b0569829336a50bc25cbde5587a5b
Colin B. Macdonald <cbm@m.fsf.org>
2019-11-11 17:39:26 -0800

    post release version bump

--- resources/version.py
+++ resources/version.py
@@ -4,5 +4,5 @@ __credits__ = ["Andrew Rechnitzer", "Colin Macdonald"]
 __license__ = "AGPLv3"
 
 
-__version__ = "0.2.1"
+__version__ = "0.2.1+"
 Plom_API_Version = "4"  # updated for background upload/download
