0c2dbc50137ef8e6dd57d7d11eac67c100043c14
andrewr <andrewr@math.ubc.ca>
2019-12-01 15:26:01 -0800

    Started renaming scripts in build.

---
+++ build/001_startHere.py
@@ -0,0 +1,8 @@
+print("To start the build process")
+print(
+    '1. Find your source PDFs and copy them to the "sourceVersions" directory as version1.pdf, version2.pdf, etc'
+)
+print(
+    '2. Copy the file "template_testSpec.toml" to "testSpec.toml" and edit using your favourite text editor.'
+)
+print('3. Run the "002_verifySpec.py" script')
