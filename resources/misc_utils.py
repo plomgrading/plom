73c00e985120295814f88e040cee27208cd47209
Colin B. Macdonald <cbm@m.fsf.org>
2019-12-03 19:46:08 -0800

    new implementation, fixing short runs

--- resources/misc_utils.py
+++ resources/misc_utils.py
@@ -7,26 +7,50 @@ __copyright__ = "Copyright (C) 2019 Colin B. Macdonald, Omer Angel"
 __license__ = "AGPL-3.0-or-later"
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+import math
+
+
 def format_int_list_with_runs(L):
     """Replace runs in a list with a range notation"""
-    L = set(map(int, L))
-    M = max(L)
-    start = None
-    r = ""
-    for i in range(-1, M + 2):
-        if start is None and i in L:  # run start
-            start = i
-            r = r + str(i)
-        elif start is not None and i not in L:  # run ended
-            if i > start + 1:
-                r = r + "–" + str(i - 1)
-            if i < M:  # not last run
-                r = r + ", "
-            start = None
-    return r
+    L = _find_runs(L)
+    L = _flatten_2len_runs(L)
+    L = ["{}–{}".format(l[0], l[-1]) if isinstance(l, list) else str(l) for l in L]
+    return ", ".join(L)
 
 
+def _find_runs(S):
+    S = [int(x) for x in S]
+    S.sort()
+    L = []
+    prev = -math.inf
+    for x in S:
+        if x - prev == 1:
+            run.append(x)
+        else:
+            run = [x]
+            L.append(run)
+        prev = x
+    return L
+
+
+def _flatten_2len_runs(L):
+    L2 = []
+    for l in L:
+        if len(l) < 3:
+            L2.extend(l)
+        else:
+            L2.append(l)
+    return L2
+
+
+# TODO: unit tests
 def test1():
     L = ['1', '2', '3', '4', '7', '10', '11', '12', '13', '14', '64']
     out = "1–4, 7, 10–14, 64"
+    assert format_int_list_with_runs(L) == out
+
+
+def test_shortruns():
+    L = ['1', '2', '4', '5', '6', '7', '9', '10', '12', '78', '79', '80']
+    out = "1, 2, 4–7, 9, 10, 12, 78–80"
     assert format_int_list_with_runs(L) == out
