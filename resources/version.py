a843b4a8e5b27377e22c62a6bbacff7f12fd86f3
Andrew <andrewr@math.ubc.ca>
2020-01-19 22:06:46 +1100

    Version API updating thingy

--- resources/version.py
+++ resources/version.py
@@ -4,5 +4,5 @@ __credits__ = ["Andrew Rechnitzer", "Colin Macdonald"]
 __license__ = "AGPLv3"
 
 
-__version__ = "0.2.2+"
-Plom_API_Version = "5"  # updated for restified API for get-next-task.
+__version__ = "0.4"
+Plom_API_Version = "6"  # updated for restified API for get-next-task.
