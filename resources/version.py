ce4edbe89cf9d82faec029dbaea844c56d3c844c
Colin B. Macdonald <cbm@m.fsf.org>
2020-01-28 11:58:11 +0000

    version bump for rc3

--- resources/version.py
+++ resources/version.py
@@ -4,5 +4,5 @@ __credits__ = ["Andrew Rechnitzer", "Colin Macdonald"]
 __license__ = "AGPLv3"
 
 
-__version__ = "0.3.0rc2+"
+__version__ = "0.3.0rc3"
 Plom_API_Version = "5"  # updated for restified API for get-next-task.
