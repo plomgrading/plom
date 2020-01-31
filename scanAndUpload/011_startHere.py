e9dfcce3113e85630d1ab841db6df102e51c1a68
Colin B. Macdonald <cbm@m.fsf.org>
2020-01-31 22:09:49 +0000

    update copyright

--- scanAndUpload/011_startHere.py
+++ scanAndUpload/011_startHere.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Andrew Rechnitzer"
-__copyright__ = "Copyright (C) 2019 Andrew Rechnitzer and Colin Macdonald"
+__copyright__ = "Copyright (C) 2019-2020 Andrew Rechnitzer and Colin Macdonald"
 __credits__ = ["Andrew Rechnitzer", "Colin Macdonald"]
 __license__ = "AGPL-3.0-or-later"
 # SPDX-License-Identifier: AGPL-3.0-or-later
