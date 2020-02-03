221c6941226f4d1e9585c842fbc6ef84b183416e
Colin B. Macdonald <cbm@m.fsf.org>
2020-02-03 09:46:38 +0000

    minor update to clean script

--- scanAndUpload/cleanAll.py
+++ scanAndUpload/cleanAll.py
@@ -2,5 +2,6 @@ import os
 
 # TODO: this script is handy for dev work but maybe we shouldn't ship it...
 os.system("rm -rf collidingPages decodedPages discardedPages sentPages unknownPages pageImages")
+os.system("rm -rf scannedExams/png")
 # TODO: bit scary for a production system
 #os.system("rm -rf archivedPDFs")
