e81d447a560509410a493f6d16e8f4a2be768832
Colin B. Macdonald <cbm@m.fsf.org>
2020-02-11 10:54:09 +0000

    routes: decorator to do token-based auth, logging and field validation

--- newServer/plomServer/routesID.py
+++ newServer/plomServer/routesID.py
@@ -8,28 +8,66 @@ def validFields(d, fields):
     return set(d.keys()) == set(fields)
 
 
+# TODO: try to work the @routes decorator in too
+def tokenauth(origf=None, *, fields=[]):
+    """Decorator for authentication by token, logging and field validation.
+
+    Used as `@tokenauth`, this deals with authenication and logging so
+    your function doesn't have too.  This is essentially a way to
+    avoid copy-pasting lots of boilerplate code.
+
+    Example
+    -------
+    ```
+    @tokenauth
+    def foo(self, data):
+        return ...
+    ```
+    Here `data` is the result of `requestions.json()`.
+
+    You can instead use `@tokenauth(fields=<list>)` to pass any fields
+    that must be present in the request.
+
+    Implemenation is complicated (because `fields` is optional)[1].
+
+    References
+    ----------
+    [1] https://stackoverflow.com/questions/3888158/making-decorators-with-optional-arguments
+    """
+
+    def _decorate(f):
+        # @functools.wrap(f)
+        async def wrapped(zelf, request):
+            print("INFO: {}: {} {}".format(f.__name__, request.method, request.rel_url))
+            data = await request.json()
+            fields.extend(["user", "token"])
+            print("DEBUG: validating fields {}".format(fields))
+            if not validFields(data, fields):
+                return web.Response(status=400)
+            if not zelf.server.validate(data["user"], data["token"]):
+                return web.Response(status=401)
+            print('DEBUG: authenticated "{}" via token'.format(data["user"]))
+            return f(zelf, data)
+
+        return wrapped
+
+    if origf:
+        return _decorate(origf)
+    return _decorate
+
+
 class IDHandler:
     def __init__(self, plomServer):
         self.server = plomServer
 
     # @routes.get("/ID/progress")
-    async def IDprogressCount(self, request):
-        data = await request.json()
-        if not validFields(data, ["user", "token"]):
-            return web.Response(status=400)
-        if not self.server.validate(data["user"], data["token"]):
-            return web.Response(status=401)
-
+    @tokenauth
+    def IDprogressCount(self, data):
         return web.json_response(self.server.IDprogressCount(), status=200)
 
     # @routes.get("/ID/classlist")
-    async def IDgetClasslist(self, request):
-        data = await request.json()
-        if not validFields(data, ["user", "token"]):
-            return web.Response(status=400)
-        if not self.server.validate(data["user"], data["token"]):
-            return web.Response(status=401)
-
+    @tokenauth
+    def IDgetClasslist(self, data):
         if os.path.isfile("../resources/classlist.csv"):
             return web.FileResponse("../resources/classlist.csv", status=200)
         else:
@@ -114,13 +152,8 @@ class IDHandler:
             return web.Response(status=204)  # that task already taken.
 
     # @routes.put("/ID/tasks/{task}")
-    async def IDreturnIDdTask(self, request):
-        data = await request.json()
-        if not validFields(data, ["user", "token", "sid", "sname"]):
-            return web.Response(status=400)
-        if not self.server.validate(data["user"], data["token"]):
-            return web.Response(status=401)
-
+    @tokenauth(fields=["sid", "sname"])
+    def IDreturnIDdTask(self, data):
         testNumber = request.match_info["task"]
         rmsg = self.server.IDreturnIDdTask(
             data["user"], testNumber, data["sid"], data["sname"]
