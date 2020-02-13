a3e22e80ebf733606189c33e1150a0ab0e72c234
Colin B. Macdonald <cbm@m.fsf.org>
2020-02-13 01:31:52 +0000

    typo

--- finishing/021_startHere.py
+++ finishing/021_startHere.py
@@ -7,7 +7,7 @@ __credits__ = ["Andrew Rechnitzer", "Colin Macdonald"]
 __license__ = "AGPL-3.0-or-later"
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-print("To start the scan process")
+print('To start the "finishing" process')
 print(
     "0. Wait until most of the marking and identifying is done before running any finishing scripts - otherwise you won't achieve much running things the scripts in this directory."
 )
