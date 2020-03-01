1728fe8b296c382d3591931df51266291e474cca
Colin B. Macdonald <cbm@m.fsf.org>
2020-03-01 22:35:57 +0000

    improve module text

--- plom/rules.py
+++ plom/rules.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 
-"""Utils Misc utilities"""
+"""Utils concerning rules about data, like valid student numbers."""
 
 __author__ = "Colin B. Macdonald"
 __copyright__ = "Copyright (C) 2020 Colin B. Macdonald"
 __license__ = "AGPL-3.0-or-later"
 # SPDX-License-Identifier: AGPL-3.0-or-later
-
-import sys
-import math
 
 def isValidUBCStudentNumber(n):
     """Is this a valid student number for UBC?"""
