3b7d0add945be51da2debf7b5f0eaed739277ca5
Colin B. Macdonald <cbm@m.fsf.org>
2020-03-10 21:52:22 +0000

    replace remaining printLog with log.*

--- plom/logit.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-def printLog(who, msg):
-    print(
-        "[{} | {}] {}".format(datetime.now().strftime("%y:%m:%d-%H:%M:%S"), who, msg),
-        flush=True,
-    )
