f6bfa0d94e2d41193dca11aef13bf30bb77aa1a9
Colin B. Macdonald <cbm@m.fsf.org>
2020-03-12 18:57:30 +0000

    re-enable parallel paper builds
    We disabled this awhile back to get plom module working.  Now its back.
    Fixes #646.  Note the nicest code, but I didn't have time to make it
    better.  I left a todo, pending better understanding of how to get
    `spec` into the parallel function without copying it many times (global
    variable maybe?).
    
    I got the following timings:
    
    before:
    ```
    real    0m9.195s
    user    0m6.864s
    sys     0m1.475s
    ```
    
    after:
    ```
    real    0m3.896s
    user    0m11.858s
    sys     0m1.897s
    ```

--- plom/produce/buildBlankPDF.py
+++ plom/produce/buildBlankPDF.py
@@ -8,25 +8,39 @@ __license__ = "AGPL-3.0-or-later"
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import os
+from multiprocessing import Pool
+from tqdm import tqdm
 
 from plom.db.examDB import PlomDB
 from .mergeAndCodePages import makePDF
 
 
+def _f(x):
+    # TODO: unfortunate to copy spec here many times, but otherwise we get
+    # AttributeError: Can't pickle local object 'buildAllPapers.<locals>.f'
+    t, pv, spec = x
+    makePDF(
+        spec["name"],
+        spec["publicCode"],
+        spec["numberOfPages"],
+        spec["numberOfVersions"],
+        t,
+        pv,
+    )
+
 def buildAllPapers(spec, dbFilename):
     examDB = PlomDB(dbFilename)
-    # TODO: slow serial loop, awaiting Pool ||ism
+    stuff = []
     for t in range(1, spec["numberToProduce"] + 1):
         pv = examDB.getPageVersions(t)
-        makePDF(
-            spec["name"],
-            spec["publicCode"],
-            spec["numberOfPages"],
-            spec["numberOfVersions"],
-            t,
-            pv,
-        )
+        stuff.append((t, pv, spec))
 
+    # Same as:
+    # for x in stuff:
+    #     _f(x)
+    N = len(stuff)
+    with Pool() as p:
+        r = list(tqdm(p.imap_unordered(_f, stuff), total=N))
 
 def confirmProcessed(spec, dbFilename):
     examDB = PlomDB(dbFilename)
