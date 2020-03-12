056a0b563c68eb5cf0bcfaa2947593c3062288b1
Colin B. Macdonald <cbm@m.fsf.org>
2020-03-12 15:55:26 +0000

    update plom module copyright and other boilerplate

--- plom/__init__.py
+++ plom/__init__.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
+
 """
 Plom is PaperLess Open Marking.  TODO: Insert longer blurb
 """
+
+__copyright__ = "Copyright (C) 2020 Andrew Rechnitzer and Colin B. Macdonald"
+__credits__ = "The Plom Project Developers"
+__license__ = "AGPL-3.0-or-later"
+# SPDX-License-Identifier: AGPL-3.0-or-later
 
 from .specParser import SpecVerifier, SpecParser
 from .version import __version__, Plom_API_Version
