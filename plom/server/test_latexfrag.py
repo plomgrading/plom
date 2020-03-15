44d4c2b8e7bbc975da5c87ec8aeead87f2b6f3c7
Colin B. Macdonald <cbm@m.fsf.org>
2020-03-15 23:43:32 +0000

    test frag against saved image

--- plom/server/test_latexfrag.py
+++ plom/server/test_latexfrag.py
@@ -1,10 +1,13 @@
 import tempfile
+import subprocess
 from pytest import raises
 
 from plom.server.latex2png import processFragment
+
 # TODO: this too: pageNotSubmitted
 
-f = tempfile.NamedTemporaryFile(delete=False, suffix='.png').name
+f = tempfile.NamedTemporaryFile(delete=False, suffix=".png").name
+
 
 def test_frag_latex():
     frag = r"\( \mathbb{Z} / \mathbb{Q} \) The cat sat on the mat and verified \LaTeX\ works for Plom."
@@ -16,4 +19,27 @@ def test_frag_broken_tex():
     assert not processFragment(frag, f)
 
 
-#TODO: plus an imagemagick compare test that it produces what is expected
+def test_frag_image_as_expected():
+    # TODO: target image in resource_pkg or encode64 string?
+    g = "plom/server/target_Q_latex_plom.png"
+    frag = r"$\mathbb{Q}$ \LaTeX\ Plom"
+    assert processFragment(frag, f)
+    r = subprocess.run(
+        ["compare", "-metric", "rmse", f, g, "diffimage"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    # output is "float (float)"
+    s = r.stdout.decode().split(" ")[1].strip("()")
+    assert float(s) < 0.2
+
+    frag = r"$f = \frac{x}{y}$ and lots and lots more, very different."
+    assert processFragment(frag, f)
+    r = subprocess.run(
+        ["compare", "-metric", "rmse", f, g, "diffimage"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    # output is "float (float)"
+    s = r.stdout.decode().split(" ")[1].strip("()")
+    assert float(s) > 0.25
