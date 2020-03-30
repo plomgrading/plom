775000c92dbd68f1508c2f01ee596728845f1777
Colin B. Macdonald <cbm@m.fsf.org>
2020-03-30 00:24:37 -0700

    improve __init__.py for various modules

---
+++ plom/client/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Plom client and supporting functions."""
+
+__copyright__ = "Copyright (C) 2020 Andrew Rechnitzer and Colin B. Macdonald"
+__credits__ = "The Plom Project Developers"
+__license__ = "AGPL-3.0-or-later"
+# SPDX-License-Identifier: AGPL-3.0-or-later
