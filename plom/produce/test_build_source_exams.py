0f45a3e2f3ec13c3c69b1c72137e81fb84ed26e8
Colin B. Macdonald <cbm@m.fsf.org>
2020-03-31 13:46:08 -0700

    use tmpdir instead of tmp_path
    Maybe tmp_path is too new for ubuntu 18.04?

--- plom/produce/test_build_source_exams.py
+++ plom/produce/test_build_source_exams.py
@@ -2,9 +2,9 @@ import os
 from .demotools import buildDemoSourceFiles
 
 
-def test_latex_demofiles(tmp_path):
+def test_latex_demofiles(tmpdir):
     cdir = os.getcwd()
-    os.chdir(tmp_path)
+    os.chdir(tmpdir)
     assert buildDemoSourceFiles()
     assert set(os.listdir("sourceVersions")) == set(("version1.pdf", "version2.pdf"))
     os.chdir(cdir)
