0f8eb03641985ac05610c404929c35188c520700
Colin B. Macdonald <cbm@m.fsf.org>
2020-04-03 09:50:57 -0700

    scripts missing docstrings
    Fixes #732.

--- plom/scripts/server.py
+++ plom/scripts/server.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""Command line tools to start Plom servers."""
+
+__copyright__ = "Copyright (C) 2020 Andrew Rechnitzer and Colin B. Macdonald"
+__credits__ = "The Plom Project Developers"
+__license__ = "AGPL-3.0-or-later"
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 import argparse
 import locale
 import os
