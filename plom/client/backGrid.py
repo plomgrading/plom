2e07e8b450b3c8bcd2c7408c221df99f25f88687
Andrew <andrewr@math.ubc.ca>
2020-04-28 10:16:04 -0700

    Better sizing of svg

--- plom/client/backGrid.py
+++ plom/client/backGrid.py
@@ -9,7 +9,7 @@ __license__ = "AGPL-3.0-or-later"
 
 import os
 import sys
-from PyQt5.QtGui import QBrush, QImage, QPixmap
+from PyQt5.QtGui import QBrush, QPixmap
 
 
 class BackGrid(QBrush):
@@ -24,6 +24,6 @@ class BackGrid(QBrush):
         if username is not None and any(
             x in username.lower() for x in ["omer", "remo", "legna", "angel"]
         ):
-            self.setTextureImage(QImage(os.path.join(base_path, "backGrid2.png")))
+            self.setTexture(QPixmap(os.path.join(base_path, "backGrid2.png")))
         else:
-            self.setTextureImage(QImage(os.path.join(base_path, "backGrid1.svg")))
+            self.setTexture(QPixmap(os.path.join(base_path, "backGrid1.svg")))
