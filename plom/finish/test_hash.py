12581da1d25ce4f4932b448e5e846352f9009376
Dryden Wiebe <dryden.wiebe@live.com>
2020-06-04 10:03:29 -0700

    TESTS

--- plom/finish/test_hash.py
+++ plom/finish/test_hash.py
@@ -1,6 +1,12 @@
 from .utils import my_hash
 
+
 def test_hash():
     assert my_hash("12345678", salt="salt") == "351525727036"
     assert my_hash("12345678", salt="salty") == "782385405730"
     assert my_hash("12345679", salt="salt") == "909470548567"
+    print("\n\n\nTESTS RUNNING\n\n\n")
+    print("\n\n\nTESTS RUNNING\n\n\n")
+    print("\n\n\nTESTS RUNNING\n\n\n")
+    print("\n\n\nTESTS RUNNING\n\n\n")
+    print("\n\n\nTESTS RUNNING\n\n\n")
