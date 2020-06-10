ff49b01887667daf7d8ee2d7f194d89c911ebf2b
Victoria <vschuster@alumni.shawnigan.ca>
2020-06-10 11:42:34 -0700

    fixedd zoom problemo

--- plom/client/pagescene.py
+++ plom/client/pagescene.py
@@ -543,7 +543,7 @@ class PageScene(QGraphicsScene):
         """Change cursor back to open-hand, and update the current stored view rectangle."""
         self.views()[0].setCursor(Qt.OpenHandCursor)
         super(PageScene, self).mouseReleaseEvent(event)
-        self.views()[0].zoomNull()
+        self.views()[0].setZoomSelector()
 
     def mousePressImage(self, event):
         """
@@ -1012,7 +1012,7 @@ class PageScene(QGraphicsScene):
             # sets the view rectangle and updates zoom-dropdown.
             self.views()[0].scale(0.8, 0.8)
             self.views()[0].centerOn(event.scenePos())
-            self.views()[0].zoomNull(True)
+            self.views()[0].setZoomSelector(True)
             self.zoomFlag = 0
             return
         else:
@@ -1064,7 +1064,7 @@ class PageScene(QGraphicsScene):
             self.views()[0].fitInView(self.zoomBoxItem, Qt.KeepAspectRatio)
 
         # sets the view rectangle and updates zoom-dropdown.
-        self.views()[0].zoomNull(True)
+        self.views()[0].setZoomSelector(True)
         # remove the box and put flag back.
         self.removeItem(self.zoomBoxItem)
         self.zoomFlag = 0
