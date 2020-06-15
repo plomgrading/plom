77823931b0249f3fd6f209a9f644d0514bf5fee7
Dryden Wiebe <dryden.wiebe@live.com>
2020-06-15 13:25:47 -0700

    Update

--- plom/server/IDReader/test_IDreader.py
+++ plom/server/IDReader/test_IDreader.py
@@ -1,4 +1,4 @@
-from .idReader import is_model_absent, log_likelihood, download_model
+from .idReader import is_model_absent, log_likelihood, download_or_train_model
 
 
 def test_is_model_absent():
@@ -20,7 +20,7 @@ def test_log_likelihood():
     assert log_likelihood(student_ids, probabilities) == 5.545177444479562
 
 
-def download_or_train_model():
+def test_download_or_train_model():
     assert download_or_train_model() == True
     assert False
     assert is_model_absent() == False
