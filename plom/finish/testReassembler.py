df4f2a4630b5ae320643dcdbfda46ad9ed6a4968
Dryden Wiebe <dryden.wiebe@live.com>
2020-06-17 10:34:56 -0700

    Fixes

--- plom/finish/testReassembler.py
+++ plom/finish/testReassembler.py
@@ -20,28 +20,28 @@ papersize_landscape = (792, 612)
 margin = 10
 
 
-def is_wider(f):
+def is_wider(f_name):
     """True if image is wider than it is high.
 
     Args:
-        f (str): The name of the file we are checking.
+        f_name (str): The name of the file we are checking.
 
     Returns:
         boolean : True if the image is wider than it is tall, False otherwise.
     """
     # TODO: shell likely SLOW for this task...?
     ratio = (
-        subprocess.check_output(["identify", "-format", "%[fx:w/h]", f])
+        subprocess.check_output(["identify", "-format", "%[fx:w/h]", f_name])
         .decode()
         .rstrip()
     )
     return float(ratio) > 1
 
 
-def reassemble(outname, shortName, sid, coverfname, imglist):
+def reassemble(out_name, short_name, sid, cover_fname, img_list):
     """Reassemble a pdf from the cover and question images.
 
-    Leave coverfname as None to omit it (e.g., when totalling).
+    Leave cover_fname as None to omit it (e.g., when totalling).
 
     Return True if successful or False if the pdf file already exists.
     Note: no attempt is made to check if its correct; merely that it
@@ -49,15 +49,25 @@ def reassemble(outname, shortName, sid, coverfname, imglist):
 
     [1] https://gitlab.math.ubc.ca/andrewr/MLP/issues/392
 
+    Args:
+        out_name (str): name of the file we are assembling.
+        short_name (str): name of the file without the whole directory. 
+        sid (str): the student id.
+        cover_fname (str): the name of the file of the coverpage of this test.
+        img_list (str): the groupimage files
+
+    Returns:
+        bool: [description]
     """
-    if os.path.isfile(outname):
+
+    if os.path.isfile(out_name):
         return False
 
     exam = fitz.open()
-    if coverfname:
-        exam.insertPDF(fitz.open(coverfname))
+    if cover_fname:
+        exam.insertPDF(fitz.open(cover_fname))
 
-    for img in imglist:
+    for img in img_list:
         # Rotate page not the image: we want landscape on screen
         if is_wider(img):
             w, h = papersize_landscape
@@ -69,24 +79,24 @@ def reassemble(outname, shortName, sid, coverfname, imglist):
 
     exam.setMetadata(
         {
-            "title": "{} {}".format(shortName, sid),
+            "title": "{} {}".format(short_name, sid),
             "producer": "Plom {}".format(__version__),
         }
     )
 
     with tempfile.NamedTemporaryFile(suffix=".pdf") as tf:
-        exam.save(outname, deflate=True)
+        exam.save(out_name, deflate=True)
 
 
 if __name__ == "__main__":
-    shortName = sys.argv[1]
+    short_name = sys.argv[1]
     sid = sys.argv[2]
     outdir = sys.argv[3]
-    coverfname = sys.argv[4]
+    cover_fname = sys.argv[4]
     # the groupimage files
     imglist = eval(sys.argv[5])
 
     # note we know the shortname is alphanumeric with no strings
     # so this is safe.
-    outname = os.path.join(outdir, "{}_{}.pdf".format(shortName, sid))
-    reassemble(outname, shortName, sid, coverfname, imglist)
+    outname = os.path.join(outdir, "{}_{}.pdf".format(short_name, sid))
+    reassemble(outname, short_name, sid, cover_fname, img_list)
