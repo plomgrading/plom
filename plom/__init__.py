a3e4a29cb6850eaddf9f8e8dac5d170e0cd8d90d
Colin B. Macdonald <cbm@m.fsf.org>
2020-07-17 14:55:21 -0700

    bump api version

--- plom/__init__.py
+++ plom/__init__.py
@@ -13,7 +13,7 @@ specdir = "specAndDatabase"
 from .specParser import SpecVerifier, SpecParser
 from .version import __version__
 
-Plom_API_Version = "16"  # updated for classlist-via-api hackery
+Plom_API_Version = "17"  # updated for bundles
 Default_Port = 41984
 
 # Image types we expect the client to be able to handle, in lowercase
