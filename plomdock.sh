a4df96d090ed0ab0ede15ad56c47b47b070613c6
Colin B. Macdonald <cbm@m.fsf.org>
2020-07-30 20:21:35 -0700

    don't need sudo for docker

--- plomdock.sh
+++ plomdock.sh
@@ -4,7 +4,7 @@
 # ------------
 #
 # First, build plomdockerbuild by running:
-#   `sudo docker build . --tag plomship`
+#   `docker build . --tag plomship`
 # in whatever directory has the `Dockerfile` in it.  For example, inside your
 # git clone and in whatever branch you want; change --tag name if you want.
 #
@@ -21,17 +21,14 @@ export PD=plom0
 export UID=`id -u`
 
 # Take it down
-#rm -rf plom
-#sudo docker stop $PD
-#sudo docker rm $PD
+ #rm -rf plom
+#docker stop $PD
+#docker rm $PD
 
 mkdir -p plom
 
 # docker pull ${BASEIMG}
-sudo docker run -p 41984:41984 --name=$PD --detach --init --env=LC_ALL=C.UTF-8 --volume=$PWD/plom:/plom:z ${BASEIMG} sleep inf
-sudo docker exec $PD adduser -u $UID --no-create-home --disabled-password --gecos "" $USER
+docker run -p 41984:41984 --name=$PD --detach --init --env=LC_ALL=C.UTF-8 --volume=$PWD/plom:/plom:z ${BASEIMG} sleep inf
+docker exec $PD adduser -u $UID --no-create-home --disabled-password --gecos "" $USER
 
-IP=`sudo docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" $PD`
-#sed -i "s/127.0.0.1/${IP}/" plom/todo/foo.json
-echo "Server IP is ${IP}"
-sudo docker exec --user $USER $PD bash -c "cd /plom; plom-demo"
+docker exec --user $USER $PD bash -c "cd /plom; plom-demo"
