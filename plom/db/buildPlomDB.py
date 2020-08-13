c8f7bf134d85ee2413fc93ec420e81855be7af50
Colin B. Macdonald <cbm@m.fsf.org>
2020-08-12 22:22:53 -0700

    undo over-eager copyright update
    [ci-skip]

--- plom/db/buildPlomDB.py
+++ plom/db/buildPlomDB.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2019-2020 Andrew Rechnitzer
 # Copyright (C) 2019-2020 Colin B. Macdonald
-# Copyright (C) 2019-2020 Dryden Wiebe
+# Copyright (C) 2020 Dryden Wiebe
 
 import random
 from plom.db import PlomDB
