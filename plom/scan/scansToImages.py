7e1ce9fff5ba863bbdc57d441e8cd66f432379b3
Colin B. Macdonald <cbm@m.fsf.org>
2020-08-25 22:25:58 -0700

    update copyright

--- plom/scan/scansToImages.py
+++ plom/scan/scansToImages.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2018-2020 Andrew Rechnitzer
 # Copyright (C) 2018 Elvis Cai
 # Copyright (C) 2019-2020 Colin B. Macdonald
+# Copyright (C) 2020 Victoria Schuster
 
 import hashlib
 import os
