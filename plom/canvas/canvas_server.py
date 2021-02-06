55b2410ba6ff8f52a4f73770db9aa5e265d89103
Forest Kobayashi <fkobayashi@hmc.edu>
2021-02-05 16:57:28 -0800

    correct build process, fix default classlist

--- plom/canvas/canvas_server.py
+++ plom/canvas/canvas_server.py
@@ -35,6 +35,8 @@ def get_classlist(course, server_dir="."):
         ("Student", "ID", "SIS User ID", "SIS Login ID", "Section", "Student Number")
     ]
 
+    conversion = [("Internal Canvas ID", "Student", "SIS User ID")]
+
     for stud in students:
         stud_name, stud_id, stud_sis_id, stud_sis_login_id = (
             stud.user["sortable_name"],
@@ -43,19 +45,51 @@ def get_classlist(course, server_dir="."):
             stud.user["integration_id"],
         )
 
-        for (this_id, this_default, num_digits) in [
-            (stud_id, default_id, 7),
-            (stud_sis_id, default_sis_id, 8),
-            (stud_sis_login_id, default_sis_login_id, 12),
-        ]:
+        internal_canvas_id = stud.user_id
 
-            if not this_id:
-                # Too lazy to figure out how to pad with nested
-                # fstrings
-                this_id = str(this_default)
-                this_id = (num_digits - len(this_id)) * "0" + this_id
-                this_default += 1
+        # In order to make defaults work, we have to construct the
+        # list here in pieces, which is really inelegant and gross.
 
+        # Can't do this with a for loop, sadly, because pass by
+        # reference makes it hard to modify the default values.
+        #
+        # I say "can't" when really I mean "didn't"
+
+        # FIXME: Treat collisions
+
+        if (
+            not stud_id
+            or stud_id is None
+            or (type(stud_id) == str and stud_id in string.whitespace)
+        ):
+            stud_id = str(default_id)
+            # 5-7 characters is what I've seen, so let's just go with 7
+            stud_id = (7 - len(stud_id)) * "0" + stud_id
+            default_id += 1
+
+        if (
+            not stud_sis_id
+            or stud_sis_id is None
+            or (type(stud_sis_id) == str and stud_sis_id in string.whitespace)
+        ):
+            stud_sis_id = str(default_sis_id)
+            # 8 characters is necessary for UBC ID
+            stud_sis_id = (8 - len(stud_sis_id)) * "0" + stud_sis_id
+            default_sis_id += 1
+
+        if (
+            not stud_sis_login_id
+            or stud_sis_login_id is None
+            or (
+                type(stud_sis_login_id) == str
+                and stud_sis_login_id in string.whitespace
+            )
+        ):
+            stud_sis_login_id = str(default_sis_login_id)
+            stud_sis_login_id = (12 - len(stud_sis_login_id)) * "0" + stud_sis_login_id
+            default_sis_login_id += 1
+
+        # Add this information to the table we'll write out to the CSV
         classlist += [
             (
                 stud_name,
@@ -67,9 +101,15 @@ def get_classlist(course, server_dir="."):
             )
         ]
 
+        conversion += [(internal_canvas_id, stud_name, stud_sis_id)]
+
     with open(f"{server_dir}/classlist.csv", "w", newline="\n") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows(classlist)
+
+    with open(f"{server_dir}/conversion.csv", "w", newline="\n") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(conversion)
 
     return
 
@@ -141,7 +181,12 @@ def get_toml(assignment, server_dir="."):
     toml += "[idPages]\npages=[1]\n"
     toml += "[doNotMark]\npages=[2]\n"
     toml += f"[question.1]\npages={list(range(3,numberOfPages+1))}\n"
-    toml += f"mark={assignment.points_possible if assignment.points_possible else 1}\n"
+    toml += (
+        f"mark={int(assignment.points_possible) if assignment.points_possible else 1}\n"
+    )
+    if assignment.points_possible - int(assignment.points_possible) != 0:
+        assert False  # OK this error needs to be handled more
+        # intelligently in the future
     toml += 'select="fix"'
 
     with open(f"{server_dir}/canvasSpec.toml", "w") as f:
@@ -193,23 +238,12 @@ def initialize(course, assignment, server_dir="server-test"):
         capture_output=True,
     )
 
-    # We formerly launched the server permanently, but it was mucking
-    # up the pretty `tqdm` bars, so I removed that for now. The
-    # associated print statements were
-    #
-    #    print("\nRelaunching the server in the __main__ scope...")
-    #
-    # and
-    #
-    # print(
-    #     "Server *should* be running now (although we can't really tell since we've supressed the output). In light of this, be extra sure to explicitly kill the server (e.g., `pkill plom-server`) before trying to start a new one --- it can persist even after the original python process has been killed.\n\nTo verify if the server is running, you can try the command\n  ss -lntu\nto check if the 41984 port has a listener.\n"
-    # )
-
-    print("Launching the server temporarily...")
-    # launch the server as a background process. To suppress output,
-    # we're going to pipe it into /dev/null.
-
+    print("Launching plom server temporarily.")
     plom_server = subprocess.Popen(["plom-server", "launch"], stdout=subprocess.DEVNULL)
+
+    print(
+        "Server *should* be running now (although hopefully you can't because theoretically output should be suppressed). In light of this, be extra sure to explicitly kill the server (e.g., `pkill plom-server`) before trying to start a new one --- it can persist even after the original python process has been killed.\n\nTo verify if the server is running, you can try the command\n  ss -lntu\nto check if the 41984 port has a listener.\n"
+    )
 
     subprocess.run(["sleep", "3"])
 
@@ -218,18 +252,40 @@ def initialize(course, assignment, server_dir="server-test"):
         ["plom-build", "class", "classlist.csv"], capture_output=True
     )
 
+    print("Building the database...")
+    build_class = subprocess.run(
+        ["plom-build", "make", "--no-pdf"], capture_output=True
+    )
+
     os.chdir(o_dir)
 
-    print("killing the server.")
-    plom_server.kill()
-    return  # plom_server
+    return plom_server
 
 
-def get_submissions(assignment, server_dir="server-test"):
+def get_conversion_table(server_dir="server-test"):
+    conversion = {}
+    with open(f"{server_dir}/conversion.csv", "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",", quotechar='"')
+        for (i, row) in enumerate(reader):
+            if i == 0:
+                continue
+            else:
+                conversion[row[0]] = row[1:]
+    return conversion
+
+
+def get_submissions(assignment, server_dir="server-test", name_by_info=True):
     """
     get the submission pdfs out of Canvas
+
+    (name_by_info): Whether to make the filenames of the form ID_Last_First.pdf
+
     """
     o_dir = os.getcwd()
+
+    if name_by_info:
+        print("Fetching conversion table...")
+        conversion = get_conversion_table(server_dir=server_dir)
 
     os.chdir(server_dir)
 
@@ -238,7 +294,12 @@ def get_submissions(assignment, server_dir="server-test"):
 
     os.chdir("upload")
 
-    print("Moved into server-test/upload")
+    if not os.path.exists("submittedHWByQ"):
+        os.mkdir("submittedHWByQ")
+
+    os.chdir("submittedHWByQ")
+
+    print("Moved into server-test/upload/submittedHWByQ")
 
     print("Fetching submissions...")
     subs = list(assignment.get_submissions())
@@ -246,16 +307,27 @@ def get_submissions(assignment, server_dir="server-test"):
     # TODO: Parallelize requests
     unsubmitted = []
     for sub in tqdm(subs):  # TODO: is `aria2c` actually faster here lol??
-        sub_name = f"{sub.user_id}.pdf"
+        if name_by_info:
+            sub_id = sub.user_id
+            stud_name, stud_sis_id = conversion[str(sub_id)]
+            last_name, first_name = [name.strip() for name in stud_name.split(",")]
+            sub_name = f"{last_name}_{first_name}.{stud_sis_id}._.pdf"
+        else:
+            sub_name = f"{sub.user_id}.pdf"
+
         if not (sub.url is None):
             sub_url = sub.url
-            subprocess.run(["aria2c", sub_url, "-o", sub_name], capture_output=True)
+            subprocess.run(
+                ["curl", "-L", sub_url, "--output", sub_name], capture_output=True
+            )
         else:
             try:
                 for obj in sub.attachments:
                     if type(obj) == dict:
                         sub_url = obj["url"]
-                subprocess.run(["aria2c", sub_url, "-o", sub_name], capture_output=True)
+                subprocess.run(
+                    ["curl", "-L", sub_url, "--output", sub_name], capture_output=True
+                )
             except AttributeError:  # Catches if student didn't submit
                 unsubmitted += [sub]
                 continue
@@ -286,15 +358,21 @@ def scan_submissions(server_dir="server-test"):
     print("Temporarily exporting scanner password...")
 
     # TODO: Parallelize here
-    print("Applying `plom-scan` to pdfs...")
-    pdfs = [f for f in os.listdir() if ".pdf" == f[-4:]]
+    print("Applying `plom-hwscan` to pdfs...")
+    pdfs = [f for f in os.listdir("submittedHWByQ") if ".pdf" == f[-4:]]
     for pdf in tqdm(pdfs):
-        subprocess.run(["plom-scan", "process", pdf], capture_output=True)
-        # TODO: Make sure the --unknown flag doesn't cause any
-        # problems here
+        stud_id = pdf.split(".")[1]
+        assert len(stud_id) == 8
         subprocess.run(
-            ["plom-scan", "upload", pdf[:-4], "--unknowns"], capture_output=True
+            ["plom-hwscan", "process", "submittedHWByQ/" + pdf, stud_id, "-q", "1"],
+            capture_output=True,
         )
+
+    # Clean up any missing submissions
+    subprocess.run(
+        ["plom-hwscan", "missing"],
+        capture_output=True,
+    )
 
     os.chdir(o_dir)
 
@@ -319,30 +397,27 @@ def stupid_preamble():
     for course in courses:
         try:
             if "Colin" in course.name:
-                sbox = course
-            if "340 202" in course.name:
-                m340_202 = course
+                to_mark = course
+            # if "340 202" in course.name:
+            #     m340 = course
         except:
             pass
 
-    assignments = list(m340_202.get_assignments())
+    assignments = list(to_mark.get_assignments())
 
     assignment = assignments[0]
 
-    return m340_202, assignment
+    return to_mark, assignment
 
 
 if __name__ == "__main__":
 
     course, assignment = stupid_preamble()
 
-    initialize(course, assignment)
+    plom_server = initialize(course, assignment)
 
-    print(
-        "\n\nPlease start the plom server. Execution will continue assuming that you *have* in 10 seconds\n\n"
-    )
-    subprocess.run(["sleep", "10"])
+    # print("getting submissions from canvas...")
+    # get_submissions(assignment)
 
-    print("getting submissions from canvas...")
-    get_submissions(assignment)
+    print("scanning submissions...")
     scan_submissions()
