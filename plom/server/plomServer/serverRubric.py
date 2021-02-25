23a20c80be906fc8b454503b51ab3d9ca8de35de
Andrew <andrewr@math.ubc.ca>
2021-02-24 16:12:48 -0800

    Some standard manager default-rubrics

--- plom/server/plomServer/serverRubric.py
+++ plom/server/plomServer/serverRubric.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2020 Vala Vakilian
-# Copyright (C) 2021 Colin B. Macdonald
+# Copyright (C) 2021 Colin B. Macdonald,
+# Copyright (C) 2021 Andrew Rechnitzer
 
 """
 Comment-related server methods.
