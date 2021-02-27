862e4f0044c52d53db92165c4a8c03a75a6841f9
Forest Kobayashi <fkobayashi@hmc.edu>
2021-02-26 16:14:14 -0800

    begin refactor

---
+++ plom/canvas/canvas_glue.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2020-2021 Forest Kobayashi
+
+"""Get information from all the canvas courses and such
+"""
+
+import canvasapi as capi
+
+# Extend the CurrentUser class to add some helpful methods, e.g. one
+# to only fetch course
+class User(capi.current_user.CurrentUser):
+    def get_courses_teaching(self):
+        """
+        Get a list of courses in which the user has instructor status
+        """
+        teaching = []
+        for course in self.get_courses():
+            try:
+                for enrollee in course.enrollments:  # Necessary?
+                    if (enrollee["user_id"] == self.id) and (
+                        enrollee["type"] in ["teacher", "ta"]
+                    ):
+                        teaching += [course]
+            except AttributeError:
+                # It looks like old courses that are no longer active
+                # still show up here, but because we've lost access to
+                # them, we can't query any attributes. The assertion
+                # here basically just ensure that that's actually
+                # what's going on.
+                #
+                # TODO: INvestigate further?
+                assert course.access_restricted_by_date
+        self.teaching = teaching
+        return teaching
+
+
+def login(API_URL, API_KEY):
+    """
+    Instantiate a canvasapi object for the user by logging into
+    API_URL with API_KEY.
+
+    Example call:
+        login(
+            "https://canvas.ubc.ca",
+            "12345~uJcFOtJm0uOzZeAERsBLAhCOCU7zg5etm45yVGHLJ9FlgTiteuGmxFTwpBNcC4qd"
+        )
+    """
+    _canvas = capi.Canvas(API_URL, API_KEY)
+    user = _canvas.get_current_user()
+    del API_KEY, _canvas  # This probably doesn't do anything
+    return user
+
+
+def test_login():
+    from api_secrets import my_key as API_KEY
+
+    API_URL = "https://canvas.ubc.ca"
+    user = login(API_URL, API_KEY)
+    user = User(user)
+    del API_KEY, API_URL
+    return user
+
+
+if __name__ == "__main__":
+    user = test_login()
