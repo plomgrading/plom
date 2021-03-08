4dfe6e37fc55dfac6bae11e6b927ff3f73d48115
Colin B. Macdonald <cbm@m.fsf.org>
2021-03-08 09:18:45 -0800

    Using capital T on dvipng input for proper alpha
    Like t transparent gives gif-style single channel transparency with
    anti-aliasing of the background colour (white).  This gives artifacts
    when text placed on non-white background (such as self-scanned work).
    
    Instead capital T Transparent uses full alpha-channel transparency which
    works with png files.  In the past one had to be careful with these
    (*cough* Internet Explorer).  But surely now, *everyone*'s browser works
    with these.  And anyway, so far we don't care about browsers: it
    definitely works with Qt.

--- plom/textools.py
+++ plom/textools.py
@@ -1,14 +1,10 @@
-# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2019-2020 Andrew Rechnitzer
+# Copyright (C) 2019-2021 Colin B. Macdonald
 
 """Tools for working with TeX"""
 
-__copyright__ = "Copyright (C) 2018-2020 Andrew Rechnitzer and Colin B. Macdonald"
-__credits__ = "The Plom Project Developers"
-__license__ = "AGPL-3.0-or-later"
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
 import os
-import sys
 import subprocess
 import tempfile
 import shutil
@@ -65,7 +61,7 @@ def texFragmentToPNG(fragment, outName):
                 "-D",
                 "225",
                 "-bg",
-                "transparent",
+                "Transparent",
                 "frag.dvi",
                 "-o",
                 "frag.png",
