7f33e0c84a33e43b0f3584dd781110bdceffaa80
Colin B. Macdonald <cbm@m.fsf.org>
2021-03-14 10:04:18 -0700

    Proof of concept custom version map uploader

--- plom/produce/buildDatabaseAndPapers.py
+++ plom/produce/buildDatabaseAndPapers.py
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2020 Andrew Rechnitzer
-# Copyright (C) 2020 Colin B. Macdonald
+# Copyright (C) 2021 Colin B. Macdonald
 
 import os
 from getpass import getpass
+import random
 
 from plom.produce import build_all_papers, confirm_processed, identify_prenamed
 from plom.produce import paperdir
@@ -36,8 +37,23 @@ def buildDatabaseAndPapers(server=None, password=None, fakepdf=False, no_qr=Fals
         exit(10)
     try:
         spec = msgr.get_spec()
+        print("="*80)
+        print("TEMPORARILY HACKING VERSION MAP CLIENT SIDE")
+        print("="*80)
+        vmap = {}
+        for t in range(1, spec["numberToProduce"] + 1):
+            vmap[t] = {}
+            for g in range(spec["numberOfQuestions"]):  # runs from 0,1,2,...
+                gs = str(g + 1)  # now a str and 1,2,3,...
+                if spec["question"][gs]["select"] == "fix":
+                    vmap[t][g + 1] = 1
+                elif spec["question"][gs]["select"] == "shuffle":
+                    vmap[t][g + 1] = random.randint(1, spec["numberOfVersions"])
+                elif spec["question"][gs]["select"] == "param":
+                    vmap[t][g + 1] = random.randint(1, spec["numberOfVersions"])
+
         try:
-            status = msgr.TriggerPopulateDB()
+            status = msgr.TriggerPopulateDB(vmap)
         except PlomBenignException:
             print("Error: Server already has a populated database")
             exit(3)
