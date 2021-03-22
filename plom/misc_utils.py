28c675c094c72fab2d1aca190e52ed682c0c0140
Colin B. Macdonald <cbm@m.fsf.org>
2021-03-22 10:45:30 -0700

    Take tab names from prev sequences of (a), (b), etc
    Various sequences supported, tries to take the longest and extend it.

--- plom/misc_utils.py
+++ plom/misc_utils.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2019 Omer Angel
+# Copyright (C) 2019, 2021 Colin B. Macdonald
+# Copyright (C) 2020 Andrew Rechnitzer
 
 """Misc utilities"""
 
-__author__ = "Colin B. Macdonald, Omer Angel"
-__copyright__ = "Copyright (C) 2019 Colin B. Macdonald, Omer Angel"
-__license__ = "AGPL-3.0-or-later"
-# SPDX-License-Identifier: AGPL-3.0-or-later
-
 import sys
 import math
+import string
 
 
 def format_int_list_with_runs(L, use_unicode=None):
@@ -51,3 +50,78 @@ def _flatten_2len_runs(L):
         else:
             L2.append(l)
     return L2
+
+
+def next_in_longest_subsequence(items):
+    """Guess next entry in the longest unordered contiguous subsequence.
+
+    args:
+        items (list): an unordered list of strings.
+
+    return:
+        str/None: the next item in a longest subsequence.
+
+    Examples:
+
+    >>> next_in_longest_subsequence(["(a)", "(b)"])
+    '(c)'
+
+    >>> next_in_longest_subsequence(["i.", "ii.", "iii."])
+    'iv.'
+
+    >>> next_in_longest_subsequence(["2", "1", "C", "(a)", "A", "B"])
+    'D'
+
+
+    >>> next_in_longest_subsequence(["2", "b", "c", "Z", "foo"])
+
+    >>> next_in_longest_subsequence(["a.", "b)", "(c)"])
+    'b.'
+
+    Notes:
+      * Behaviour in a tie not well-defined; you'll get one of them.
+      * "(a), (b)" and "a., b." are different subsequences.
+      * The sequences should not be longer than the alphabet.  Overly
+        long sequences don't count, e.g., if you already have a-z, then
+        that subsequence cannot be extended:
+
+    >>> from string import ascii_lowercase
+    >>> next_in_longest_subsequence(["Q1", "Q2", *ascii_lowercase])
+    'Q3'
+    """
+    romans = ["i", "ii", "iii", "iv", "v", "vi", "vii", "viii", "ix", "x"]
+    romans.extend(["x" + n for n in romans] + ["xx" + n for n in romans])
+    smallints = range(1, 31)
+
+    # these are the sequences we will search for
+    sequences = [
+        string.ascii_lowercase,
+        [f"{x}." for x in string.ascii_lowercase],
+        [f"{x})" for x in string.ascii_lowercase],
+        [f"({x})" for x in string.ascii_lowercase],
+        string.ascii_uppercase,
+        [f"{x}." for x in string.ascii_uppercase],
+        [f"{x})" for x in string.ascii_uppercase],
+        [f"({x})" for x in string.ascii_uppercase],
+        romans,
+        [f"{x}." for x in romans],
+        [f"{x})" for x in romans],
+        [f"({x})" for x in romans],
+        [f"Q{x}" for x in smallints],
+        [f"{x}." for x in smallints],
+        [f"{x}" for x in smallints],
+    ]
+
+    counts = [0] * len(sequences)
+    for idx, seq in enumerate(sequences):
+        for count, x in enumerate(seq):
+            if x not in items:
+                counts[idx] = count
+                break
+
+    idx, n = max(enumerate(counts), key=lambda t: t[1])
+
+    if n > 0:
+        return sequences[idx][n]
+
+    return None
