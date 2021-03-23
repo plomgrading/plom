6caac185bf95c40289308ab08b8a649c35222aab
Forest Kobayashi <fkobayashi@hmc.edu>
2021-03-23 13:22:07 -0700

    add features to canvas glue

--- plom/canvas/canvas_glue.py
+++ plom/canvas/canvas_glue.py
@@ -28,10 +28,10 @@ class User(capi.current_user.CurrentUser):
         Get a list of courses in which the user has instructor status
         """
         teaching = []
-        for course in self.get_courses():
+        for course in list(self.get_courses()):
             try:
                 for enrollee in course.enrollments:  # Necessary?
-                    if (enrollee["user_id"] == self.id) and (
+                    if (enrollee["user_id"] == self.canvas.id) and (
                         enrollee["type"] in ["teacher", "ta"]
                     ):
                         teaching += [course]
@@ -47,9 +47,78 @@ class User(capi.current_user.CurrentUser):
                     assert course.access_restricted_by_date
                 except AttributeError:
                     print(f"Course {course} has no such attribute.")
-                    print(dir(course))
+                    # print(dir(course))
         self.teaching = teaching
         return teaching
+
+# Extend the course.Course class to add some helpful methods, e.g. one
+# to fetch all students in the course
+class Course(capi.course.Course):
+    def __init__(self, capi_course):
+
+        # Ensure that we got the right thing passed in
+        assert isinstance(capi_course, capi.course.Course)
+
+        return
+
+    def get_students(self):
+        """
+        Get a list of the students in the class
+        """
+        students = [
+            _ for _ in course.get_enrollments() if _.role == "StudentEnrollment"
+        ]
+        self.students = students
+        return students
+
+    def get_classlist_csv(self):
+        """
+        Plom needs a csv with the classlist data
+        """
+
+        try:
+            students = self.students
+        except AttributeError:
+            students = self.get_students()
+
+        classlist = [
+            (
+                "Student",
+                "ID",
+                "SIS User ID",
+                "SIS Login ID",
+                "Section",
+                "Student Number",
+            )
+        ]
+
+        for stud in students:
+            try:
+                stud_name, stud_id, stud_sis_id, stud_sis_login_id = (
+                    stud.user["sortable_name"],
+                    stud.id,
+                    stud.sis_user_id,
+                    stud.user["integration_id"],
+                )
+
+                # Add this information to the table we'll write out to
+                # the CSV
+                classlist += [
+                    (
+                        stud_name,
+                        stud_id,
+                        stud_sis_id,
+                        stud_sis_login_id,
+                        self.name,
+                        stud_sis_id,
+                    )
+                ]
+
+            except AttributeError:
+                assert stud.user["name"] == "Test Student"
+
+        self.classlist_csv = classlist
+        return classlist
 
 
 def login(API_URL, API_KEY):
