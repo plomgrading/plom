6733d4e18c728df1183125fdc08aa502fff760d3
Colin B. Macdonald <cbm@m.fsf.org>
2021-03-30 21:41:22 -0700

    update copyright headers

--- plom/db/db_create.py
+++ plom/db/db_create.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2018-2020 Andrew Rechnitzer
+# Copyright (C) 2018-2021 Andrew Rechnitzer
 # Copyright (C) 2020-2021 Colin B. Macdonald
 
 from datetime import datetime
