3ccbd875de63be2eb83f6a6d8f668c156faeb7cc
Colin B. Macdonald <cbm@m.fsf.org>
2021-04-02 21:52:04 -0700

    Make rubric api requires a "kind" field.
    The DB end of things needs "kind" but it was missing at the verify bit.
    Probably just an oversight when meta came back.

--- plom/server/plomServer/serverRubric.py
+++ plom/server/plomServer/serverRubric.py
@@ -25,7 +25,10 @@ def McreateRubric(self, username, new_rubric):
         list: [True, new-key] or [False]
     """
     # check rubric sent has required fields
-    if any(X not in new_rubric for X in ["delta", "text", "question", "tags", "meta"]):
+    if any(
+        X not in new_rubric
+        for X in ("delta", "text", "question", "tags", "meta", "kind")
+    ):
         return [False]
     # else let DB create the new element and return the new key
     return self.DB.McreateRubric(username, new_rubric)
@@ -48,9 +51,8 @@ def MmodifyRubric(self, username, key, updated_rubric):
         list: [True, new-key] or [False]
     """
     # check rubric sent has required fields
-    if any(
-        X not in updated_rubric for X in ["delta", "text", "question", "tags", "meta"]
-    ):
+    # TODO: currently cannot change question
+    if any(X not in updated_rubric for X in ("delta", "text", "tags", "meta", "kind")):
         return [False, "incomplete"]
     # else let DB modify the rubric and return the key.
     return self.DB.MmodifyRubric(username, key, updated_rubric)
