933fcfdc7dd9d95cf9d3dac8bc7b373ecd96b256
Colin B. Macdonald <cbm@m.fsf.org>
2021-07-11 11:18:30 -0700

    check if has orientation before trying jpegtran
    But only warn if we don't have jpegtran if we actually had a
    re-orientation to do.  Happily Pillow (PIL) which we already depend on,
    can check the jpeg orientation tags.  So check if we have a non-zero tag
    before we bother with jpegtran

--- plom/scan/rotate.py
+++ plom/scan/rotate.py
@@ -2,6 +2,8 @@
 # Copyright (C) 2020-2021 Colin B. Macdonald
 
 import os
+import PIL.Image
+import PIL.ExifTags
 import subprocess
 from warnings import warn
 
@@ -52,26 +54,47 @@ def rotateBitmap(fname, angle):
 def normalizeJPEGOrientation(f):
     """Transform image according to its Exif metadata.
 
+    args:
+        f (pathlib.Path/str): a jpeg file.
+
     Gives a warning if size not a multiple 16 b/c underlying library
     just quietly mucks up the bottom/right edge:
     https://github.com/jbaiter/jpegtran-cffi/issues/23
 
     In Plom, we generally transcode jpeg's that are not multiples of 16.
     """
-    im = jpegtran.JPEGImage(f)
-    if im.exif_orientation:
-        if im.width % 16 or im.height % 16:
-            warn(f'  jpeg "{f}" dims not mult of 16: re-orientations may be lossy')
-        im2 = im.exif_autotransform()
-        print(
-            '  normalizing "{}" {}x{} to "{}" {}x{}'.format(
-                im.exif_orientation,
-                im.width,
-                im.height,
-                im2.exif_orientation,
-                im2.width,
-                im2.height,
-            )
+    # First use Pillow: return early if we can, in case we don't have jpegtran
+    pil_img = PIL.Image.open(f)
+    exif = pil_img.getexif()
+    all_tags = PIL.ExifTags.TAGS
+    orientations = [v for k, v in exif.items() if all_tags.get(k) == "Orientation"]
+    if not orientations or orientations[0] == 0:
+        return
+    if orientations[0] == 1:
+        return
+
+    if not have_jpegtran:
+        warn(
+            f"  jpeg {f} has EXIF orientation but no jpegtran-cffi: skipping orientation normalize"
         )
-        # str to workaround https://github.com/jbaiter/jpegtran-cffi/issues/28
-        im2.save(str(f))
+        return
+
+    im = jpegtran.JPEGImage(f)
+    if not im.exif_orientation:
+        return
+
+    if im.width % 16 or im.height % 16:
+        warn(f'  jpeg "{f}" dims not mult of 16: re-orientations may be lossy')
+    im2 = im.exif_autotransform()
+    print(
+        '  normalizing "{}" {}x{} to "{}" {}x{}'.format(
+            im.exif_orientation,
+            im.width,
+            im.height,
+            im2.exif_orientation,
+            im2.width,
+            im2.height,
+        )
+    )
+    # str to workaround https://github.com/jbaiter/jpegtran-cffi/issues/28
+    im2.save(str(f))
