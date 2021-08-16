6b390e967dd6e50a414ebed1af6c0d946da49a3d
Colin B. Macdonald <cbm@m.fsf.org>
2021-08-16 10:52:23 -0700

    Remove PageView's unused latexAFragment
    Various things call latexAFragment via their parent but none of those
    parents are PageView, at least as far as I can tell.

--- plom/client/pageview.py
+++ plom/client/pageview.py
@@ -77,25 +77,6 @@ class PageView(QGraphicsView):
         # then any other stuff needed by parent class
         super().resizeEvent(event)
 
-    def DEPRECATED_latexAFragment(self, txt):
-        """
-        Latexes a fragment of text.
-
-        Args:
-            txt (str): text to be formatted.
-
-        Returns:
-            (png): a file containing the Latexed text.
-
-        TODO: is this ever called?  pageview has tex support?
-        """
-        cur = self.cursor()
-        self.setCursor(QCursor(Qt.WaitCursor))
-        QApplication.processEvents()  # this triggers a cursor update
-        ret = self.parent.latexAFragment(txt)
-        self.setCursor(cur)
-        return ret
-
     def setZoomSelector(self, update=False):
         """
         Sets Zoom combo box to show current selection.
