0ea5f4d7fec6b6fed1a8dc870540dfd31a638d19
Colin B. Macdonald <cbm@m.fsf.org>
2021-08-23 11:12:35 -0700

    Add back a stub function and deprecation warning

---
+++ plom/scripts/scan.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2021 Colin B. Macdonald
+
+"""Stub for a deprecated technique of accessing scanning tools."""
+
+from warnings import warn
+
+import plom.scan
+import plom.scan.__main__
+
+
+def main():
+    warn(
+        "Please use `plom-scan` or `python3 -m plom.scan` directly",
+        category=DeprecationWarning,
+    )
+    plom.scan.__main__.main()
+
+
+if __name__ == "__main__":
+    main()
