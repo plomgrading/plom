d90e46f0b00fa087af22d84bfd496af9ef192520
Colin B. Macdonald <cbm@m.fsf.org>
2021-09-06 22:45:18 -0700

    Import star for plom.produce

--- plom/produce/__init__.py
+++ plom/produce/__init__.py
@@ -11,6 +11,8 @@ __license__ = "AGPL-3.0-or-later"
 
 from pathlib import Path
 
+from plom import __version__
+
 paperdir = Path("papersToPrint")
 
 from .buildNamedPDF import build_all_papers, confirm_processed, identify_prenamed
@@ -20,3 +22,11 @@ from .buildDatabaseAndPapers import build_database, build_papers
 
 from .buildClasslist import process_classlist_file, get_demo_classlist
 from .upload_classlist import upload_classlist, upload_demo_classlist
+
+# what you get from "from plom.produce import *"
+__all__ = [
+    "get_demo_classlist",
+    "process_classlist_file",
+    "upload_classlist",
+    "upload_demo_classlist",
+]
