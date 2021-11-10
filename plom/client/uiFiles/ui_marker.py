1aa282ba15263a90528f214015cb99497d641954
Colin B. Macdonald <cbm@m.fsf.org>
2021-11-09 20:04:00 -0800

    Minor change for smaller diff in autogen file

--- plom/client/uiFiles/ui_marker.py
+++ plom/client/uiFiles/ui_marker.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'qtCreatorFiles/ui_marker.ui'
+# Form implementation generated from reading ui file './qtCreatorFiles/ui_marker.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.0
 #
