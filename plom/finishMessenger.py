9d77736e92ac1bf7ef69e7e3bccbf54e55015a3f
Colin B. Macdonald <cbm@m.fsf.org>
2021-11-09 19:06:47 -0800

    Missing exception import

--- plom/finishMessenger.py
+++ plom/finishMessenger.py
@@ -6,7 +6,11 @@ from io import BytesIO
 import urllib3
 import requests
 
-from plom.plom_exceptions import PlomSeriousException, PlomAuthenticationException
+from plom.plom_exceptions import (
+    PlomSeriousException,
+    PlomAuthenticationException,
+    PlomNoSolutionException,
+)
 from plom.baseMessenger import BaseMessenger
 
 # TODO: how to do this in subclass?
