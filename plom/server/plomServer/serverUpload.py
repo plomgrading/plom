03e7ab5dce042921dfbfba221b65894a0939c4b5
Joey <joeyshi12@gmail.com>
2022-02-16 22:00:43 -0800

    consolidate subprocess page rotation calls into separate function

--- plom/server/plomServer/serverUpload.py
+++ plom/server/plomServer/serverUpload.py
@@ -317,7 +317,7 @@ def unknownToTestPage(self, file_name, test, page, rotation):
     status, code, msg = self.DB.moveUnknownToTPage(file_name, test, page)
     if status:
         # rotate the page
-        self.DB.setImageRotation(file_name, rotation)
+        rotatePage(file_name, rotation)
         return (True, "testPage", None)
 
     if not status and code != "scanned":
@@ -327,7 +327,7 @@ def unknownToTestPage(self, file_name, test, page, rotation):
     status, code, msg = self.DB.moveUnknownToCollision(file_name, test, page)
     if status:
         # rotate the page
-        self.DB.setImageRotation(file_name, rotation)
+        rotatePage(file_name, rotation)
         return (True, "collision", None)
     return (status, code, msg)
 
@@ -336,7 +336,7 @@ def unknownToExtraPage(self, fname, test, question, rotation):
     rval = self.DB.moveUnknownToExtraPage(fname, test, question)
     if rval[0]:
         # moved successfully. now rotate the page
-        self.DB.setImageRotation(fname, rotation)
+        rotatePage(fname, rotation)
     return rval
 
 
@@ -344,7 +344,7 @@ def unknownToHWPage(self, fname, test, questions, rotation):
     rval = self.DB.moveUnknownToHWPage(fname, test, questions)
     if rval[0]:
         # moved successfully. now rotate the page
-        self.DB.setImageRotation(fname, rotation)
+        rotatePage(fname, rotation)
     return rval
 
 
@@ -431,6 +431,15 @@ def getImagesInBundle(self, bundle_name):
 
 def getPageFromBundle(self, bundle_name, bundle_order):
     return self.DB.getPageFromBundle(bundle_name, bundle_order)
+
+
+def rotatePage(file_name, rotation):
+    subprocess.run(
+        ["mogrify", "-quiet", "-rotate", rotation, file_name],
+        stderr=subprocess.STDOUT,
+        shell=False,
+        check=True,
+    )
 
 
 ##
