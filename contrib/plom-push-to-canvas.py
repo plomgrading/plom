8eb15eaf6dadda2c81c2b4a846c703976cf95d14
Colin B. Macdonald <cbm@m.fsf.org>
2022-02-22 11:37:38 -0800

    Fix Canvas scripts --section/--no-section tristate
    Fixes #1968.

--- contrib/plom-push-to-canvas.py
+++ contrib/plom-push-to-canvas.py
@@ -191,16 +191,15 @@ if __name__ == "__main__":
 
     if args.no_section:
         section = None
+    elif args.section:
+        section = get_section_by_id_number(course, args.section)
     else:
-        if args.section is None:
-            section = get_section_by_id_number(course, args.section)
+        section = interactively_get_section(course)
+        if section is None:
+            print('Note: you can use "--no-section" to omit selecting section.\n')
         else:
-            section = interactively_get_section(course)
-            if section is None:
-                print('Note: you can use "--no-section" to omit selecting section.\n')
-            else:
-                print(f'Note: you can use "--section {section.id}" to reselect.\n')
-        print(f"Ok using section: {section}")
+            print(f'Note: you can use "--section {section.id}" to reselect.\n')
+    print(f"Ok using section: {section}")
 
     if args.assignment:
         assignment = get_assignment_by_id_number(course, args.assignment)
