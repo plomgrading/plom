4eb6d0df753ebaac04124626ce64d1d4f3887240
Colin B. Macdonald <cbm@m.fsf.org>
2022-03-22 21:48:32 -0700

    Move IDReader outside server module
    Maybe this will fix CI problems?

--- plom/server/run_the_predictor.py
+++ plom/server/run_the_predictor.py
@@ -17,8 +17,8 @@ import json
 import os
 import sys
 
-from plom.server.IDReader_RF.model_utils import download_or_train_model
-from plom.server.IDReader_RF.predictStudentID import compute_probabilities
+from plom.IDReader_RF.model_utils import download_or_train_model
+from plom.IDReader_RF.predictStudentID import compute_probabilities
 
 from plom import specdir
 
