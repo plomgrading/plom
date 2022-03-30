1ca6d6b59adeab2ce72e6799df82d5ca7f98b320
Andrew <andrewr@math.ubc.ca>
2022-03-30 10:48:49 -0700

    Really helps to add the audit source file

---
+++ plom/finish/audit.py
@@ -0,0 +1,21 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022 Andrew Rechnitzer
+
+import json
+from plom.finish import start_messenger
+
+
+def main(server=None, password=None):
+    msgr = start_messenger(server, password)
+    try:
+        audit = msgr.RgetFileAudit()
+    finally:
+        msgr.closeUser()
+        msgr.stop()
+
+    with open("audit.json", "w+") as fh:
+        json.dump(audit, fh, indent="  ")
+
+
+if __name__ == "__main__":
+    main()
