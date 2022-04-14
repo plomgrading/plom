5f6c50d50d7b334b7391e5f5e9fef170838f623a
Colin B. Macdonald <cbm@m.fsf.org>
2022-04-14 11:06:47 -0700

    TODOs and docs re updating predictions

--- plom/server/plomServer/serverID.py
+++ plom/server/plomServer/serverID.py
@@ -188,9 +188,9 @@ def IDputPredictions(self, predictions, classlist, spec):
             id_predictions[int(sid)].append(X["name"])
     # now push everything into the DB
     problem_list = []
-    # TODO: may need to erase the table here?
     for sid, test_and_name in id_predictions.items():
         # get the student_name from the classlist
+        # TODO: probably we should only do this if current certainty less than 0.5
         # returns (True,None,None) or (False, 409, msg) or (False, 404, msg)
         r, what, msg = self.server.DB.add_or_change_id_prediction(
             test_and_name[0], sid, 0.5
@@ -308,9 +308,9 @@ def predict_id_lap_solver(self):
     prediction_pairs = lap_solver(papers, sids, cost_matrix)
     status += f" done in {time.process_time() - t:.02} seconds."
 
-    # TODO: may need to erase the table here?
     log.info("Saving prediction results into database /w certainty 0.5")
     for test_number, student_ID in prediction_pairs:
+        # TODO: we probably should get current uncertainties and not overwrite if above 0.5
         self.DB.add_or_change_id_prediction(test_number, student_ID, 0.5)
         # TODO - capture any error outputs
 
