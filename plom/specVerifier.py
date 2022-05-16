ffb9848830ee725523b1946de29a028471fe718a
Colin B. Macdonald <cbm@m.fsf.org>
2022-05-15 20:11:53 -0700

    specVerifier: use ValueError
    We removed `numberToPrename`, throwing a NotImplementedError.  This was
    reasonable but:
    
      * the docs for specVerifier do not mention the new behaviour, only ValueError
      * various bits of code, e.g., in API for `put_spec`, only expect ValueError and thus fail with 500 instead of something more graceful.
      * usually NotImplementedError is for code we haven't written yet.
    
    There is a deprecation warning mechanism but we already dropped it (and
    we're not at 1.0.0 yet).
    
    I think it might be easier to just throw ValueError.

--- plom/specVerifier.py
+++ plom/specVerifier.py
@@ -495,9 +495,9 @@ class SpecVerifier:
                 print(f'  contains "{x}"{chk}')
         # check for no longer supported numberToName field
         if "numberToName" in self.spec:
-            raise NotImplementedError(
-                'The "numberToName" spec-field is removed in favour of'
-                f' the "paper_number" column in the classlist. {self.spec}'
+            raise ValueError(
+                'The "numberToName" spec-field has been removed in favour'
+                ' of the "paper_number" column in the classlist.'
             )
 
     def check_name_and_production_numbers(self, print=print):
