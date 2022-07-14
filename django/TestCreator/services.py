628735d6d05b53aebda4bf9811e6d01b71628bd5
Edith Coates <ecoates.bc@gmail.com>
2022-07-14 10:56:02 -0700

    progress service functions

--- django/TestCreator/services.py
+++ django/TestCreator/services.py
@@ -654,3 +654,92 @@ def create_page_thumbnail_list(pdf: models.ReferencePDF):
         pages.append(thumbnail)
 
     return pages
+
+
+"""
+Test progress functions
+"""
+
+def get_progress():
+    """
+    Get or init the singleton test progress object
+
+    Returns:
+        models.TestSpecProgress
+    """
+    progress, created = models.TestSpecProgress.objects.get_or_create(pk=1)
+    return progress
+
+
+def reset_progress():
+    """
+    Clear the progress object
+
+    Returns:
+        models.TestSpecProgress
+    """
+    models.TestSpecProgress.objects.all().delete()
+    return get_progress()
+
+
+def progress_init_questions():
+    """
+    Create a dict of each question in the test: is question i's detail page submitted?
+    """
+    question_dict = {i: False for i in range(get_num_questions)}
+    progress = get_progress()
+    progress.are_questions_completed = question_dict
+    progress.save()
+    
+
+def progress_init_pages():
+    """
+    Create a dict of each page in the test: has page i been selected by something?
+    """
+    num_pages = len(load_spec().pages)
+    page_dict = {i: False for i in range(num_pages)}
+    progress = get_progress()
+    progress.are_pages_selected = page_dict
+    progress.save()
+
+
+def progress_set_names(complete: bool):
+    """Set the completed status of the names page"""
+    progress = get_progress()
+    progress.is_names_completed = complete
+    progress.save()
+
+
+def progress_set_versions_pdf(complete: bool):
+    """Set the completed status of the versions/upload pdf page"""
+    progress = get_progress()
+    progress.is_versions_pdf_completed = complete
+    progress.save()
+
+
+def progress_set_id_page(complete: bool):
+    """Set the completed status of the ID select page"""
+    progress = get_progress()
+    progress.is_id_page_completed = complete
+    progress.save()
+
+
+def progress_set_question_page(complete: bool):
+    """Set the completed status of the names page"""
+    progress = get_progress()
+    progress.is_question_page_completed = complete
+    progress.save()
+
+
+def progress_set_question_detail_page(index: int, complete: bool):
+    """Set the completed status of a question detail page"""
+    progress = get_progress()
+    progress.are_questions_completed[index] = complete
+    progress.save()
+
+
+def progress_set_page_selected(index: int, selected: bool):
+    """Set the selected status of a test page"""
+    progress = get_progress()
+    progress.are_pages_selected[index] = selected
+    progress.save()
