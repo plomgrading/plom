68873c31b9ce4b86e61a4156aedbd031f06e2a95
Edith Coates <ecoates.bc@gmail.com>
2022-07-26 12:09:36 -0700

    Reference PDF tests

--- django/TestCreator/tests/__init__.py
+++ django/TestCreator/tests/__init__.py
@@ -4,3 +4,4 @@ from .test_testpec_questions import *
 from .test_testspec_forms import *
 from .test_testspec_progress import *
 from .test_testspec_question_detail_view import *
+from .test_testspec_refpdf import *
