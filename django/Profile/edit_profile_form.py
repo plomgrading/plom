331422fbde43c97f38717cfb59a3aeaf40ea148b
brennenchiu <bchiyeung1996@gmail.com>
2022-08-08 21:41:17 -0700

    has a working edit profile page, but still need a bit more design

---
+++ django/Profile/edit_profile_form.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
+
+
+class EditProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'email']
--- django/Profile/views.py
+++ django/Profile/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-
+from .edit_profile_form import EditProfileForm
 
 # Create your views here.
 
 
-class Profile(View):
+class Profile(LoginRequiredMixin, View):
+    login_url = 'login'
     profile_page = 'Profile/profile.html'
     navbar_colour = {'admin': '#808080',
                      'manager': '#AD9CFF',
                      'marker': '#FF434B',
                      'scanner': '#0F984F'}
+    form = EditProfileForm()
 
     def get(self, request):
         try:
@@ -22,7 +24,20 @@ class Profile(View):
             colour = Profile.navbar_colour[user]
         else:
             colour = '#FFFFFF'
-            context = {'navbar_colour': colour, 'user_group': user}
+            context = {'form': Profile.form,'navbar_colour': colour, 'user_group': user}
             return render(request, self.profile_page, context)
-        context = {'navbar_colour': colour, 'user_group': user}
+        context = {'form': Profile.form, 'navbar_colour': colour, 'user_group': user}
         return render(request, self.profile_page, context, status=200)
+
+    def post(self, request):
+        try:
+            user = request.user.groups.all()[0].name
+        except IndexError:
+            user = None
+        if user in Profile.navbar_colour:
+            colour = Profile.navbar_colour[user]
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            context = {'form': Profile.form, 'navbar_colour': colour, 'user_group': user}
+            return render(request, self.profile_page, context, status=200)
