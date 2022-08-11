3cb078039409004ab37e136e72d07aac1460cf67
brennenchiu <bchiyeung1996@gmail.com>
2022-08-11 12:26:05 -0700

    scanners and markers created and added to their respective group

--- django/Authentication/management/commands/plom_create_demo_users.py
+++ django/Authentication/management/commands/plom_create_demo_users.py
@@ -1,21 +1,47 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User, Group
+from django.db import IntegrityError
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        number_of_users = 5
-        demon_users = []
-        email = '@plom.ca'
+        range_of_scanners_markers = 5
         manager_group = Group.objects.get(name='manager')
         marker_group = Group.objects.get(name='marker')
         scanner_group = Group.objects.get(name='scanner')
+        exist_usernames = [str(username) for username in User.objects.all()]
+        email = '@plom.ca'
 
         # Here is to create a single manager user
         manager = 'manager1'
-        User.objects.create_user(username=manager,
-                                 email=manager + email,
-                                 password=manager).groups.add(manager_group)
+        scanner = 'scanner'
+        scanner_username = ''
+        marker_username = ''
+        marker = 'marker'
+        try:
+            User.objects.create_user(username=manager,
+                                     email=manager + email,
+                                     password=manager).groups.add(manager_group)
+            print(f'{manager} created and added to {manager_group} group!')
+
+        except IntegrityError:
+            print(f'{manager} already exists!')
+
+        try:
+            for number_of_scanner_marker in range(1, range_of_scanners_markers + 1):
+                scanner_username = scanner + str(number_of_scanner_marker)
+                marker_username = marker + str(number_of_scanner_marker)
+                User.objects.create_user(username=scanner_username,
+                                         email=scanner_username + email,
+                                         password=scanner_username).groups.add(scanner_group)
+                print(f'{scanner_username} created and added to {scanner_group} group!')
+                User.objects.create_user(username=marker_username,
+                                         email=marker_username + email,
+                                         password=marker_username).groups.add(marker_group)
+                print(f'{marker_username} created and added to {marker_group} group!')
+        except IntegrityError:
+            print(f'{scanner_username} already exists!')
+            print(f'{marker_username} already exists!')
         # demon_users.append(manager)
         # # Here is to create a list of demo scanner and marker users
         # scanner = 'scanner'
