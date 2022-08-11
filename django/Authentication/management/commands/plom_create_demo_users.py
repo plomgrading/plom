2c125d03a14fc376e9ff545e0a73aba78ce39136
brennenchiu <bchiyeung1996@gmail.com>
2022-08-10 17:12:30 -0700

    created demo manager and add to manager group but needs to check for if manager1 exist already

--- django/Authentication/management/commands/plom_create_demo_users.py
+++ django/Authentication/management/commands/plom_create_demo_users.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User, Group
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        number_of_users = 5
+        demon_users = []
         email = '@plom.ca'
         manager_group = Group.objects.get(name='manager')
         marker_group = Group.objects.get(name='marker')
@@ -12,6 +14,20 @@ class Command(BaseCommand):
         # Here is to create a single manager user
         manager = 'manager1'
         User.objects.create_user(username=manager,
-                                 email=manager+email,
+                                 email=manager + email,
                                  password=manager).groups.add(manager_group)
-
+        # demon_users.append(manager)
+        # # Here is to create a list of demo scanner and marker users
+        # scanner = 'scanner'
+        # marker = 'marker'
+        # for index in range(1, number_of_users + 1):
+        #     demon_users.append(scanner + str(index))
+        #     demon_users.append(marker + str(index))
+        #
+        # print(demon_users)
+        #
+        # test = 'manager'
+        # if test in str(User.objects.all().get(username=test)):
+        #     print("Yes")
+        # else:
+        #     print("No")
