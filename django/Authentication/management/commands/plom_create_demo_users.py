1018c3f24e82ef87b791e54c508ffeb98c8f337d
brennenchiu <bchiyeung1996@gmail.com>
2022-08-11 15:10:40 -0700

    error check if a demo user is deleted, run command will add them again

--- django/Authentication/management/commands/plom_create_demo_users.py
+++ django/Authentication/management/commands/plom_create_demo_users.py
@@ -15,8 +15,6 @@ class Command(BaseCommand):
         # Here is to create a single manager user
         manager = 'manager1'
         scanner = 'scanner'
-        scanner_username = ''
-        marker_username = ''
         marker = 'marker'
         try:
             User.objects.create_user(username=manager,
@@ -27,33 +25,22 @@ class Command(BaseCommand):
         except IntegrityError:
             print(f'{manager} already exists!')
 
-        try:
-            for number_of_scanner_marker in range(1, range_of_scanners_markers + 1):
-                scanner_username = scanner + str(number_of_scanner_marker)
-                marker_username = marker + str(number_of_scanner_marker)
+        for number_of_scanner_marker in range(1, range_of_scanners_markers + 1):
+            scanner_username = scanner + str(number_of_scanner_marker)
+            marker_username = marker + str(number_of_scanner_marker)
+
+            if scanner_username in exist_usernames:
+                print(f'{scanner_username} already exists!')
+            else:
                 User.objects.create_user(username=scanner_username,
                                          email=scanner_username + email,
                                          password=scanner_username).groups.add(scanner_group)
                 print(f'{scanner_username} created and added to {scanner_group} group!')
+
+            if marker_username in exist_usernames:
+                print(f'{marker_username} already exists!')
+            else:
                 User.objects.create_user(username=marker_username,
                                          email=marker_username + email,
                                          password=marker_username).groups.add(marker_group)
                 print(f'{marker_username} created and added to {marker_group} group!')
-        except IntegrityError:
-            print(f'{scanner_username} already exists!')
-            print(f'{marker_username} already exists!')
-        # demon_users.append(manager)
-        # # Here is to create a list of demo scanner and marker users
-        # scanner = 'scanner'
-        # marker = 'marker'
-        # for index in range(1, number_of_users + 1):
-        #     demon_users.append(scanner + str(index))
-        #     demon_users.append(marker + str(index))
-        #
-        # print(demon_users)
-        #
-        # test = 'manager'
-        # if test in str(User.objects.all().get(username=test)):
-        #     print("Yes")
-        # else:
-        #     print("No")
