7d8c9183d41aaece007335779424771eaf8778e5
Andrew Rechnitzer <andrewr@math.ubc.ca>
2022-08-12 14:22:52 -0700

    Test source upload/download/remove command working

--- django/Preparation/management/commands/plom_preparation_test_source.py
+++ django/Preparation/management/commands/plom_preparation_test_source.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from Preparation.services import TestSourceService
+from Preparation.services import TestSourceService, temp_functions
 
 from pathlib import Path
 
@@ -9,13 +9,14 @@ class Command(BaseCommand):
     help = "Displays the current status of prenaming, and allows user to enable or disable it."
 
     def check_duplicates(self):
-        tss=TestSourceService()
+        tss = TestSourceService()
         duplicates = tss.check_pdf_duplication()
         if duplicates:
-            self.stderr.write(f"There appear to be duplicate source pdfs on the server:")
-            for sha,versions in duplicates.items():
+            self.stderr.write(
+                f"There appear to be duplicate source pdfs on the server:"
+            )
+            for sha, versions in duplicates.items():
                 self.stderr.write(f"\tVersions {versions} have sha256 {sha}")
-                
 
     def show_status(self):
         tss = TestSourceService()
@@ -66,19 +67,21 @@ class Command(BaseCommand):
         if version in up_list:
             self.copy_source_into_place(version, tss.get_source_as_bytes(version))
         else:
-            self.stdout.write(
+            self.stderr.write(
                 f"Test pdf source Version {version} is not on the server."
             )
 
     def remove_source(self, version=None, all=False):
         tss = TestSourceService()
         up_list = tss.get_list_of_uploaded_sources()
-        if len(up_list)==0:
+        if len(up_list) == 0:
             self.stdout.write(f"There are no test sources on the server.")
             return
-            
+
         if all:
-            self.stdout.write(f"Removing all {len(up_list)} test source pdfs on server.")
+            self.stdout.write(
+                f"Removing all {len(up_list)} test source pdfs on server."
+            )
             tss.delete_all_test_sources()
             return
         if version in up_list:
@@ -87,21 +90,31 @@ class Command(BaseCommand):
                 f"Removing test pdf source version {version} from server."
             )
         else:
-            self.stdout.write(
+            self.stderr.write(
                 f"Test pdf source Version {version} is not on the server."
             )
 
     def upload_source(self, version=None, source_pdf=None):
+        if not temp_functions.is_there_a_valid_spec():
+            self.stderr.write(
+                f"There is not a valid test specification on the server. Cannot upload."
+            )
+            return
+
         tss = TestSourceService()
         src_list = tss.get_list_of_sources()
         if version not in src_list:
             version_list = sorted(list(src_list.keys()))
-            self.stderr.write(f"Version {version} is invalid - must be one of {version_list}")
+            self.stderr.write(
+                f"Version {version} is invalid - must be one of {version_list}"
+            )
             return
-        
+
         existing_src = src_list[version]
         if existing_src is not None:
-            self.stderr.write(f"Version {version} already on server with sha256 = {existing_src[1]}. Delete or upload to a different version.")
+            self.stderr.write(
+                f"Version {version} already on server with sha256 = {existing_src[1]}. Delete or upload to a different version."
+            )
             return
 
         source_path = Path(source_pdf)
@@ -111,8 +124,19 @@ class Command(BaseCommand):
         # send the PDF
         # TODO - fix 6 to get the required number of pages from the spec.
         # we should not be able to upload unless we have a spec
-        with open(source_path, 'rb') as fh:
-            tss.take_source_from_upload(version, 6, fh)
+        with open(source_path, "rb") as fh:
+            success, msg = tss.take_source_from_upload(
+                version, temp_functions.how_many_test_pages(), fh
+            )
+            if success:
+                self.stdout.write(
+                    f"Upload of source pdf for version {version} succeeded."
+                )
+            else:
+                self.stderr.write(
+                    f"Upload of source pdf for version {version} failed = {msg}"
+                )
+                return
 
         # check for any duplicates
         self.check_duplicates()
@@ -128,7 +152,9 @@ class Command(BaseCommand):
         sp_R = sub.add_parser("remove", help="Remove a test source pdf")
 
         sp_U.add_argument("source_pdf", type=str, help="The source pdf to upload")
-        sp_U.add_argument("-v", "--version", type=int, help="The version to upload", required=True)
+        sp_U.add_argument(
+            "-v", "--version", type=int, help="The version to upload", required=True
+        )
 
         grp_D = sp_D.add_mutually_exclusive_group(required=True)
         grp_D.add_argument("-v", "--version", type=int, help="The version to download")
@@ -146,7 +172,9 @@ class Command(BaseCommand):
         if options["command"] == "status":
             self.show_status()
         elif options["command"] == "upload":
-            self.upload_source(version=options["version"], source_pdf=options["source_pdf"])
+            self.upload_source(
+                version=options["version"], source_pdf=options["source_pdf"]
+            )
         elif options["command"] == "download":
             self.download_source(version=options["version"], all=options["all"])
         elif options["command"] == "remove":
