c7640b9be5aea611e0d9adc7a84fa0ab663c43f4
brennenchiu <bchiyeung1996@gmail.com>
2022-08-16 11:44:02 -0700

    added doc string and cleaned up code a bit

--- django/Authentication/management/commands/plom_create_demo_users.py
+++ django/Authentication/management/commands/plom_create_demo_users.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User, Group
 from django.db import IntegrityError
-# pip install tabulate
 from tabulate import tabulate
 
 
--- django/Authentication/management/commands/plom_delete_demo_users.py
+++ django/Authentication/management/commands/plom_delete_demo_users.py
@@ -1,8 +1,12 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
+    """
+    This is the command for "python manage.py plom_delete_demo_users.py
+    It deletes all the users within demo group.
+    """
     def handle(self, *args, **options):
         for demo_user in User.objects.filter(groups__name='demo'):
             demo_user.delete()
