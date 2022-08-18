e9b33f669afbfe3066f1517b37612dcb223f6a3d
Edith Coates <ecoates.bc@gmail.com>
2022-08-18 13:38:42 -0700

    Basic mocking functionality

--- django/Preparation/views/mocker.py
+++ django/Preparation/views/mocker.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
-from Preparation.services import ExamMockerService
+import shutil
+from django.http import FileResponse
+from django.core.files.uploadedfile import SimpleUploadedFile
+from Preparation.services import ExamMockerService, TestSourceService
+from TestCreator.services import TestSpecService
 
 from Preparation.views.needs_manager_view import ManagerRequiredBaseView
 
@@ -7,5 +10,12 @@ class MockExamView(ManagerRequiredBaseView):
     """Create a mock test PDF"""
     def post(self, request, version):
         mocker = ExamMockerService()
-        mocked = mocker.mock_exam(version, 'lalalala')
-        return HttpResponse(mocked)
+        tss = TestSourceService()
+        spec = TestSpecService()
+        source_path = tss.get_source_pdf_path(version)
+
+        n_pages = spec.get_n_pages()
+        pdf_path = mocker.mock_exam(version, source_path, n_pages, spec.get_short_name_slug())
+        pdf_doc = SimpleUploadedFile(pdf_path.name, pdf_path.open('rb').read(), content_type='application/pdf')
+        shutil.rmtree(pdf_path.parent)
+        return FileResponse(pdf_doc)
