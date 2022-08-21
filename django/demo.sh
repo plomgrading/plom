02ea8c77a3859aab727c16d0ccd9c6aa1a192f5f
Colin B. Macdonald <cbm@m.fsf.org>
2022-08-20 20:10:54 -0700

    Colin's demo script

---
+++ django/demo.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+rm -f db.sqlite3
+
+#python3 manage.py makemigrations TestCreator Preparation
+#python3 manage.py makemigrations Connect
+#python3 manage.py makemigrations
+python3 manage.py migrate
+
+# new thing to try #90
+#python3 manage.py reset_migrations Authentication Preparation TestCreator
+
+
+# old way, have to type password every time
+#python3 manage.py createsuperuser --username cbm  --email foo@bar.com
+
+python3 manage.py plom_create_groups
+#python3 manage.py creategroups
+
+python3 manage.py plom_create_demo_users
+
+python3 manage.py runserver
