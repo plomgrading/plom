cfb9d7cfaa2ef8218f245ff0e8f74f099d98c2ff
Edith Coates <ecoates.bc@gmail.com>
2022-08-25 11:29:20 -0700

    simple build paper function

--- django/BuildPaperPDF/services/build_papers.py
+++ django/BuildPaperPDF/services/build_papers.py
@@ -1,6 +1,14 @@
 from plom.create.buildDatabaseAndPapers import build_papers
 
+from django.conf import settings
+
+
 class BuildPapersService:
     """Use Core Plom to build test-papers."""
-    def build_single_paper():
-        pass
+    
+    def build_single_paper(messenger):
+        build_papers(
+            basedir=settings.BASE_DIR,
+            indexToMake=1,
+            msgr=messenger
+        )
