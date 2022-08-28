fc25cc6a466c253ce7c7160c1bc2c0ab49b3d8e8
Colin B. Macdonald <cbm@m.fsf.org>
2022-08-28 12:08:53 -0700

    plom-create status: improve section header underlines

--- plom/create/status.py
+++ plom/create/status.py
@@ -119,7 +119,7 @@ def status(*, msgr):
         print("    " + "\n    ".join(wrap(format_int_list_with_runs(not_named), 72)))
 
     print("\nDatabase")
-    print("---------\n")
+    print("--------\n")
     vmap = msgr.getGlobalQuestionVersionMap()
     if len(vmap) > 0:
         print(check_mark + f" There are {len(vmap)} rows in the papers table")
@@ -130,7 +130,7 @@ def status(*, msgr):
         print(warn_mark + " No rows have been inserted in the papers table")
 
     print("\nRubrics")
-    print("---------\n")
+    print("-------\n")
     rubs = msgr.MgetRubrics()
     if len(rubs) == 0:
         print("Server does not yet have any rubrics")
