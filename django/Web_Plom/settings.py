e0a0086f149f1a44ca3210ade853e1fc1f2491dc
brennenchiu <bchiyeung1996@gmail.com>
2022-09-06 15:16:21 -0700

    set allowed_hosts

--- django/Web_Plom/settings.py
+++ django/Web_Plom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2ujgq&p27afoi(#3%^98vj2(274ic+j2rxemflb#z3z9x6z=rn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
