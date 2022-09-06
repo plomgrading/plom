c6c374417400b3173d1a1cc9145a64ececb090ba
brennenchiu <bchiyeung1996@gmail.com>
2022-09-06 14:17:12 -0700

    debug = False

--- django/Web_Plom/settings.py
+++ django/Web_Plom/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2ujgq&p27afoi(#3%^98vj2(274ic+j2rxemflb#z3z9x6z=rn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
