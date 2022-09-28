88db58fe57281c766c44ab79e72968ddec56d7ec
Edith Coates <ecoates.bc@gmail.com>
2022-09-28 15:17:59 -0700

    basic bundle upload validation

---
+++ django/Scan/forms.py
@@ -0,0 +1,57 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022 Edith Coates
+
+import pathlib
+import hashlib
+import fitz
+from fitz import FileDataError
+from datetime import datetime
+
+from django import forms
+from django.forms import ValidationError
+from django.utils.text import slugify
+
+
+class BundleUploadForm(forms.Form):
+    """
+    Upload a bundle PDF
+    """
+
+    pdf = forms.FileField(
+        allow_empty_file=False,
+        max_length=100,
+        label="",
+        widget=forms.FileInput(attrs={"accept": "application/pdf"}),
+    )
+
+    def clean(self):
+        data = self.cleaned_data
+        pdf = data['pdf']
+
+        # size less than 5 MB?
+        if pdf.size >= 5e+6:
+            raise ValidationError("File size must be less than 5 MB.")
+
+        # correct format, readable by fitz, not a duplicate?
+        try:
+            file_bytes = pdf.read()
+            pdf_doc = fitz.open(stream=file_bytes)
+            if "PDF" not in pdf_doc.metadata["format"]:
+                raise ValidationError("File is not a valid PDF.")
+
+            # TODO: Should we prevent uploading duplicate bundles or warn them?
+            hashed = hashlib.sha256(file_bytes).hexdigest()
+
+            # get slug
+            filename_stem = pathlib.Path(str(pdf)).stem
+            slug = slugify(filename_stem)
+
+            data.update({
+                "pdf_doc": pdf_doc,
+                "slug": slug,
+                "time_uploaded": datetime.now(),
+                "sha256": hashed,
+            })
+            return data
+        except (FileDataError, KeyError):
+            raise ValidationError("Unable to open file.")
--- django/Scan/views.py
+++ django/Scan/views.py
@@ -1,9 +1,11 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2022 Edith Coates
 
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from Base.base_group_views import ScannerRequiredView
+from Scan.forms import BundleUploadForm
 
 
 class ScannerHomeView(ScannerRequiredView):
@@ -11,5 +13,23 @@ class ScannerHomeView(ScannerRequiredView):
     Hello, world!
     """
 
+    def build_context(self):
+        context = super().build_context()
+        context.update({
+            "form": BundleUploadForm(),
+        })
+        return context
+
     def get(self, request):
-        return HttpResponse("Scan")
+        context = self.build_context()
+        return render(request, "Scan/home.html", context)
+
+    def post(self, request):
+        context = self.build_context()
+        form = BundleUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('.')
+        else:
+            context.update({"form": form})
+            return render(request, "Scan/home.html", context)
