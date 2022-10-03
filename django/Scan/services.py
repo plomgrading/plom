a5c2f8296e3282208e7bb460af7d7ead1c5c2732
Edith Coates <ecoates.bc@gmail.com>
2022-10-03 11:41:00 -0700

    split bundle into page images

--- django/Scan/services.py
+++ django/Scan/services.py
@@ -1,9 +1,10 @@
 import pathlib
+import hashlib
 from datetime import datetime
 from django.db import transaction
 from django.conf import settings
 
-from Scan.models import StagingBundle
+from Scan.models import StagingBundle, StagingImage
 
 
 class ScanService:
@@ -14,7 +15,8 @@ class ScanService:
     @transaction.atomic
     def upload_bundle(self, pdf_doc, slug, user, time_uploaded, pdf_hash):
         """
-        Upload a bundle PDF and store it in the filesystem + database
+        Upload a bundle PDF and store it in the filesystem + database.
+        Also, split PDF into page images + store in filesystem and database.
         """
         timestamp = datetime.timestamp(time_uploaded)
         file_name = f"{slug}_{timestamp}.pdf"
@@ -34,6 +36,41 @@ class ScanService:
             pdf_hash=pdf_hash,
         )
         bundle_db.save()
+
+        image_dir = user_dir / "images"
+        image_dir.mkdir(exist_ok=True)
+        slug_dir = image_dir / f"{slug}_{timestamp}"
+        slug_dir.mkdir(exist_ok=True)
+        self.split_and_save_bundle_images(pdf_doc, bundle_db, slug_dir)
+
+    @transaction.atomic
+    def split_and_save_bundle_images(self, pdf_doc, bundle, save_path):
+        """
+        Read a PDF document and save page images to filesystem/database
+
+        Args:
+            pdf_doc: fitz.document object of a bundle
+            bundle: StagingBundle object
+            save_path: pathlib.Path object of path to save image files
+        """
+        n_pages = pdf_doc.page_count
+        timestamp = datetime.timestamp(bundle.time_uploaded)
+        for i in range(n_pages):
+            filename = f"{bundle.slug}_{timestamp}_{i}.png"
+            pixmap = pdf_doc.get_page_pixmap(i)
+            pixmap.save(save_path / filename)
+
+            with open(save_path / filename, 'rb') as f:
+                image_hash = hashlib.sha256(f.read()).hexdigest()
+
+            image_db = StagingImage(
+                bundle=bundle,
+                bundle_order=i,
+                file_name=filename,
+                file_path=str(save_path / filename),
+                image_hash=image_hash,
+            )
+            image_db.save()
 
     @transaction.atomic
     def remove_bundle(self, slug, timestamp, user):
