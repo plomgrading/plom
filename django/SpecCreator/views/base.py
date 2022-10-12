269805d027dd5a3627b5ad73522ed30908335f59
Edith Coates <ecoates.bc@gmail.com>
2022-10-12 12:50:19 -0700

    Disable some editing after Papers database initialised

--- django/SpecCreator/views/base.py
+++ django/SpecCreator/views/base.py
@@ -1,9 +1,23 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from Base.base_group_views import ManagerRequiredView
+from Papers.services import PaperInfoService
 
 from SpecCreator.services import StagingSpecificationService, ReferencePDFService
 
 
 class TestSpecPageView(ManagerRequiredView):
+    def dispatch(self, request):
+        """
+        Redirect to the assessment preparation page if the Papers database
+        is already populated
+        """
+        paper_info = PaperInfoService()
+        if paper_info.is_paper_database_populated():
+            return HttpResponseRedirect(reverse("prep_landing"))
+        return super().dispatch()
+
     def build_context(self, page_name):
         context = super().build_context()
         spec = StagingSpecificationService()
