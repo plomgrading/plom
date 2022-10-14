a1e238274af5bf0ec49ce458acf536c049999c5f
brennenchiu <bchiyeung1996@gmail.com>
2022-10-14 10:25:59 -0700

    added Copyright

--- django/Scan/services.py
+++ django/Scan/services.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022 Edith Coates
+# Copyright (C) 2022 Brennen Chiu
+
 from operator import index
 import pathlib
 import hashlib
--- django/Scan/views.py
+++ django/Scan/views.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2022 Edith Coates
+# Copyright (C) 2022 Brennen Chiu
 
 import pathlib
 from datetime import datetime
