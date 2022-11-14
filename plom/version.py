b6ab681defc1457d01220f2b7740bc778ce3335b
Colin B. Macdonald <cbm@m.fsf.org>
2022-11-13 21:36:05 -0800

    Post-release version bump

--- plom/version.py
+++ plom/version.py
@@ -2,5 +2,5 @@
 # Copyright (C) 2020-2022 Colin B. Macdonald
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-__version__ = "0.11.1"
+__version__ = "0.11.2.dev"
 # Also hardcoded in AppImageBuilder.yml
