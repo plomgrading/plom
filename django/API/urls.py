4943b94a2cbb698c20c6ecb42db440a765b1cd2c
Colin B. Macdonald <cbm@m.fsf.org>
2022-11-15 20:43:46 -0800

    Blacken

--- django/API/urls.py
+++ django/API/urls.py
@@ -19,7 +19,9 @@ urlpatterns = [
     path("info/spec/", GetSpecification.as_view(), name="api_info_spec"),
     path("Version/", ServerVersion.as_view(), name="api_server_version"),
     path("MK/maxMark/", QuestionMaxMark.as_view(), name="api_question_mark"),
-    path("maxmark/<int:question>", QuestionMaxMark2.as_view(), name="api_question_mark2"),
+    path(
+        "maxmark/<int:question>", QuestionMaxMark2.as_view(), name="api_question_mark2"
+    ),
     path("ID/classlist/", GetClasslist.as_view(), name="api_get_classlist"),
     path("ID/predictions/", GetIDPredictions.as_view(), name="api_get_predictions"),
 ]
