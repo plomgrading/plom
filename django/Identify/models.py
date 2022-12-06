c6682ea6d7a7d795478986de9c36e9246d007bd1
Edith Coates <ecoates.bc@gmail.com>
2022-12-06 10:56:38 -0800

    new ID task models

---
+++ django/Identify/models.py
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022 Edith Coates
+
+from django.db import models
+
+from Base.models import BaseTask, BaseAction
+from Papers.models import Paper
+
+
+class PaperIDTask(BaseTask):
+    """
+    Represents a test-paper that needs to be identified.
+    """
+
+    paper = models.OneToOneField(Paper, on_delete=models.CASCADE)
+
+
+class PaperIDAction(BaseAction):
+    """
+    Represents an identification of a test-paper.
+    """
+
+    student_name = models.TextField(default="")
+    student_id = models.TextField(default="")
