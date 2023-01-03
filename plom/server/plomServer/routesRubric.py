af5677a1db86b54a5f29cde4ae0f8557bdd92770
Colin B. Macdonald <cbm@m.fsf.org>
2023-01-03 15:31:44 -0800

    Update copyright

--- plom/server/plomServer/routesRubric.py
+++ plom/server/plomServer/routesRubric.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2019-2021 Andrew Rechnitzer
-# Copyright (C) 2020-2021 Colin B. Macdonald
+# Copyright (C) 2020-2023 Colin B. Macdonald
 # Copyright (C) 2020 Vala Vakilian
 
 from aiohttp import web
