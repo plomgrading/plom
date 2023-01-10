feb46a88669768d5a9e8b3b9fa00120df4033c7f
Colin B. Macdonald <cbm@m.fsf.org>
2023-01-10 13:19:53 -0800

    CI: job to check copyright header updates

---
+++ contrib/is_copyright_uptodate.py
@@ -0,0 +1,45 @@
+#!/bin/env python3
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022-2023 Colin B. Macdonald
+
+"""Output the names of files given as args that are not copyright in a given year.
+
+Gives non-zero exit code if there were any that need updates.
+
+Also, snitches on files without apparent copyright headers, although
+this is FYI only (does not effect exit code).
+"""
+
+import re
+import sys
+from datetime import datetime
+
+year = datetime.utcnow().year
+p = re.compile(f'.*Copyright.*{year}.*')
+p2 = re.compile('.*Copyright.*')
+
+if __name__ == "__main__":
+    at_least_one = False
+    files = set(sys.argv[1:])
+    print(f"Checking copyright headers for {year} in {len(files)} files...")
+    for f in files:
+        try:
+            with open(f, 'r') as fh:
+                data = fh.read().replace('\n', '')
+        except UnicodeDecodeError:
+            print(f"    Skipping binary (?) file: {f}")
+            continue
+        except FileNotFoundError:
+            print(f"    Skipping deleted (?) file: {f}")
+            continue
+        if not p2.match(data):
+            print(f"[!] Skipping file without copyright header: {f}")
+            continue
+        if not p.match(data):
+            at_least_one = True
+            print(f"[F] Needs copyright header update: {f}")
+    if at_least_one:
+        print("At least one file needs updated, see list above")
+        sys.exit(1)
+    print("No files need updates")
+    sys.exit(0)
