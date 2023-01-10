a2b8623942078e695e7fefc107c37c35d93798b6
Colin B. Macdonald <cbm@m.fsf.org>
2023-01-10 14:02:12 -0800

    copyright update from 2022

--- plom/server/test_configs.py
+++ plom/server/test_configs.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2021-2022 Colin B. Macdonald
+# Copyright (C) 2021-2023 Colin B. Macdonald
+# Copyright (C) 2022 Andrew Rechnitzer
 
 from pathlib import Path
 from pytest import raises
