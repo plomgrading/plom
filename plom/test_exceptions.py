8363f286fd016c6e94a7f1347f1349521bb35451
Colin B. Macdonald <cbm@m.fsf.org>
2023-01-23 19:11:02 -0800

    copyright updates

--- plom/test_exceptions.py
+++ plom/test_exceptions.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2020-2021 Colin B. Macdonald
+# Copyright (C) 2020-2021, 2023 Colin B. Macdonald
 
 import plom.plom_exceptions
 from plom.plom_exceptions import PlomException, PlomAuthenticationException
