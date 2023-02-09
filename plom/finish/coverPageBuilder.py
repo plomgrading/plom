47ad280458b3eef494c6b6575fa01bd47cbfc6c6
tamnguyen135 <tamnguyen135@hotmail.com>
2023-02-09 12:01:42 -0800

    Changed rendering of coverpage in coverPageBuilder.py using fitz instead of the css/html to pdf library

--- plom/finish/coverPageBuilder.py
+++ plom/finish/coverPageBuilder.py
@@ -4,9 +4,12 @@
 # Copyright (C) 2019-2020, 2022 Colin B. Macdonald
 # Copyright (C) 2020 Dryden Wiebe
 # Copyright (C) 2021 Liam Yih
+# Copyright (C) 2023 Tam Nguyen
 
 from plom.misc_utils import local_now_to_simple_string
-
+import fitz
+import logging
+logging.basicConfig(level = logging.DEBUG)
 
 def makeCover(test_num, sname, sid, tab, pdfname, solution=False):
     """Create html page of name ID etc and table of marks.
@@ -20,84 +23,49 @@ def makeCover(test_num, sname, sid, tab, pdfname, solution=False):
         solution (bool): whether or not this is a cover page for solutions
     """
     # hide imports until needed Issue #2231.
-    from weasyprint import HTML, CSS
+    cover = fitz.open()
 
-    # A simple CSS header to style the cover page nicely.
-    css = CSS(
-        string="""
-@page {
-  size: Letter; /* Change from the default size of A4 */
-  margin: 2.5cm; /* Set margin on each page */
-}
-body {
-    font-family: sans serif;
-}
-table, th, td {
-    border: 1px solid black;
-    border-collapse: collapse;
-    padding: 5px;
-    text-align: center;
-}
-"""
-    )
+    hdisp = fitz.Rect(75, 0, 75, 0)
+    vdisp = fitz.Rect(0, 50, 0, 50)
+    align = 1 #centre 
+    fontsize = 14
 
-    htmlText = "<html><body>\n"
-    if solution:
-        htmlText += "<h3>Solutions</h3>\n"
-    else:
-        htmlText += "<h3>Results</h3>\n"
-    htmlText += """
-<ul>
-  <li>Name = {}</li>
-  <li>ID = {}</li>
-  <li>Test number = {}</li>
-</ul>
-<table>""".format(
-        sname, sid, test_num
-    )
-    if solution:
-        htmlText += "<tr><th>question</th><th>version</th><th>mark out of</th></tr>\n"
-    else:
-        htmlText += (
-            "<tr><th>question</th><th>version</th><th>mark</th><th>out of</th></tr>\n"
-        )
-    # Start computing total mark.
-    totalMark = 0
-    maxPossible = 0
-    for y in tab:
-        totalMark += y[2] if y[2] is not None else 0
-        maxPossible += y[3]
-        # Row of mark table with Group, Version, Mark, MaxPossibleMark
-        g, v, m, x = y
+    page = cover.new_page()
+    p = fitz.Point(50, 75)  # start point of 1st line
+    text = "Results: \n \u2022 Name = {} \n \u2022 ID = {} \n \u2022 Test number = {}".format(sname, sid, test_num)
+    page.insert_text(p, text,  fontname = "helv", fontsize = fontsize)
 
-        if solution:  # ignore 'Mark'
-            htmlText += f"<tr><td>{g}</td><td>{v}</td><td>{x}</td></tr>\n"
-        else:
-            htmlText += f"<tr><td>{g}</td><td>{v}</td><td>{m}</td><td>{x}</td></tr>\n"
+    shape = page.new_shape()
+    # Drawing the header with question, version, mark, outof
+    r = [fitz.Rect(50, 200, 125, 250) + hdisp * j for j in range(0, 4)]
+    t = ["question", "version", "mark", "out of"]    
+    for j in range(0, 4):
+        shape.draw_rect(r[j])
+        shape.insert_textbox(r[j], t[j], align = align, fontsize = fontsize)
 
-    # Final total mark out of maxPossible total mark.
-    if solution:
-        htmlText += f"<tr><td>total</td><td>&middot;</td><td>{maxPossible}</td>\n"
-    else:
-        htmlText += (
-            "<tr><td>total</td><td>&middot;</td><td>{}</td><td>{}</td>\n".format(
-                totalMark, maxPossible
-            )
-        )
-    htmlText += """
-    </table>
-    <footer style="position:absolute; bottom:0;">
-    """
-    htmlText += """
-    Coverpage produced on {}
-    </ul>
-    </footer>
-    """.format(
-        local_now_to_simple_string()
-    )
-    htmlText += """
-</body>
-</html>"""
-    # Pipe the htmltext into weasyprint.
-    cover = HTML(string=htmlText)
-    cover.write_pdf(pdfname, stylesheets=[css])
+    # Drawing the tab
+    for i in range(0, len(tab)):
+        r = [r[j] + vdisp for j in range(0,4)]
+        for j in range(0, 4):
+            shape.draw_rect(r[j])
+            shape.insert_textbox(r[j], str(tab[i][j]), align = align, fontsize = fontsize)
+
+    # Drawing the rest
+    r = [r[j] + vdisp for j in range(0,4)]
+    t = ["total", ".", sum([tab[i][2] for i in range(0, len(tab))]), sum([tab[i][3] for i in range(0, len(tab))])]
+    for j in range(0, 4):
+        shape.draw_rect(r[j])
+        shape.insert_textbox(r[j], str(t[j]), align = 1, fontsize = fontsize) 
+
+    shape.finish(width = 0.3, color = (0, 0, 0))
+    shape.commit()
+    
+    # Last words
+    text = "Cover page produced on {}".format(local_now_to_simple_string())
+    p = fitz.Point(50, page.rect.height - 50)
+    page.insert_text(p, text,  fontname = "helv", fontsize = fontsize)
+
+    cover.save(pdfname)
+    cover.close()
+
+    
