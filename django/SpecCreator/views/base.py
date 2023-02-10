87de9452267b4602d4db0bfc9ef3eda94fa378e7
Andrew Rechnitzer <arechnitzer@gmail.com>
2023-02-09 16:16:42 -0800

    Fix for #2518 and a demo that creates users but nothing else

--- django/SpecCreator/views/base.py
+++ django/SpecCreator/views/base.py
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2022 Edith Coates
+# Copyright (C) 2023 Andrew Rechnitzer
 
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -27,7 +30,11 @@ class TestSpecPageView(ManagerRequiredView):
         valid_spec = SpecificationService()
 
         show_alert = False
-        valid = valid_spec.get_the_spec()
+        try:
+            valid = valid_spec.get_the_spec()
+        except ObjectDoesNotExist:
+            valid = {}
+
         if valid and not spec.compare_spec(valid):
             show_alert = True
 
