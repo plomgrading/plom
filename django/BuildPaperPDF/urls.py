e1bba0e7b90b561bbe694026671cda706c84437b
Colin B. Macdonald <cbm@m.fsf.org>
2023-02-16 22:45:41 -0800

    blacken

--- django/BuildPaperPDF/urls.py
+++ django/BuildPaperPDF/urls.py
@@ -20,7 +20,9 @@ urlpatterns = [
     path("", BuildPaperPDFs.as_view(), name="create_paperPDFs"),
     path("update/", UpdatePDFTable.as_view(), name="update_paperPDFs"),
     path("get/<int:paper_number>", GetPDFFile.as_view(), name="get_paperPDFs"),
-    path("get_zip_stream/", GetStreamingZipOfPDFs.as_view(), name="zip_stream_paperPDFs"),
+    path(
+        "get_zip_stream/", GetStreamingZipOfPDFs.as_view(), name="zip_stream_paperPDFs"
+    ),
     path("start/all/", StartAllPDFs.as_view(), name="start_all_PDFs"),
     path("start/<int:paper_number>", StartOnePDF.as_view(), name="start_one_PDF"),
     path("cancel/all", CancelAllPDf.as_view(), name="cancel_all_PDFs"),
