ab03202d7d90521098a9be8820db9a85c1e1e71c
Andrew Rechnitzer <andrewr@math.ubc.ca>
2023-02-16 16:10:14 -0800

    Remove old import from url

--- django/BuildPaperPDF/urls.py
+++ django/BuildPaperPDF/urls.py
@@ -6,7 +6,6 @@ from django.urls import path
 from .views import (
     BuildPaperPDFs,
     GetPDFFile,
-    GetCompressedPDFs,
     GetStreamingZipOfPDFs,
     UpdatePDFTable,
     StartAllPDFs,
@@ -21,7 +20,6 @@ urlpatterns = [
     path("", BuildPaperPDFs.as_view(), name="create_paperPDFs"),
     path("update/", UpdatePDFTable.as_view(), name="update_paperPDFs"),
     path("get/<int:paper_number>", GetPDFFile.as_view(), name="get_paperPDFs"),
-    path("get_zip/", GetCompressedPDFs.as_view(), name="zip_paperPDFs"),
     path("get_zip_stream/", GetStreamingZipOfPDFs.as_view(), name="zip_stream_paperPDFs"),
     path("start/all/", StartAllPDFs.as_view(), name="start_all_PDFs"),
     path("start/<int:paper_number>", StartOnePDF.as_view(), name="start_one_PDF"),
