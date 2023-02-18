08ecb44a2b2e466ea2dfd922004415995c245f23
Colin B. Macdonald <cbm@m.fsf.org>
2023-02-17 23:47:00 -0800

    Both client and server need toml

--- setup.py
+++ setup.py
@@ -48,7 +48,6 @@ server_install_requires = [
     "canvasapi>=2.0.0",
     "exif>=1.2.2",
     "fonttools>=4.37.1",
-    "toml>=0.10.0",
     "tqdm",
     "numpy>=1.17.0",
     "pandas>=1.0.0",
@@ -70,6 +69,8 @@ server_install_requires = [
     "packaging",
     'importlib_resources>=5.0.0 ; python_version<"3.9"',  # until we drop 3.8
     "stdiomask>=0.0.6",
+    'tomli>=2.0.1 ; python_version<"3.11"',  # until we drop 3.10
+    "tomlkit>=0.11.4",
     "zipfly",
     "zxing-cpp>=1.4.0",
 ]
