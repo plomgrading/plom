b0014e9168e80161c5a355fc9295abc31af0f869
Colin B. Macdonald <cbm@m.fsf.org>
2023-03-04 13:26:27 -0800

    rotation: jpeg colour test

--- plom/scan/test_rotations.py
+++ plom/scan/test_rotations.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2023 Andrew Rechnitzer
 # Copyright (C) 2023 Natalie Balashov
 
+from math import sqrt
 from pathlib import Path
 import sys
 
@@ -21,6 +22,13 @@ from plom.finish.examReassembler import rot_angle_from_jpeg_exif_tag
 
 def relative_error(x, y):
     return abs(x - y) / abs(x)
+
+
+def relative_error_vec(x, y):
+    assert len(x) == len(y)
+    err2norm = sqrt(sum([(x[i] - y[i]) ** 2 for i in range(len(x))]))
+    x2norm = sqrt(sum([x[i] ** 2 for i in range(len(x))]))
+    return err2norm / x2norm
 
 
 def test_rotate_png(tmpdir):
@@ -59,3 +67,33 @@ def pil_load_with_jpeg_exif_rot_applied(f):
     r = rot_angle_from_jpeg_exif_tag(f)
     im = im.rotate(r, expand=True)
     return im
+
+
+def test_rotate_jpeg(tmpdir):
+    tmpdir = Path(tmpdir)
+    tmpdir = Path(".")
+    # make a lowish-quality jpeg
+    orig = tmpdir / "rgb.jpg"
+    with (resources.files(plom.scan) / "rgb.png") as fh:
+        im = Image.open(fh)
+        im.load()
+        im.save(orig, "JPEG", quality=2, optimize=True)
+    with open(orig, "rb") as fh:
+        b = fh.read()
+    # angle, and place where we expect a red pixel
+    for angle, redpixel in [
+        (0, (32, 0)),
+        (90, (63, 32)),
+        (-90, (0, 32)),
+        (270, (0, 32)),
+        (180, (32, 63)),
+    ]:
+        # make a copy
+        f = tmpdir / f"img{angle}.jpg"
+        with open(f, "wb") as fh:
+            fh.write(b)
+        rotate_bitmap(f, angle)
+        # now load it back and check for a red pixel in the right place
+        im = pil_load_with_jpeg_exif_rot_applied(f)
+        colour = im.getpixel(redpixel)
+        assert relative_error_vec(colour, (255, 0, 0)) <= 0.1
