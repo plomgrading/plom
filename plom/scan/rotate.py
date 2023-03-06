plomgrading/plom

commit 82412f6dc2916e11636e7114cb390c216b33151f

Separate out the image mucker, for demo/debugging
Fixes #2582.

plom/scan/rotate.py
@@ -81,15 +81,21 @@ def pil_load_with_jpeg_exif_rot_applied(f):
     """PIL's Image load does not apply exif orientation, so provide a helper that does.
 
     Args:
-        f (str/pathlib.Path): a path to a jpeg file.
+        f (str/pathlib.Path): a path to a file.
+
+    If the input is not a jpeg, we simplify open it with ``PIL`` and
+    return with no special processing.  If its a jpeg, we apply the exif
+    rotations, then return.
 
     Returns:
         PIL.Image: with exif orientation applied.
     """
+    f = Path(f)
     im = Image.open(f)
     im.load()
-    r = rot_angle_from_jpeg_exif_tag(f)
-    im = im.rotate(r, expand=True)
+    if f.suffix.casefold() in ("jpg", "jpeg"):
+        r = rot_angle_from_jpeg_exif_tag(f)
+        im = im.rotate(r, expand=True)
     return im
 
 

plom/scan/scansToImages.py
@@ -26,6 +26,7 @@ from plom import __version__
 from plom import PlomImageExts
 from plom import ScenePixelHeight
 from plom.scan.bundle_utils import make_bundle_dir
+from plom.scan.rotate import pil_load_with_jpeg_exif_rot_applied
 
 
 log = logging.getLogger("scan")
@@ -207,9 +208,13 @@ def processFileToBitmaps(
                 dest,
                 basename,
                 file_name,
-                debug_jpeg=debug_jpeg,
                 add_metadata=add_metadata,
             )
+            # For testing, randomly make jpegs, rotated a bit, of various qualities
+            if debug_jpeg and random.uniform(0, 1) <= 0.5:
+                outname = make_mucked_up_jpeg(
+                    outname, dest / ("muck-" + basename + ".jpg")
+                )
             files.append(outname)
         assert len(files) == len(doc), "Expected one image per page"
     return files
@@ -327,8 +332,6 @@ def render_page_to_bitmap(
             uniqifying pages, you can pass whatever you want.
 
     Keyword Args:
-        debug_jpeg (bool): make jpegs, randomly rotated of various
-            quality settings, for debugging or demos.  Default: False.
         add_metadata (bool): add invisible metadata to each image
             including bundle name and random numbers.  Default: True.
             If you disable this, you can get two identical images
@@ -391,35 +394,6 @@ def render_page_to_bitmap(
         warn(_m)
         log.warning(_m)
 
-    # For testing, randomly make jpegs, rotated a bit, of various qualities
-    if debug_jpeg and random.uniform(0, 1) <= 0.5:
-        outname = dest / (basename + ".jpg")
-        img = PIL.Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-        angle = random.choice([90.5, 180.4, -90.3, -88, -1])
-        msgs = [f"hard-rotate {angle}"]
-        img = img.rotate(
-            angle,
-            resample=PIL.Image.BILINEAR,
-            expand=True,
-            fillcolor=(128, 128, 128, 0),
-        )
-        quality = random.choice([6, 30, 94, 94, 94])
-        msgs.append(f"quality {quality}")
-        r = random.choice([None, None, None, 3, 6, 8])
-        if r:
-            msgs.append(f"exif rotate {r}")
-        log.info("  Randomly making jpeg " + ", ".join(msgs))
-        img.save(outname, "JPEG", quality=quality, optimize=True)
-        # We write some unique metadata into the JPEG exif data to avoid Issue #1573
-        im_shell = exif.Image(outname)
-        im_shell.set("user_comment", generate_metadata_str(bundle_name, p.number))
-        if r:
-            im_shell.set("orientation", r)
-        with open(outname, "wb") as f:
-            f.write(im_shell.get_file())
-        # add_metadata_jpeg_comment(outname, file_name, p.number)
-        return outname
-
     pngname = dest / (basename + ".png")
     jpgname = dest / (basename + ".jpg")
     if add_metadata:
@@ -446,6 +420,43 @@ def render_page_to_bitmap(
     jpgname.unlink()
     return pngname
     # WebP here is also an option, Issue #1864.
+
+
+def make_mucked_up_jpeg(f, outname):
+    """
+            debug_jpeg (bool): make jpegs, randomly rotated of various
+            quality settings, for debugging or demos.  Default: False.
+
+    Args:
+        f (pathlib.Path): input
+        out (pathlib.Path): output file
+    """
+    img = pil_load_with_jpeg_exif_rot_applied(f)
+
+    angle = random.choice([90.5, 180.4, -90.3, -88, -1])
+    msgs = [f"hard-rotate {angle}"]
+    img = img.rotate(
+        angle,
+        resample=PIL.Image.BILINEAR,
+        expand=True,
+        fillcolor=(128, 128, 128, 0),
+    )
+    quality = random.choice([6, 30, 94, 94, 94])
+    msgs.append(f"quality {quality}")
+    r = random.choice([None, None, None, 3, 6, 8])
+    if r:
+        msgs.append(f"exif rotate {r}")
+    log.info("  Randomly making jpeg " + ", ".join(msgs))
+    img.save(outname, "JPEG", quality=quality, optimize=True)
+    im_shell = exif.Image(outname)
+    # debugging so maybe we don't need unique JPEG exif metadata for Issue #1573
+    # im_shell.set("user_comment", generate_metadata_str(bundle_name, p.number))
+    if r:
+        im_shell.set("orientation", r)
+    with open(outname, "wb") as f:
+        f.write(im_shell.get_file())
+    # add_metadata_jpeg_comment(outname, file_name, p.number)
+    return outname
 
 
 def extractImageFromFitzPage(page, doc):

commit cfe8ff1cb96423fe02bd8120b0bb4d9dc20714bf

Fix broken pil_load_with_jpeg_exif_rot_applied
Broken by previous commit 82412f6dc2916e11636e7114cb390c216b33151f

plom/scan/rotate.py
@@ -93,7 +93,7 @@ def pil_load_with_jpeg_exif_rot_applied(f):
     f = Path(f)
     im = Image.open(f)
     im.load()
-    if f.suffix.casefold() in ("jpg", "jpeg"):
+    if f.suffix.casefold() in (".jpg", ".jpeg"):
         r = rot_angle_from_jpeg_exif_tag(f)
         im = im.rotate(r, expand=True)
     return im

plom/scan/test_rotations.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2021-2022 Colin B. Macdonald
+# Copyright (C) 2021-2023 Colin B. Macdonald
 # Copyright (C) 2023 Andrew Rechnitzer
 # Copyright (C) 2023 Natalie Balashov
 
@@ -14,7 +14,6 @@ else:
 
 from PIL import Image
 from PIL import ImageChops
-from pytest import raises
 
 import plom.scan
 from plom.scan import rotate_bitmap
@@ -187,7 +186,7 @@ def test_rotate_jpeg_lossless_cw(tmpdir):
         # minus sign b/c PIL does CCW
         im2 = im2.rotate(-angle, expand=True)
         diff = ImageChops.difference(im, im2)
-        # diff.save(f"diff{angle}.png")
+        diff.save(f"diff{angle}.png")
         assert not diff.getbbox()
 
 
@@ -215,7 +214,7 @@ def test_rotate_jpeg_lossless_ccw(tmpdir):
         im2.load()
         im2 = im2.rotate(angle, expand=True)
         diff = ImageChops.difference(im, im2)
-        # diff.save(f"diff{angle}.png")
+        diff.save(f"diff{angle}.png")
         assert not diff.getbbox()
 
 
@@ -280,5 +279,5 @@ def test_rotate_default_ccw(tmpdir):
         im1 = pil_load_with_jpeg_exif_rot_applied(f1)
         im2 = pil_load_with_jpeg_exif_rot_applied(f2)
         diff = ImageChops.difference(im1, im2)
-        # diff.save(f"diff{angle}.png")
+        diff.save(f"diff{angle}.png")
         assert not diff.getbbox()