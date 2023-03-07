1850382bf058b698bbf24efcc35bfe7249dfc803
Colin B. Macdonald <cbm@m.fsf.org>
2023-03-06 16:59:08 -0800

    Fix for too many collisions in demo
    Fixes #2594.

--- plom/scan/scansToImages.py
+++ plom/scan/scansToImages.py
@@ -212,9 +212,9 @@ def processFileToBitmaps(
             )
             # For testing, randomly make jpegs, rotated a bit, of various qualities
             if debug_jpeg and random.uniform(0, 1) <= 0.5:
-                outname = make_mucked_up_jpeg(
-                    outname, dest / ("muck-" + basename + ".jpg")
-                )
+                new = make_mucked_up_jpeg(outname, dest / ("muck-" + basename + ".jpg"))
+                outname.unlink()
+                outname = new
             files.append(outname)
         assert len(files) == len(doc), "Expected one image per page"
     return files
