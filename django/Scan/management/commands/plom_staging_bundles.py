04deb34ad4080fdf63d466518d800b1901784272
Colin B. Macdonald <cbm@m.fsf.org>
2023-03-10 00:06:48 -0800

    Missing raise

--- django/Scan/management/commands/plom_staging_bundles.py
+++ django/Scan/management/commands/plom_staging_bundles.py
@@ -116,7 +116,7 @@ class Command(BaseCommand):
                 f"Pushing {bundle_name} - processing it in the background now."
             )
         except ValueError as err:
-            CommandError(err)
+            raise CommandError(err)
 
     def read_bundle_qr(self, bundle_name):
         scanner = ScanService()
@@ -126,7 +126,7 @@ class Command(BaseCommand):
                 f"Reading {bundle_name} QR codes - processing it in the background now."
             )
         except ValueError as err:
-            CommandError(err)
+            raise CommandError(err)
 
     def add_arguments(self, parser):
         sp = parser.add_subparsers(
