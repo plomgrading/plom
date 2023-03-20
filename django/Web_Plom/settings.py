7a57677e9aa375324910aec3d1886ae59008508b
Colin B. Macdonald <cbm@m.fsf.org>
2023-03-20 16:23:18 -0700

    Clean up unnecessary imports and comments

--- django/Web_Plom/settings.py
+++ django/Web_Plom/settings.py
@@ -16,7 +16,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import sys
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
