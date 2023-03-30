9c9e817b1041664c277d827890710d339e98a033
Andrew Rechnitzer <andrewr@math.ubc.ca>
2023-03-30 16:24:04 -0700

    Copywriting

--- django/Progress/tests/test_manage_scan.py
+++ django/Progress/tests/test_manage_scan.py
@@ -1,3 +1,8 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022 Edith Coates
+# Copyright (C) 2022 Brennen Chiu
+# Copyright (C) 2023 Andrew Rechnitzer
+
 from django.test import TestCase
 from django.conf import settings
 from model_bakery import baker
@@ -5,7 +10,6 @@ from model_bakery import baker
 from Papers.models import (
     Image,
     CollidingImage,
-    FixedPage,
     DNMPage,
     DiscardedImage,
     Bundle,
