71c80e5fe820c939a940d938dcc942dfb53b7c0e
Colin B. Macdonald <cbm@m.fsf.org>
2023-04-12 12:02:11 -0700

    Use PYTHONPATH to access `plom` module from parent dir

---
+++ django/demo_path_hack.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022-2023 Colin B. Macdonald
+# Copyright (C) 2022 Edith Coates
+# Copyright (C) 2022 Brennen Chiu
+# Copyright (C) 2023 Andrew Rechnitzer
+
+set -e
+
+export PYTHONPATH=".."
+
+python3 -m demo
