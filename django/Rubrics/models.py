d45f3c7fda588148c50c916f277cb95d19c48238
brennenchiu <bchiyeung1996@gmail.com>
2023-04-17 11:51:54 -0700

    blacken

--- django/Rubrics/models.py
+++ django/Rubrics/models.py
@@ -58,7 +58,7 @@ class NeutralRubric(Rubric):
 
 class AbsoluteRurbic(Rubric):
     """
-    A rubric that shows the maximum possible amount of score. 
+    A rubric that shows the maximum possible amount of score.
     E.g. "1 of 5"
     """
 
