f259ec5dd3fcc5974efd5e2b0ab032159940b37d
Colin B. Macdonald <cbm@m.fsf.org>
2023-05-07 22:34:21 -0700

    One fewer deprecated imghdr imports
    See Issue #2706.

--- plom/client/identifier.py
+++ plom/client/identifier.py
@@ -11,7 +11,6 @@ __copyright__ = "Copyright (C) 2018-2023 Andrew Rechnitzer, Colin B. Macdonald, 
 __credits__ = "The Plom Project Developers"
 __license__ = "AGPL-3.0-or-later"
 
-import imghdr
 import logging
 from pathlib import Path
 import sys
