e38f5c148e01c551a576c1af07a9b07471aed372
Colin B. Macdonald <cbm@m.fsf.org>
2023-05-07 22:47:26 -0700

    Update copyright headers

--- plom/server/plomServer/routesUserInit.py
+++ plom/server/plomServer/routesUserInit.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2019-2020 Andrew Rechnitzer
-# Copyright (C) 2020-2022 Colin B. Macdonald
+# Copyright (C) 2020-2023 Colin B. Macdonald
 # Copyright (C) 2020 Vala Vakilian
 # Copyright (C) 2022 Edith Coates
 
