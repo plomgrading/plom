31a2590a8eb35be476da515054879dc6aef507ec
Colin B. Macdonald <cbm@m.fsf.org>
2023-05-08 07:43:54 -0700

    Partially real data

--- django/API/views/report.py
+++ django/API/views/report.py
@@ -5,9 +5,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from Mark.services import MarkingTaskService
+
 
 class REPspreadsheet(APIView):
     def get(self, request):
+        # TODO: q1v is question 1 version
+        # TODO: q1m is question 1 mark
         canned = {
             "2": {
                 "identified": True,
@@ -78,7 +82,8 @@ class REPidentified(APIView):
 
 class REPcompletionStatus(APIView):
     def get(self, request):
-        # TODO
+        # TODO: Identified, Marked, num_questions_marked
+        # TODO: what time-stamp?  last-modified?
         canned = {
             "11": [False, True, 2, "2023-05-03T07:48:13.739950+00:00"],
             "12": [True, True, 3, "2023-05-03T07:48:13.751636+00:00"],
@@ -93,12 +98,17 @@ class REPcompletionStatus(APIView):
 
 class REPcoverPageInfo(APIView):
     def get(self, request, papernum):
-        # TODO
-        canned = [
-            [["10130103", "Vandeventer, Irene"], [1, 1, 0], [2, 1, 1], [3, 2, 5]],
-            [["10152155", "Little, Abigail"], [1, 1, 0], [2, 1, 2], [3, 1, 5]],
-            [["10203891", "Coleman, Ashley"], [1, 2, 5], [2, 1, 5], [3, 1, 5]],
-        ]
-        canned = canned[papernum % len(canned)]
-        print(canned)
-        return Response(canned, status=status.HTTP_200_OK)
+        # Return value looks like this:
+        # [["10130103", "Vandeventer, Irene"], [1, 1, 0], [2, 1, 1], [3, 2, 5]]
+        service = MarkingTaskService()
+        r = []
+        # TODO: hardcoded
+        r.append(["10130103", "Vandeventer, Irene"])
+        # TODO: hardcoded for 3 questions
+        for question in range(1, 3 + 1):
+            annotation = service.get_latest_annotation(papernum, question)
+            # TODO: see MgetAnnotations: extra sanity checking?
+            # TODO: hard-coded version 1
+            ver = 1
+            r.append([question, ver, annotation.score])
+        return Response(r, status=status.HTTP_200_OK)
