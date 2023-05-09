4d7ec56dace1f5cf9ff01ff678980e8649cd810a
Edith Coates <ecoates.bc@gmail.com>
2023-05-09 13:22:22 -0700

    update docstring in API/urls.py

--- django/API/urls.py
+++ django/API/urls.py
@@ -16,6 +16,11 @@ from API.routes import (
 Handle URL patterns for the plom-client / server API.
 See docs for including other URLconfs:
 https://docs.djangoproject.com/en/4.2/topics/http/urls/#including-other-urlconfs
+
+Note: The URL Patterns classes are made in order to seamlessly split up urls.py
+across multiple files. In the future, once we're able to start changing the design
+of the plom-client URLS, we ought to transition to using Django REST Framework
+routers: https://www.django-rest-framework.org/api-guide/routers/
 """
 
 
