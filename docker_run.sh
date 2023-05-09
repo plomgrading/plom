a08b8b615d598a440db28fe259239b8f77ddb76e
Colin B. Macdonald <cbm@m.fsf.org>
2023-05-09 15:12:23 -0700

    CI: copyright updates from linter

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# SPDX-License-Identifier: FSFAP
+# Copyright (C) 2023 Edith Coates
+# Copyright (C) 2023 Colin B. Macdonald
 
 python3 -m demo --test
 python3 manage.py runserver 0.0.0.0:8000
