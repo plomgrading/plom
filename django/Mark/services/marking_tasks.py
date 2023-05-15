dfda28a78e09735b1352c9e4849cb24372e5754f
Edith Coates <ecoates.bc@gmail.com>
2023-05-15 10:24:14 -0700

    docstring linting in Mark/services/marking_tasks.py

--- django/Mark/services/marking_tasks.py
+++ django/Mark/services/marking_tasks.py
@@ -27,20 +27,16 @@ from Mark.models import (
 
 
 class MarkingTaskService:
-    """
-    Functions for creating and modifying marking tasks.
-    """
+    """Functions for creating and modifying marking tasks."""
 
     def create_task(self, paper, question_number, user=None):
-        """
-        Create a marking task.
+        """Create a marking task.
 
         Args:
             paper: a Paper instance, the test paper of the task
             question_number: int, the question of the task
             user: optional, User instance: user assigned to the task
         """
-
         pqvs = PQVMappingService()
         if not pqvs.is_there_a_pqv_map():
             raise RuntimeError("Server does not have a question-version map.")
@@ -67,10 +63,7 @@ class MarkingTaskService:
         return the_task
 
     def init_all_tasks(self):
-        """
-        Initialize all of the marking tasks for an entire exam, with null users.
-        """
-
+        """Initialize all of the marking tasks for an entire exam, with null users."""
         spec_service = SpecificationService()
         if not spec_service.is_there_a_spec():
             raise RuntimeError("The server does not have a spec.")
@@ -113,8 +106,7 @@ class MarkingTaskService:
         return (len(completed), len(total))
 
     def get_latest_task(self, paper_number, question_number):
-        """
-        Get a marking task from its paper number and question number.
+        """Get a marking task from its paper number and question number.
 
         Args:
             paper_number: int
@@ -128,14 +120,11 @@ class MarkingTaskService:
         )
 
     def unpack_code(self, code):
-        """
-        Return a tuple of (paper_number, question_number)
-        from a task code string.
+        """Return a tuple of (paper_number, question_number) from a task code string.
 
         Args:
             code (str): a task code, e.g. q0001g1
         """
-
         assert len(code) == len("q0000g0")
         paper_number = int(code[1:5])
         question_number = int(code[-1])
@@ -143,13 +132,11 @@ class MarkingTaskService:
         return paper_number, question_number
 
     def get_task_from_code(self, code):
-        """
-        Get a marking task from its code.
+        """Get a marking task from its code.
 
         Arg:
             code: str, a unique string that includes the paper number and question number.
         """
-
         try:
             paper_number, question_number = self.unpack_code(code)
             return self.get_latest_task(paper_number, question_number)
@@ -159,8 +146,7 @@ class MarkingTaskService:
             raise RuntimeError(f"Task {code} does not exist.")
 
     def get_user_tasks(self, user, question=None, version=None):
-        """
-        Get all the marking tasks that are assigned to this user.
+        """Get all the marking tasks that are assigned to this user.
 
         Args:
             user: User instance
@@ -170,7 +156,6 @@ class MarkingTaskService:
         Returns:
             QuerySet[MarkingTask]: tasks
         """
-
         tasks = MarkingTask.objects.filter(assigned_user=user)
         if question:
             tasks = tasks.filter(question_number=question)
@@ -180,8 +165,7 @@ class MarkingTaskService:
         return tasks
 
     def get_first_available_task(self, question=None, version=None):
-        """
-        Return the first marking task with a 'todo' status.
+        """Return the first marking task with a 'todo' status.
 
         Args:
             question (optional): int, requested question number
@@ -191,7 +175,6 @@ class MarkingTaskService:
             MarkingTask or None: The first available task, or None if
                 no such task exists.
         """
-
         available = MarkingTask.objects.filter(status=MarkingTask.TO_DO)
 
         if question:
@@ -208,22 +191,16 @@ class MarkingTaskService:
             return None
 
     def are_there_tasks(self):
-        """
-        Return True if there is at least one marking task in the database.
-        """
-
+        """Return True if there is at least one marking task in the database."""
         return MarkingTask.objects.exists()
 
     def assign_task_to_user(self, user, task):
-        """
-        Write a user to a marking task and update its status. Also creates
-        and saves a ClaimMarkingTask action instance.
+        """Write a user to a marking task and update its status. Also creates and saves a ClaimMarkingTask action instance.
 
         Args:
             user: reference to a User instance
             task: reference to a MarkingTask instance
         """
-
         if task.status == MarkingTask.OUT:
             raise RuntimeError("Task is currently assigned.")
 
@@ -232,27 +209,22 @@ class MarkingTaskService:
         task.save()
 
     def surrender_task(self, user, task):
-        """
-        Remove a user from a marking task, set its status to 'todo', and
-        save the action to the database.
+        """Remove a user from a marking task, set its status to 'todo', and save the action to the database.
 
         Args:
             user: reference to a User instance
             task: reference to a MarkingTask instance
         """
-
         task.assigned_user = None
         task.status = MarkingTask.TO_DO
         task.save()
 
     def surrender_all_tasks(self, user):
-        """
-        Surrender all of the tasks currently assigned to the user.
+        """Surrender all of the tasks currently assigned to the user.
 
         Args:
             user: reference to a User instance
         """
-
         user_tasks = MarkingTask.objects.filter(
             assigned_user=user, status=MarkingTask.OUT
         )
@@ -260,8 +232,7 @@ class MarkingTaskService:
             self.surrender_task(user, task)
 
     def user_can_update_task(self, user, code):
-        """
-        Return true if a user is allowed to update a certain task, false otherwise.
+        """Return true if a user is allowed to update a certain task, false otherwise.
 
         TODO: should be possible to remove the "assigned user" and "status" fields
         and infer both from querying ClaimTask and MarkAction instances.
@@ -270,7 +241,6 @@ class MarkingTaskService:
             user: reference to a User instance
             code: (str) task code
         """
-
         the_task = self.get_task_from_code(code)
         if the_task.assigned_user and the_task.assigned_user != user:
             return False
@@ -284,10 +254,7 @@ class MarkingTaskService:
         return True
 
     def mark_task(self, user, code, score, time, image, data):
-        """
-        Save a user's marking attempt to the database.
-        """
-
+        """Save a user's marking attempt to the database."""
         task = self.get_task_from_code(code)
         editions_so_far = Annotation.objects.filter(task=task).count()
         annotation = Annotation(
@@ -302,37 +269,26 @@ class MarkingTaskService:
         annotation.save()
 
     def get_n_marked_tasks(self):
-        """
-        Return the number of marking tasks that are completed.
-        """
-
+        """Return the number of marking tasks that are completed."""
         return len(MarkingTask.objects.filter(status=MarkingTask.COMPLETE))
 
     def get_n_total_tasks(self):
-        """
-        Return the total number of tasks in the database.
-        """
-
+        """Return the total number of tasks in the database."""
         return len(MarkingTask.objects.all())
 
     def mark_task_as_complete(self, code):
-        """
-        Set a task as complete - assuming a client has made a successful request.
-        """
-
+        """Set a task as complete - assuming a client has made a successful request."""
         task = self.get_task_from_code(code)
         task.status = MarkingTask.COMPLETE
         task.save()
 
     def save_annotation_image(self, md5sum, annot_img):
-        """
-        Save an annotation image to disk and the database.
+        """Save an annotation image to disk and the database.
 
         Args:
             md5sum: (str) the annotation image's hash.
             annot_img: (InMemoryUploadedFlie) the annotation image file.
         """
-
         imgtype = imghdr.what(None, h=annot_img.read())
         if imgtype not in ["png", "jpg", "jpeg"]:
             raise ValidationError(
@@ -360,8 +316,7 @@ class MarkingTaskService:
         return img
 
     def validate_and_clean_marking_data(self, user, code, data, plomfile):
-        """
-        Validate the incoming marking data.
+        """Validate the incoming marking data.
 
         Args:
             user: reference to a User instance.
@@ -376,7 +331,6 @@ class MarkingTaskService:
             `rubrics_used (list)`: a list of Rubric objects, extracted based on
             keys found inside the `annot_data`.
         """
-
         annot_data = json.loads(plomfile)
         cleaned_data = {}
 
@@ -419,8 +373,7 @@ class MarkingTaskService:
         return cleaned_data, annot_data, rubrics_used
 
     def get_user_mark_results(self, user, question=None, version=None):
-        """
-        For each completed task, get the latest annotation instances for a particular user.
+        """For each completed task, get the latest annotation instances for a particular user.
 
         Args:
             user: User instance
@@ -430,7 +383,6 @@ class MarkingTaskService:
         Returns:
             list [Annotation]: the relevant annotations
         """
-
         complete_tasks = MarkingTask.objects.filter(
             assigned_user=user, status=MarkingTask.COMPLETE
         )
@@ -449,8 +401,7 @@ class MarkingTaskService:
         return list(annotations)
 
     def get_latest_annotation(self, paper, question):
-        """
-        Get the latest annotation for a particular paper/question.
+        """Get the latest annotation for a particular paper/question.
 
         Args:
             paper: int, the paper number
@@ -459,23 +410,19 @@ class MarkingTaskService:
         Returns:
             Annotation: the latest annotation instance
         """
-
         task = self.get_latest_task(paper, question)
         return Annotation.objects.filter(task=task).order_by("-edition").first()
 
     def get_all_tags(self):
-        """
-        Get all of the saved tags.
+        """Get all of the saved tags.
 
         Returns:
             list[str]: The text of all the tags that exist.
         """
-
         return [tag.text for tag in MarkingTaskTag.objects.all()]
 
     def get_tags_for_task(self, code):
-        """
-        Get a list of tags assigned to this marking task.
+        """Get a list of tags assigned to this marking task.
 
         Args:
             code: str, the question/paper code for a task
@@ -483,24 +430,21 @@ class MarkingTaskService:
         Returns:
             list[str]: the text of all tags for this task.
         """
-
         task = self.get_task_from_code(
             code
         )  # TODO: what if the client has an OOD task with the same code?
         return [tag.text for tag in task.markingtasktag_set.all()]
 
     def create_tag(self, user, tag_text):
-        """
-        Create a new tag that can be associated with marking task.
+        """Create a new tag that can be associated with marking task.
 
         Args:
             user: reference to a User instance
             tag_text: str, the text content of a tag.
 
-            Returns:
-                MarkingTaskTag: reference to the newly created tag
+        Returns:
+            MarkingTaskTag: reference to the newly created tag
         """
-
         if re.match(r"([-_+;:@]|\w)+", tag_text):
             # allowable characters: - _ + ; : @ and any alphanumeric character
             new_tag = MarkingTaskTag(
@@ -514,20 +458,17 @@ class MarkingTaskService:
             raise ValidationError(f"Invalid tag text: {tag_text}")
 
     def add_tag(self, tag, task):
-        """
-        Add a tag to a marking task.
+        """Add a tag to a marking task.
 
         Args:
             tag: reference to a MarkingTaskTag instance
             task: reference to a MarkingTask instance
         """
-
         tag.task.add(task)
         tag.save()
 
     def get_tag_from_text(self, text):
-        """
-        Get a tag object from its text contents.
+        """Get a tag object from its text contents.
 
         Args:
             text: str, the text contents of a tag.
@@ -536,7 +477,6 @@ class MarkingTaskService:
             MarkingTaskTag | None: The tag if it exists, and
             None if it does not exist.
         """
-
         text_tags = MarkingTaskTag.objects.filter(text=text)
         if text_tags.exists():
             # Assuming the queryset will always have a length of one
