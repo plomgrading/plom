30a603b16abb31cb22e736f25dcbbcfe5998cc69
julianLapenna12 <7jlapenna@gmail.com>
2023-05-16 14:40:27 -0700

    Split into 2 tests to check pass and fail conditions

--- plom/create/test_page_counts.py
+++ plom/create/test_page_counts.py
@@ -7,20 +7,26 @@ from pathlib import Path
 from plom.create.demotools import buildDemoSourceFiles
 from plom.create.buildDatabaseAndPapers import check_equal_page_count
 
-def test_equal_page_count(tmpdir):
+def test_equal_page_count_true(tmpdir):
     """Checks that the page counts of each source version pdf are equal.
 
     Arguments:
         tmpdir (dir): The directory holding the source version pdfs.
     """
+    # check that it passes when the page counts are equal
     tmp = Path(tmpdir)
     # build the source version pdfs in sourceVersions/
     buildDemoSourceFiles(tmp)
     check_true = check_equal_page_count(tmp / "sourceVersions")
-    print(check_true)
     assert check_true
-    print(x for x in (tmp / "sourceVersions").glob("*.pdf"))
-    # save a modified version2.pdf with less pages
+    
+
+def test_equal_page_count_false(tmpdir):
+    # check that it fails when the page counts are not equal
+    tmp = Path(tmpdir)
+    # build the source version pdfs in sourceVersions/
+    buildDemoSourceFiles(tmp)
+    # create a new file with a single page
     clone = fitz.open()
     clone.new_page()
     clone.save(tmp / "sourceVersions/version3.pdf")
@@ -30,7 +36,3 @@ def test_equal_page_count(tmpdir):
     assert not check_false
     # delete version3.pdf
     os.remove(tmp / "sourceVersions/version3.pdf")
-
-    
-
-test_equal_page_count(Path("plom/create"))
