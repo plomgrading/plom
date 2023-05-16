2b5777fae00f0a88a03eae6da20914ad23810d62
julianLapenna12 <7jlapenna@gmail.com>
2023-05-16 14:30:37 -0700

    Created new file with blank page for testing
    Switched from renaming files to creating a new file with a
    blank page. Test passes before with just the normal source
    version PDFs and after adding the imposter PDF. Formatting
    is still an issue it seems as I call the test function from
    within the file.

--- plom/create/test_page_counts.py
+++ plom/create/test_page_counts.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2023 Julian Lapenna
 
+import fitz
 import os
 from pathlib import Path
 from plom.create.demotools import buildDemoSourceFiles
@@ -11,24 +12,25 @@ def test_equal_page_count(tmpdir):
 
     Arguments:
         tmpdir (dir): The directory holding the source version pdfs.
-
-    Raises:
-        ValueError: The page counts are not equal.
     """
-
     tmp = Path(tmpdir)
     # build the source version pdfs in sourceVersions/
-    buildDemoSourceFiles(tmp, solutions=True)
+    buildDemoSourceFiles(tmp)
     check_true = check_equal_page_count(tmp / "sourceVersions")
     print(check_true)
     assert check_true
     print(x for x in (tmp / "sourceVersions").glob("*.pdf"))
-    # change the name of one of the solution pdfs to make the page counts unequal
-    os.rename(tmp / "sourceVersions" / "solutions1.pdf", tmp / "sourceVersions" / "version3.pdf")
+    # save a modified version2.pdf with less pages
+    clone = fitz.open()
+    clone.new_page()
+    clone.save(tmp / "sourceVersions/version3.pdf")
     # check that the page counts are no longer equal
     check_false = check_equal_page_count(tmp / "sourceVersions")
     print(check_false)
     assert not check_false
-    print(x for x in (tmp / "sourceVersions").glob("*.pdf"))
-    # change the name of the solution pdf back to solutions1.pdf
-    os.rename(tmp / "sourceVersions" / "version3.pdf", tmp / "sourceVersions" / "solutions1.pdf")
+    # delete version3.pdf
+    os.remove(tmp / "sourceVersions/version3.pdf")
+
+    
+
+test_equal_page_count(Path("plom/create"))
