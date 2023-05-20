94aa7d6a2eb06baa40e08e584b8682d577adfe80
julianLapenna12 <7jlapenna@gmail.com>
2023-05-19 17:21:46 -0700

    Switched to `tmp_path` from `tmpdir` and cleaned up code

--- plom/create/test_page_counts.py
+++ plom/create/test_page_counts.py
@@ -1,36 +1,27 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2023 Julian Lapenna
 
-import os
-from pathlib import Path
-
 import fitz
 
 from plom.create.demotools import buildDemoSourceFiles
 from plom.create.buildDatabaseAndPapers import check_equal_page_count
 
 
-def test_equal_page_count_true(tmpdir):
+def test_equal_page_count_true(tmp_path):
     """Checks that the page counts of each source version pdf are equal.
 
     Arguments:
         tmpdir (dir): The directory holding the source version pdfs.
     """
-    tmp = Path(tmpdir)
     # build the source version pdfs in sourceVersions/
-    buildDemoSourceFiles(tmp)
-    check_true = check_equal_page_count(tmp / "sourceVersions")
-    assert check_true
+    buildDemoSourceFiles(tmp_path)
+    assert check_equal_page_count(tmp_path / "sourceVersions")
 
 
-def test_equal_page_count_false(tmpdir):
-    tmp = Path(tmpdir)
-    buildDemoSourceFiles(tmp)
+def test_equal_page_count_false(tmp_path):
+    buildDemoSourceFiles(tmp_path)
     # create a new file with a single page
     clone = fitz.open()
     clone.new_page()
-    clone.save(tmp / "sourceVersions/version3.pdf")
-    # check that the page counts are no longer equal
-    check_false = check_equal_page_count(tmp / "sourceVersions")
-    assert not check_false
-    os.remove(tmp / "sourceVersions/version3.pdf")
+    clone.save(tmp_path / "sourceVersions/version3.pdf")
+    assert not check_equal_page_count(tmp_path / "sourceVersions")
