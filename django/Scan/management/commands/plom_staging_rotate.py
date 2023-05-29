af561baac9e3a64fe77977236078b25bd2999e28
brennenchiu <bchiyeung1996@gmail.com>
2023-05-29 11:20:34 -0700

    initial work for rotate images in command line

---
+++ django/Scan/management/commands/plom_staging_rotate.py
@@ -0,0 +1,41 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2023 Brennen Chiu
+
+from django.core.management.base import BaseCommand, CommandError
+
+from Scan.services import ImageRotateService
+
+
+class Command(BaseCommand):
+    """
+    python3 manage.py plom_staging_rotate (username) (bundle name) (bundle_order)
+    """
+
+    help = "Rotate a page from the given bundle name and bundle order"
+
+    def rotate_image_from_bundle(self, username, bundle_name, bundle_order):
+        scanner = ImageRotateService()
+        scanner.rotate_image_cmd(username, bundle_name, bundle_order)
+        
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "username", type=str, help="Which user is performing this operation"
+        )
+        parser.add_argument(
+            "bundle",
+            type=str,
+            help="The bundle from which to rotate a page",
+        )
+        parser.add_argument(
+            "order",
+            type=int,
+            help="The order of the page",
+        )
+    
+    def handle(self, *args, **options):
+        self.rotate_image_from_bundle(
+            options["username"],
+            options["bundle"],
+            options["order"],
+        )
---
+++ django/Scan/services/image_rotate.py
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2023 Brennen Chiu
+
+from PIL import Image
+
+from django.contrib.auth.models import User
+from django.db import transaction
+from django.core.exceptions import ObjectDoesNotExist
+
+from Scan.models import (StagingBundle)
+
+
+class ImageRotateService:
+
+    @transaction.atomic
+    def rotate_image(self, user_obj, bundle_obj, bundle_order):
+        if bundle_obj.pushed:
+            raise ValueError("This bundle has been pushed - it cannot be modified.")
+
+        try:
+            staging_img = bundle_obj.stagingimage_set.get(bundle_order=bundle_order)
+        except ObjectDoesNotExist:
+            raise ValueError(f"Cannot find an image at order {bundle_order}")
+        
+        print(staging_img.image_file)
+        image = Image.open(staging_img.image_file)
+        image = image.rotate(angle=180, expand=True)
+        image.show()
+
+    @transaction.atomic
+    def rotate_image_cmd(self, username, bundle_name, bundle_order):
+        try:
+            user_obj = User.objects.get(username__iexact=username, groups__name__in=["scanner", "manager"])
+        except ObjectDoesNotExist:
+            raise PermissionError(
+                f"User '{username}' does not exist or has wrong permissions!"
+            )
+        
+        try:
+            bundle_obj = StagingBundle.objects.get(slug=bundle_name)
+        except ObjectDoesNotExist:
+            raise ValueError(f"Bundle '{bundle_name}' does not exist!")
+        
+        self.rotate_image(user_obj, bundle_obj, bundle_order)
+    
+
