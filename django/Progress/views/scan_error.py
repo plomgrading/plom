129e96edc51abecd740ebb9d031dcfc6bc59536c
Edith Coates <ecoates.bc@gmail.com>
2023-05-30 11:13:20 -0700

    copyright header updates

--- django/Progress/views/scan_error.py
+++ django/Progress/views/scan_error.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2022 Brennen Chiu
+# Copyright (C) 2023 Edith Coates
 
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, FileResponse
