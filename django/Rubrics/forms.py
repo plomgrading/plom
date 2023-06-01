8ca93df0e446e7a26a5bd513b4686fab166a63d8
julianLapenna12 <7jlapenna@gmail.com>
2023-06-01 11:29:33 -0700

    Fixed form label format

--- django/Rubrics/forms.py
+++ django/Rubrics/forms.py
@@ -5,6 +5,6 @@ from django import forms
 
 
 class RubricForm(forms.Form):
-    option1 = forms.BooleanField(required=False, label='option1')
-    option2 = forms.BooleanField(required=False, label='option2')
-    option3 = forms.BooleanField(required=False, label='option3')
+    option1 = forms.BooleanField(required=False, label="option1")
+    option2 = forms.BooleanField(required=False, label="option2")
+    option3 = forms.BooleanField(required=False, label="option3")
