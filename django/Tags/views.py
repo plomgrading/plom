f226f87d0b46557348e1df7d5029625755438f3e
julianLapenna12 <7jlapenna@gmail.com>
2023-06-08 13:35:42 -0700

    Fixed styling, removed comments and added copyright

--- django/Tags/views.py
+++ django/Tags/views.py
@@ -16,59 +16,44 @@ class TagLandingPageView(ManagerRequiredView):
     form = TagEditForm
 
     def get(self, request):
-        print("GET: ", request.GET)
-        print("session: ", request.session)
         context = self.build_context()
+        text_field_form = TagFormFilter()
+
         tag_filter_text = request.session.get("tag_filter_text", "")
         tag_filter_strict = request.session.get("strict_match", "off")
 
-        text_field_form = TagFormFilter()
-        context.update({"text_field_form": text_field_form})
-        context.update({"tag_filter_text": tag_filter_text})
-        context.update({"tag_filter_strict": tag_filter_strict})
-
-
         if tag_filter_strict == "on":
-            print("exact match")
             task_tags = self.ts.get_task_tags_with_tag_exact(tag_filter_text)
         else:
-            print("loose match")
             task_tags = self.ts.get_task_tags_with_tag(tag_filter_text)
 
-        print(task_tags)
-        print(task_tags.__len__())
-
-        # print(task_tags[0].text)
-        # print(task_tags[0].task.all())
         papers = self.ts.get_papers_from_task_tags(task_tags)
-        print(papers)
-        print(type(papers))
-
         tag_counts = self.ts.get_task_tags_counts()
-        print(tag_counts)
 
-        context.update({"tag_count": task_tags.__len__()})
-        print("good 1")
-        context.update({"task_tags": task_tags})
-        print("good 2")
-        context.update({"papers": papers})
-        print("good 3")
-        context.update({"tag_counts": tag_counts})
-        print("good 4")
+        context.update(
+            {
+                "tag_count": task_tags.__len__(),
+                "task_tags": task_tags,
+                "papers": papers,
+                "tag_counts": tag_counts,
+                "text_field_form": text_field_form,
+                "tag_filter_text": tag_filter_text,
+                "tag_filter_strict": tag_filter_strict
+            }
+        )
 
         return render(request, self.template_name, context=context)
 
     def tag_filter(request):
         """Filter papers by tag."""
-        print("POST:", request.POST)
         request.session["tag_filter_text"] = request.POST.get("tag_filter_text", "")
         request.session["strict_match"] = request.POST.get("strict_match", "off")
 
         return redirect("tags_landing")
-    
+
 
 class TagItemView(ManagerRequiredView):
-    """A page for displaying a single tag and its annotations."""
+    """A page for displaying a single tag and related information."""
 
     template_name = "Tags/tag_item.html"
     ts = TagService()
@@ -84,11 +69,8 @@ class TagItemView(ManagerRequiredView):
 
     def post(request, tag_text):
         form = TagEditForm(request.POST)
-        print(form)
-        print(form.is_valid())
-        
+
         if form.is_valid():
-            print("form is valid")
             tag = TagItemView.ts.get_tag(tag_text=tag_text)
             for key, value in form.cleaned_data.items():
                 tag.__setattr__(key, value)
@@ -97,5 +79,5 @@ class TagItemView(ManagerRequiredView):
 
     def tag_delete(request, tag_text):
         """Delete a tag."""
-        print("tag_delete")
+        # TODO: Delete tag from the database.
         return redirect("tags_landing")
