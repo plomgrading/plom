971c342c992d5055a212993ba7dc1641ad1954d5
Natalie Balashov <margaret.balashov@gmail.com>
2023-06-07 21:01:42 -0700

    Add IDPredictions table in Identify/models.py

--- django/Identify/models.py
+++ django/Identify/models.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2023 Natalie Balashov
 
 from django.db import models
+from django.contrib.auth.models import User
 
 from Base.models import BaseTask, BaseAction
 from Papers.models import Paper
@@ -19,3 +20,11 @@ class PaperIDAction(BaseAction):
 
     student_name = models.TextField(default="")
     student_id = models.TextField(default="")
+
+
+class IDPrediction(models.Model):
+    paper = models.ForeignKey(Paper, null=False, on_delete=models.CASCADE)
+    student_id = models.CharField(null=True, max_length=255)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    predictor = models.CharField(null=False, max_length=255)
+    certainty = models.FloatField(null=False, default=0.0)
