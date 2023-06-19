9e898a52ac0a1e737f575632b3adce084af41f82
Natalie Balashov <margaret.balashov@gmail.com>
2023-06-19 07:39:47 -0700

    Remove scikit-learn warnings and minor clean up of trainRandomForestModel
    Fixes #1840 by using X.values instead of X, which uses the values of the
    dataframe without the headers.
    
    Some flake8 linting, deletion of unused imports/variables and comment
    rewording.
    
    fetch_openml() has parser="auto" to silence another warning.

--- plom/idreader/trainRandomForestModel.py
+++ plom/idreader/trainRandomForestModel.py
@@ -1,13 +1,13 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2020 Vala Vakilian
 # Copyright (C) 2021, 2023 Colin B. Macdonald
+# Copyright (C) 2023 Natalie Balashov
 
 import gzip
 from pathlib import Path
 import pickle
 from warnings import warn
 
-import pandas as pd
 import sklearn
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
@@ -17,22 +17,23 @@ from sklearn.metrics import accuracy_score
 
 def train_model():
     """Trains the random forest model and saves it."""
-    mnist = fetch_openml("mnist_784")
-
-    # pylint: disable=no-member
-    images = pd.DataFrame(mnist.data)
-    labels = pd.DataFrame(mnist.target)
+    mnist = fetch_openml("mnist_784", parser="auto")
 
     # Split the data into training and testing data
     X_train, X_test, y_train, y_test = train_test_split(
         mnist.data, mnist.target, test_size=1 / 8.0
     )
 
-    # TODO: This is used only for debugging.
+    # Useful to use a subset of data if debugging:
     # X_train = X_train[0:100]
     # y_train = y_train[0:100]
 
-    # Define and Train the model
+    # Only use values from X dataframes (without headers)
+    # Prevents scikit-learn feature name warnings
+    X_train = X_train.values
+    X_test = X_test.values
+
+    # Define and train the model
     model = RandomForestClassifier()
     model.fit(X_train, y_train)
 
@@ -43,11 +44,10 @@ def train_model():
     with gzip.open(model_cache / filename, "wb") as f:
         pickle.dump(model, f)
 
-    # Now we are done, we will do a sanity check
+    # Assess the accuracy of the trained model
     with gzip.open(model_cache / filename, "rb") as f:
         loaded_model = pickle.load(f)
     testing_predictions = loaded_model.predict(X_test)
-    # score = model.score(X_test, y_test)
     score = accuracy_score(testing_predictions, y_test)
     print("Model score is: ", str(score))
     if score < 0.95:
