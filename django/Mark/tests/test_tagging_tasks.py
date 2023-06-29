51ad57b4cf14903c2a328c0d32149832e97b1aec
Colin B. Macdonald <cbm@m.fsf.org>
2023-06-29 09:28:45 -0700

    Marked expected failure using `unittest` not `pytest`
    Hat-tip @natmarbal.

--- django/Mark/tests/test_tagging_tasks.py
+++ django/Mark/tests/test_tagging_tasks.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2023 Colin B. Macdonald
 
-import pytest
+import unittest
 
 from django.test import TestCase
 from django.contrib.auth.models import User
@@ -22,16 +22,17 @@ class MarkingTaskServiceTaggingTests(TestCase):
         with self.assertRaisesMessage(ValueError, "No such tag"):
             s.remove_tag_text_from_task_code("no_such_tag_411d1b1443e5", task.code)
 
-    # @pytest.mark.xfail(reason="Issue #2810")
-    # def test_tag_remove_no_such_local_tag(self):
-    #     s = MarkingTaskService()
-    #     user = baker.make(User)
-    #     s.create_tag(user, "hello")
-    #     task = baker.make(
-    #         MarkingTask, question_number=1, paper__paper_number=2, code="q0002g1"
-    #     )
-    #     with self.assertRaisesMessage(ValueError, "does not have tag"):
-    #         s.remove_tag_text_from_task_code("hello", task.code)
+    # Issue #2810
+    @unittest.expectedFailure
+    def test_tag_remove_no_such_local_tag(self):
+        s = MarkingTaskService()
+        user = baker.make(User)
+        s.create_tag(user, "hello")
+        task = baker.make(
+            MarkingTask, question_number=1, paper__paper_number=2, code="q0002g1"
+        )
+        with self.assertRaisesMessage(ValueError, "does not have tag"):
+            s.remove_tag_text_from_task_code("hello", task.code)
 
     def test_tag_remove_invalid_code(self):
         s = MarkingTaskService()
