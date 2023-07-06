c102cdfb9aa320f20491fe94a872b65f9c9300ea
Edith Coates <ecoates.bc@gmail.com>
2023-07-05 17:34:39 -0700

    implemented reassembly endpoints

--- django/API/views/report.py
+++ django/API/views/report.py
@@ -1,5 +1,8 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2023 Colin B. Macdonald
+# Copyright (C) 2023 Edith Coates
+
+from django.shortcuts import get_object_or_404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,6 +10,7 @@ from rest_framework import status
 
 from Finish.services import ReassembleService
 
+from Papers.models import Paper
 from Mark.services import MarkingTaskService
 
 
@@ -30,16 +34,9 @@ class REPidentified(APIView):
 
 class REPcompletionStatus(APIView):
     def get(self, request):
-        # TODO: Identified, Marked, num_questions_marked
-        # TODO: what time-stamp?  last-modified?
-        canned = {
-            "11": [False, True, 2, "2023-05-03T07:48:13.739950+00:00"],
-            "12": [True, True, 3, "2023-05-03T07:48:13.751636+00:00"],
-            "13": [True, True, 3, "2023-05-03T07:48:13.762651+00:00"],
-            "14": [True, True, 3, "2023-05-03T07:48:13.775470+00:00"],
-        }
+        completion_data = ReassembleService().get_completion_status()
         return Response(
-            canned,
+            completion_data,
             status=status.HTTP_200_OK,
         )
 
@@ -48,15 +45,7 @@ class REPcoverPageInfo(APIView):
     def get(self, request, papernum):
         # Return value looks like this:
         # [["10130103", "Vandeventer, Irene"], [1, 1, 0], [2, 1, 1], [3, 2, 5]]
-        service = MarkingTaskService()
-        r = []
-        # TODO: hardcoded
-        r.append(["10130103", "Vandeventer, Irene"])
-        # TODO: hardcoded for 3 questions
-        for question in range(1, 3 + 1):
-            annotation = service.get_latest_annotation(papernum, question)
-            # TODO: see MgetAnnotations: extra sanity checking?
-            # TODO: hard-coded version 1
-            ver = 1
-            r.append([question, ver, annotation.score])
-        return Response(r, status=status.HTTP_200_OK)
+        reasseble = ReassembleService()
+        paper = get_object_or_404(Paper, paper_number=papernum)
+        cover_page_info = reasseble.get_cover_page_info(paper)
+        return Response(cover_page_info, status=status.HTTP_200_OK)
--- django/Finish/services/reassemble_service.py
+++ django/Finish/services/reassemble_service.py
@@ -9,6 +9,7 @@ from Mark.services import MarkingTaskService
 from Papers.models import Paper
 from Papers.services import SpecificationService
 from Preparation.services import PQVMappingService
+from Progress.services import ManageScanService
 
 
 class ReassembleService:
@@ -28,6 +29,22 @@ class ReassembleService:
             and completed_tasks.count() + ood_tasks.count() == paper_tasks.count()
         )
 
+    def get_n_questions_marked(self, paper):
+        """Return the number of questions that are marked in a paper.
+
+        Args:
+            paper: a reference to a Paper instance
+        """
+        n_questions = SpecificationService().get_n_questions()
+        n_marked = 0
+        for i in range(1, n_questions + 1):
+            question_number_tasks = MarkingTask.objects.filter(
+                paper=paper, question_number=i, status=MarkingTask.COMPLETE
+            )
+            if question_number_tasks.count() == 1:
+                n_marked += 1
+        return n_marked
+
     def get_last_updated_timestamp(self, paper):
         """Return the latest update timestamp from the IDActions or Annotations.
 
@@ -44,7 +61,9 @@ class ReassembleService:
 
         if self.is_paper_marked(paper):
             paper_annotations = Annotation.objects.filter(task__paper=paper)
-            latest_annotation_instance = paper_annotations.order_by("-time_of_last_update").first()
+            latest_annotation_instance = paper_annotations.order_by(
+                "-time_of_last_update"
+            ).first()
             latest_annotation = latest_annotation_instance.time_of_last_update
         else:
             latest_annotation = None
@@ -77,6 +96,27 @@ class ReassembleService:
         action = PaperIDAction.objects.get(task=latest_task)
         return action.student_id, action.student_name
 
+    def get_question_data(self, paper, question_number):
+        """For a given question, return the test's question version and score.
+
+        Args:
+            paper: a reference to a Paper instance
+            question_number: int, question index
+
+        Returns:
+            tuple (int, int or None): question version and score
+        """
+        qvmap = PQVMappingService().get_pqv_map_dict()
+        version = qvmap[paper.paper_number][question_number]
+        if self.is_paper_marked(paper):
+            annotation = MarkingTaskService().get_latest_annotation(
+                paper.paper_number, question_number
+            )
+            mark = annotation.score
+        else:
+            mark = None
+        return version, mark
+
     def paper_spreadsheet_dict(self, paper):
         """Return a dictionary representing a paper for the reassembly spreadsheet.
 
@@ -97,21 +137,32 @@ class ReassembleService:
 
         n_questions = SpecificationService().get_n_questions()
         paper_marked = self.is_paper_marked(paper)
-        if paper_marked:
-            mts = MarkingTaskService()
-            for i in range(1, n_questions + 1):
-                annotation = mts.get_latest_annotation(paper.paper_number, i)
-                paper_dict[f"q{i}m"] = annotation.score
-        else:
-            for i in range(1, n_questions + 1):
-                paper_dict[f"q{i}m"] = 0
-        qvmap = PQVMappingService().get_pqv_map_dict()
         for i in range(1, n_questions + 1):
-            paper_dict[f"q{i}v"] = qvmap[paper.paper_number]
+            version, mark = self.get_question_data(paper, i)
+            paper_dict[f"q{i}m"] = mark
+            paper_dict[f"q{i}v"] = version
         paper_dict["marked"] = paper_marked
 
         paper_dict["last_update"] = self.get_last_updated_timestamp(paper)
         return paper_dict
+
+    def get_paper_status(self, paper):
+        """Return a list of [scanned?, identified?, n questions marked, time of last update] for a given paper.
+
+        Args:
+            paper: reference to a Paper object
+
+        Returns:
+            list of [bool, bool, int, datetime]
+        """
+        paper_id_info = self.get_paper_id_or_none(paper)
+        is_id = paper_id_info is not None
+        complete_paper_keys = ManageScanService().get_all_completed_test_papers().keys()
+        is_scanned = paper.paper_number in complete_paper_keys
+        n_marked = self.get_n_questions_marked(paper)
+        last_modified = self.get_last_updated_timestamp(paper)
+
+        return [is_scanned, is_id, n_marked, is_scanned, last_modified]
 
     def get_spreadsheet_data(self):
         """Return a dictionary with all of the required data for a reassembly spreadsheet."""
@@ -135,3 +186,31 @@ class ReassembleService:
                 student_id, student_name = paper_id_info
                 spreadsheet_data[paper.paper_number] = [student_id, student_name]
         return spreadsheet_data
+
+    def get_completion_status(self):
+        """Return a dictionary of overall test completion progress."""
+        spreadsheet_data = {}
+        papers = Paper.objects.all()
+        for paper in papers:
+            spreadsheet_data[paper.paper_number] = self.get_paper_status(paper)
+        return spreadsheet_data
+
+    def get_cover_page_info(self, paper):
+        """Return information needed to build a cover page for a reassembled test.
+
+        Args:
+            paper: a reference to a Paper instance
+        """
+        cover_page_info = []
+        paper_id_info = self.get_paper_id_or_none(paper)
+        if paper_id_info:
+            cover_page_info.append(list(paper_id_info))
+        else:
+            cover_page_info.append([None, None])
+
+        n_questions = SpecificationService().get_n_questions()
+        for i in range(1, n_questions + 1):
+            version, mark = self.get_question_data(paper, i)
+            cover_page_info.append([i, version, mark])
+
+        return cover_page_info
