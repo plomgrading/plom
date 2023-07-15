a35cc8a1aba03f108e88050d274983cb135cf220
Colin B. Macdonald <cbm@m.fsf.org>
2023-07-14 18:30:36 -0700

    Remove the script
    This should be runnable by `python manage.py generate_report`.

--- django/generate_report.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2023 Julian Lapenna
-
-export PYTHONPATH=".."
-export DJANGO_SETTINGS_MODULE=Web_Plom.settings
-
-python Finish/management/commands/generate_report.py
