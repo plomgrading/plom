ae2b62a167ec38bdfbeed3000aa62962038b98ec
Colin B. Macdonald <cbm@m.fsf.org>
2023-07-14 21:57:32 -0700

    Lint fixes to docs

--- django/Profile/admin.py
+++ django/Profile/admin.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022 Brennen Chiu
+
 from django.contrib import admin
 
 # Register your models here.
--- django/Profile/edit_profile_form.py
+++ django/Profile/edit_profile_form.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022 Brennen Chiu
+
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
 
