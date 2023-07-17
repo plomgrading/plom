a72259285a93c640cd91cf624cfb2b5202292c72
Colin B. Macdonald <cbm@m.fsf.org>
2023-07-17 10:28:26 -0700

    spec: questions are store in a question list-of-dict

--- django/SpecCreator/services/spec_generate.py
+++ django/SpecCreator/services/spec_generate.py
@@ -11,7 +11,7 @@ class TestSpecGenerateService:
     def __init__(self, spec_service: TestSpecService):
         self.spec = spec_service
 
-    def generate_spec_dict(self):
+    def generate_spec_dict(self) -> dict:
         """Create a dictionary that can be dumped into a .toml file."""
         spec_dict = {}
         spec = self.spec.specification()
@@ -29,8 +29,8 @@ class TestSpecGenerateService:
         spec_dict["idPage"] = self.spec.get_id_page_number()
         spec_dict["doNotMarkPages"] = self.spec.get_dnm_page_numbers()
 
-        # notice that plom wants a spec with "question"(singular) not "questions" (plural)
-        question = {}
+        # notice that Plom wants a spec with list "question" (singular) dicts
+        question = []
         for i in range(self.spec.get_n_questions()):
             q_dict = {}
             q_service = self.spec.questions[i + 1]
@@ -38,7 +38,7 @@ class TestSpecGenerateService:
             q_dict["mark"] = q_service.get_question_marks()
             q_dict["label"] = q_service.get_question_label()
             q_dict["select"] = q_service.get_question_fix_or_shuffle()
-            question[str(i + 1)] = q_dict
+            question.append(q_dict)
 
         spec_dict["question"] = question
 
