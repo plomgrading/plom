e7141d0a6f1acdfd2221da1b20e4373dd5f43118
julianLapenna12 <7jlapenna@gmail.com>
2023-07-18 14:19:01 -0700

    Fixed n+1 query in get_student_info_from_paper()

--- django/Finish/services/student_marks_service.py
+++ django/Finish/services/student_marks_service.py
@@ -126,7 +126,9 @@ class StudentMarkService:
             paper_obj = Paper.objects.get(pk=paper_num)
         except Paper.DoesNotExist:
             raise Paper.DoesNotExist
-        marking_tasks = paper_obj.markingtask_set.all()
+        marking_tasks = paper_obj.markingtask_set.all().select_related(
+            "latest_annotation"
+        )
         paper_id_task = PaperIDTask.objects.filter(paper=paper_obj).first()
         last_update = None
 
