111bc29ae45e47c3c9e4fd0a183e7d63bfbb4960
divy-07 <divy07.com@gmail.com>
2023-07-24 20:43:10 -0700

    move d3 js files in appropriate folders

--- django/static/js/histogram/histogram.js
+++ django/static/js/histogram/histogram.js
@@ -6,7 +6,7 @@
 function renderHist(data, divId) {
     // Clear existing contents of the div
     d3.select("#" + divId).html("");
-    
+
     // Set up dimensions and margins
     const margin = { top: 20, right: 20, bottom: 50, left: 50 };
     const width = 400 - margin.left - margin.right;
