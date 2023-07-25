3bc4a86afc32a7b0c88eae3a381fee7684e628a3
Colin B. Macdonald <cbm@m.fsf.org>
2023-07-24 22:34:23 -0700

    Some more files with no copyright headers

--- plom_server/Preparation/admin.py
+++ plom_server/Preparation/admin.py
@@ -1,5 +1,10 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022 Andrew Rechnitzer
+# Copyright (C) 2023 Colin B. Macdonald
+
 from django.contrib import admin
 from .models import StagingClasslistCSV
+
 
 # Register your models here.
 admin.site.register(StagingClasslistCSV)
