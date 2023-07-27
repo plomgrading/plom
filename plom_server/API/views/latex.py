5153f7a05fa7a6625f66414a705ec4f93a9008a7
Andrew Rechnitzer <andrewr@math.ubc.ca>
2023-07-27 11:10:15 -0700

    Get the binary latex fragment response working

--- plom_server/API/views/latex.py
+++ plom_server/API/views/latex.py
@@ -1,6 +1,9 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2023 Colin B. Macdonald
 
+import json
+from django.http import HttpResponse
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import APIException
@@ -11,18 +14,33 @@ from plom.textools import texFragmentToPNG
 
 class MlatexFragment(APIView):
     def post(self, request):
-        print("latex not implemented yet, Issue #2639")
-        print(request)
-        data = request.POST
-        print(data)
-        fragment = data.get("fragment")
-        valid, value = texFragmentToPNG(fragment)
-        print(fragment)
-        print(valid)
-        print(type(valid))
-        print(type(value))
-        print(len(value))
-        return Response(
-            "post: Sorry server does not support latex",
-            status=status.HTTP_406_NOT_ACCEPTABLE,
-        )
+        try:
+            request_json = json.loads(request.body)
+            fragment = request_json.get("fragment")
+        except ValueError:
+            return Response(
+                "post: Problem decoding json from client",
+                status=status.HTTP_406_NOT_ACCEPTABLE,
+            )
+        except KeyError:
+            return Response(
+                "post: Json did not include required 'fragment' key",
+                status=status.HTTP_406_NOT_ACCEPTABLE,
+            )
+
+        try:
+            valid, value = texFragmentToPNG(fragment)
+        except RuntimeError:
+            return Response(
+                "post: Sorry server does not support latex",
+                status=status.HTTP_406_NOT_ACCEPTABLE,
+            )
+        if valid:
+            # see https://stackoverflow.com/questions/47192986/difference-between-response-and-httpresponse-django   (for example)
+            # for why to use httpresponse here instead of DRF's Response
+            return HttpResponse(value)
+        else:
+            return Response(
+                value,
+                status=status.HTTP_406_NOT_ACCEPTABLE,
+            )
