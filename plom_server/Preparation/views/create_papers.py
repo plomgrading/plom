1e692968d43c82b3cd63befed6845623a50e751d
Colin B. Macdonald <cbm@m.fsf.org>
2023-07-28 22:34:34 -0700

    Deleting code, for great justice

--- plom_server/Preparation/views/create_papers.py
+++ plom_server/Preparation/views/create_papers.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 from Base.base_group_views import ManagerRequiredView
 
 from Papers.services import SpecificationService, PaperInfoService
+from ..services import PQVMappingService
 
 
 class PaperCreationView(ManagerRequiredView):
@@ -16,10 +17,15 @@ class PaperCreationView(ManagerRequiredView):
         spec = SpecificationService()
         paper_info = PaperInfoService()
         context = super().build_context()
+        pqvs = PQVMappingService()
+        foo = pqvs.get_pqv_map_dict()
+        # print(f"debugging: qvmap is:\n  {foo}")
+        n_to_produce = len(foo)
+        print(f"n_to_produce from qvmap is {n_to_produce}")
         context.update(
             {
                 "is_populated": paper_info.is_paper_database_populated(),
-                "n_papers": spec._get_n_to_produce(),
+                "n_papers": n_to_produce,
                 "n_questions": spec.get_n_questions(),
                 "n_versions": spec.get_n_versions(),
                 "n_pages": spec.get_n_pages(),
