f50d4ca002d57907ec1d69a54b55a54ce8b9f962
divy-07 <divy07.com@gmail.com>
2023-08-01 12:27:49 -0700

    copyright headers

--- plom_server/Web_Plom/settings.py
+++ plom_server/Web_Plom/settings.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2022-2023 Brennen Chiu
 # Copyright (C) 2023 Colin B. Macdonald
 # Copyright (C) 2023 Julian Lapenna
+# Copyright (C) 2023 Divy Patel
 
 """Django settings for Plom project.
 
