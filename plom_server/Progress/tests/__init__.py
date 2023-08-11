4fae9e52b2e361b50ccce3e5fc1a44a2128d0df6
Andrew Rechnitzer <andrewr@math.ubc.ca>
2023-08-11 11:31:25 -0700

    Copyright fix

--- plom_server/Progress/tests/__init__.py
+++ plom_server/Progress/tests/__init__.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2022 Edith Coates
+# Copyright (C) 2023 Andrew Rechnitzer
 
 from .test_manage_scan import *
 from .test_manage_discard import *
