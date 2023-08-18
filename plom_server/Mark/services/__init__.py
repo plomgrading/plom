96ec43123b3f97fc7041bc011349de47e45591e7
Andrew Rechnitzer <andrewr@math.ubc.ca>
2023-08-18 14:10:49 -0700

    Minor ci error fixes

--- plom_server/Mark/services/__init__.py
+++ plom_server/Mark/services/__init__.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2022 Edith Coates
+# Copyright (C) 2023 Andrew Rechnitzer
 
 from .marking_tasks import MarkingTaskService
 from .page_data import PageDataService
