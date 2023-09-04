a63928f93e9be858e9771e412ef3bc9027642bbf
Colin B. Macdonald <cbm@m.fsf.org>
2023-09-04 09:09:14 -0700

    Cleanup a readme and doc running this stuff
    Fixes #2982.  See also Issue #2604.

--- plom_server/docker_run.sh
+++ plom_server/docker_run.sh
@@ -5,3 +5,11 @@
 
 python3 manage.py plom_demo --no-waiting
 python3 manage.py runserver 0.0.0.0:8000
+
+# Some stuff for making a basic server
+# TODO: needs huey stuff
+# python3 manage.py makemigrations
+# python3 manage.py migrate
+# python3 manage.py createsuperuser --username root --noinput --email root@example.com
+# python3 manage.py plom_create_groups
+# python3 manage.py shell -c "from django.contrib.auth.models import User, Group; manager_group = Group.objects.get(name='manager'); User.objects.create_user(username='manager', password='1234').groups.add(manager_group)"
