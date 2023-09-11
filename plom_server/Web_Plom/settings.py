0314a28a403ca5d53e934c21ad2614603641d89b
Edith Coates <ecoates.bc@gmail.com>
2023-09-11 16:08:27 -0700

    Remove coreapi from this branch

--- plom_server/Web_Plom/settings.py
+++ plom_server/Web_Plom/settings.py
@@ -231,7 +231,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
 
 # Media and user-uploaded files
