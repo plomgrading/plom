a80c9fb7e8496ef08cd959d306bc68c653d45635
Colin B. Macdonald <cbm@m.fsf.org>
2023-09-12 12:39:27 -0700

    Copyright update

--- plom/db/db_upload.py
+++ plom/db/db_upload.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2018-2022 Andrew Rechnitzer
-# Copyright (C) 2020-2022 Colin B. Macdonald
+# Copyright (C) 2020-2023 Colin B. Macdonald
 # Copyright (C) 2022 Joey Shi
 
 from datetime import datetime, timezone
