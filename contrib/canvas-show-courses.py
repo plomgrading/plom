368a39365eea790f165f6accf4284834101c8d94
Philip Loewen <loew@math.ubc.ca>
2023-09-19 15:38:03 -0700

    Two new scripts collect info from Canvas and print it as CSV.

---
+++ contrib/canvas-show-courses.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+# List the courses for the user with a given API key.
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2023 Philip D. Loewen
+
+from canvasapi import Canvas
+import os
+import sys
+from pathlib import Path
+import csv
+
+api_key = os.environ.get("CANVAS_API_KEY", None)
+
+if api_key is None:
+    homedir = os.environ["HOME"]
+    # print("Home directory is {:s}.".format(homedir))
+    secretsfile = Path(f"{homedir}/.secrets/canvas-api-key")
+    # print(f"secretsfile is {secretsfile}.")
+    if os.path.exists(secretsfile) and os.access(secretsfile, os.R_OK):
+        with open(secretsfile, "r") as f:
+            api_key = f.read()
+        print(f"Retrieved CANVAS_API_KEY from {secretsfile}.")
+
+if api_key is None:
+    print("This script requires a Canvas API key to be provided")
+    print("in the environment variable CANVAS_API_KEY.")
+    print("A fallback search in the file {} failed.".format(secretsfile))
+    print("That variable remains unset, so execution cannot continue.")
+    sys.exit(1)
+
+api_url = "https://canvas.ubc.ca"
+canvas = Canvas(api_url, api_key)
+me = canvas.get_current_user()
+my_courses = me.get_courses()
+del canvas
+
+print(f"\nThe given key identifies {me.name}, ", end="")
+print(f"user number {me.id}.\n")
+
+print(f"User {me.id} has TeacherEnrollment role in the following courses [CSV]:\n")
+print(80 * "=")
+
+# Canvas returns many course objects, some with unexpected characteristics.
+# Ignore those in what follows. Build a dict of apparently-relevant ones.
+
+cnum2title = {}  # Prefix 'c' indicates 'course number to [course] title'
+for c in my_courses:
+    if hasattr(c, "course_code") and hasattr(c, "enrollments"):
+        for d in c.enrollments:
+            # Each d is a dict of roles, etc.
+            # If one shows me in the Teacher role, print it and
+            # ignore the rest.
+            if d["user_id"] == me.id and d["role"] == "TeacherEnrollment":
+                cnum2title[int(c.id)] = c.name
+
+stdout_ = sys.stdout
+csvwriter = csv.writer(stdout_, quoting=csv.QUOTE_NONNUMERIC)
+csvwriter.writerow(["Canvas ID", "Course Name"])
+for cnum, ctitle in sorted(cnum2title.items(), key=lambda kv: kv[1]):
+    csvwriter.writerow([cnum, ctitle])
+
+sys.exit(0)
