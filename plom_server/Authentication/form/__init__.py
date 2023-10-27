2814b8ee9078155a03f11d4b9b058d5684ed388a
brennenchiu <bchiyeung1996@gmail.com>
2023-10-27 13:05:49 -0700

    added copyright header

--- plom_server/Authentication/form/__init__.py
+++ plom_server/Authentication/form/__init__.py
@@ -1,1 +1,4 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2023 Brennen Chiu
+
 from .choices import USERNAME_CHOICES, USER_TYPE_CHOICES
--- plom_server/Authentication/form/choices.py
+++ plom_server/Authentication/form/choices.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2023 Brennen Chiu
+
 USER_TYPE_CHOICES = [
     ("manager", "Manager"),
     ("marker", "Marker"),
