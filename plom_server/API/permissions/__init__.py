6556356a5c43bdaffba5ed05124fd06dc9973610
Colin B. Macdonald <cbm@m.fsf.org>
2023-11-26 14:10:15 -0800

    lint

--- plom_server/API/permissions/__init__.py
+++ plom_server/API/permissions/__init__.py
@@ -1,4 +1,10 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2023 Edith Coates
+# Copyright (C) 2023 Colin B. Macdonald
 
-from .permissions import *
+from .permissions import (
+    AllowAnyReadOnly,
+    IsManager,
+    IsManagerReadOnly,
+    IsManagerOrAuthenticatedReadOnly,
+)
