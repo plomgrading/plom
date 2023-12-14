f4fccf48f7391b20e487ae8e3e1e171190d5e905
Andrew Rechnitzer <andrewr@math.ubc.ca>
2023-12-13 21:57:41 -0800

    Towards huey background soln build

--- plom_server/Finish/services/build_soln_service.py
+++ plom_server/Finish/services/build_soln_service.py
@@ -1,32 +1,105 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2023 Colin B. Macdonald
 # Copyright (C) 2023 Andrew Rechnitzer
 
+import arrow
 import fitz
+import io
 from pathlib import Path
+import random
 import tempfile
+import time
+from typing import Any, Dict, List, Optional
+import zipfly
 
-from django.conf import settings
+
+from django_huey import db_task, get_queue
+
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.files import File
+from django.db import transaction
+from django.db.models import Q
 
 from .soln_source import SolnSourceService
 from .reassemble_service import ReassembleService
+from Progress.services import ManageScanService
 
-
+from Base.models import HueyTaskTracker
+from Identify.models import PaperIDTask
 from Papers.models import (
     SolnSpecQuestion,
     Paper,
     QuestionPage,
 )
-from Finish.models import SolutionSourcePDF
+from Finish.models import SolutionSourcePDF, BuildSolutionPDFChore
 
 
 class BuildSolutionService:
     """Class that contains helper functions for sending data to plom-finish."""
 
-    base_dir = settings.MEDIA_ROOT
-    reassemble_dir = base_dir / "reassemble"
+    def get_all_paper_status_for_solution_build(self):
+        """Get the status information for all papers for solution build.
 
-    def watermark_pages(self, doc, watermark_text):
+        Returns:
+            List of dicts representing each row of the data.
+        """
+        status: Dict[str, Any] = {}
+        all_papers = Paper.objects.all()
+        for paper in all_papers:
+            status[paper.paper_number] = {
+                "paper_num": int(paper.paper_number),
+                "scanned": False,
+                "identified": False,
+                "student_id": "",
+                "when_id_done": None,
+                "when_id_done_humanised": None,
+                "build_soln_status": "",
+                "build_soln_time": None,
+                "build_soln_time_humanised": None,
+                "outdated": False,
+                "obsolete": None,
+            }
+        mss = ManageScanService()
+        for pn in mss.get_all_completed_test_papers():
+            status[pn]["scanned"] = True
+
+        for task in PaperIDTask.objects.filter(
+            status=PaperIDTask.COMPLETE
+        ).prefetch_related("paper", "latest_action"):
+            status[task.paper.paper_number]["identified"] = True
+            status[task.paper.paper_number][
+                "student_id"
+            ] = task.latest_action.student_id
+            status[task.paper.paper_number]["when_id_done"] = task.last_update
+
+        for task in BuildSolutionPDFChore.objects.filter(
+            obsolete=False
+        ).prefetch_related("paper"):
+            status[task.paper.paper_number][
+                "build_soln_status"
+            ] = task.get_status_display()
+            status[task.paper.paper_number]["obsolete"] = task.obsolete
+            if task.status == HueyTaskTracker.COMPLETE:
+                status[task.paper.paper_number]["build_soln_time"] = task.last_update
+                status[task.paper.paper_number][
+                    "build_soln_time_humanised"
+                ] = arrow.get(task.last_update).humanize()
+
+        # do last round of updates
+        for pn in status:
+            if status[pn]["when_id_done"]:
+                status[pn]["when_id_done_humanised"] = arrow.get(
+                    status[pn]["when_id_done"]
+                ).humanize()
+            # if soln pdf built before last id-ing of the paper, then soln pdf is outdated.
+            if status[pn]["build_soln_time"] and status[pn]["when_id_done"]:
+                if status[pn]["build_soln_time"] < status[pn]["when_id_done"]:
+                    status[pn]["outdated"] = True
+
+        # we used the keys of paper number to build it but now keep only the rows
+        return list(status.values())
+
+    def watermark_pages(self, doc: fitz.Document, watermark_text: str) -> None:
         margin = 10
         for pg in doc:
             h = pg.rect.height
@@ -46,7 +119,7 @@ class BuildSolutionService:
             ), f"Text didn't fit: is SID label too long? {watermark_text}"
             pg.draw_rect(wm_rect, color=[0, 0, 0], stroke_opacity=0.25)
 
-    def assemble_solution_for_paper(self, paper_number, watermark=False):
+    def assemble_solution_for_paper(self, paper_number: int, watermark=False) -> bytes:
         try:
             paper_obj = Paper.objects.get(paper_number=paper_number)
         except ObjectDoesNotExist:
@@ -86,3 +159,280 @@ class BuildSolutionService:
                 self.watermark_pages(dest_doc, f"Solutions for {sid_sname_pair[0]}")
 
         return dest_doc.tobytes()
+
+    def reset_single_solution_build(
+        self, paper_num: int, *, wait: Optional[int] = None
+    ) -> None:
+        """Obsolete the solution build of a paper.
+
+        Args:
+            paper_num: The paper number of the solution build task to reset.
+
+        Keyword Args:
+            wait: how long to wait on running chores.  ``None`` is the
+                default which means don't wait.  If you specify an integer,
+                we will wait for that many seconds; raise a HueyException
+                if the chore has not finished in time.  Note ``0`` is not
+                quite the same as ``None`` because ``None`` will not cause
+                an exception.
+
+        Raises:
+            ObjectDoesNotExist: no such paper number or not chore for paper.
+            HueyException: timed out waiting for result.
+        """
+        chore = BuildSolutionPDFChore.objects.filter(
+            obsolete=False, paper__paper_number=paper_num
+        ).get()
+        chore.set_as_obsolete()
+        if chore.status == HueyTaskTracker.QUEUED:
+            queue = get_queue("tasks")
+            queue.revoke_by_id(str(chore.huey_id))
+            chore.set_as_obsolete_with_error("never ran: forcibly dequeued")
+        if chore.status == HueyTaskTracker.RUNNING:
+            if wait is None:
+                print(
+                    f"Note: Chore running: {chore.huey_id};"
+                    " we marked it obsolete but otherwise ignoring"
+                )
+            else:
+                print(f"Chore running: {chore.huey_id}, we will wait for {wait}s...")
+                r = queue.result(
+                    str(chore.huey_id), blocking=True, timeout=wait, preserve=True
+                )
+                print(
+                    f"The running task {chore.huey_id} has finished, and returned {r}"
+                )
+
+    def reset_all_soln_build(self) -> None:
+        """Reset all build soln, including completed ones."""
+        # TODO: future work for a waiting version, see WIP below?
+        wait = None
+        # first cancel all queued chores
+        self.try_to_cancel_all_queued_chores()
+        # any ones that we did not obsolete, we'll get 'em now:
+        for chore in BuildSolutionPDFChore.objects.filter(obsolete=False).all():
+            chore.set_as_obsolete()
+            if chore.status == HueyTaskTracker.RUNNING:
+                if wait is None:
+                    print(
+                        f"Note: Chore running: {chore.huey_id};"
+                        " we marked it obsolete but otherwise ignoring"
+                    )
+                else:
+                    raise NotImplementedError(
+                        f"waiting on the chore running: {chore.huey_id}"
+                    )
+
+    def queue_single_solution_build(self, paper_num: int) -> None:
+        """Create and queue a huey task to build solution for the given paper.
+
+        If the solution-PDF was already built, it will be first made obsolete.
+
+        Args:
+            paper_num: The paper number to build solutions for.
+        """
+        try:
+            paper = Paper.objects.get(paper_number=paper_num)
+        except Paper.DoesNotExist:
+            raise ValueError("No paper with that number") from None
+
+        # mark any existing ones obsolete
+        try:
+            self.reset_single_solution_build(paper_num)
+        except ObjectDoesNotExist:
+            pass
+
+        with transaction.atomic(durable=True):
+            if BuildSolutionPDFChore.objects.filter(
+                paper=paper, obsolete=False
+            ).exists():
+                raise ValueError(
+                    f"There are non-obsolete BuildSolutionPDFChores for papernum {paper_num}:"
+                    " make them obsolete before creating another"
+                )
+            chore = BuildSolutionPDFChore.objects.create(
+                paper=paper,
+                huey_id=None,
+                status=BuildSolutionPDFChore.STARTING,
+            )
+            chore.save()
+            tracker_pk = chore.pk
+
+        res = huey_build_soln_for_paper(
+            paper_num, tracker_pk=tracker_pk, _debug_be_flaky=False
+        )
+        print(f"Just enqueued Huey solution build task id={res.id}")
+        HueyTaskTracker.transition_to_queued_or_running(tracker_pk, res.id)
+
+    def queue_all_solution_builds(self) -> None:
+        """Queue the solution build for all all papers that have been used."""
+        # first work out which papers have been used (ie have any pages)
+        for data in self.get_all_paper_status_for_solution_build():
+            # must be ID'd to be ready for solution build
+            if not data["identified"]:
+                continue
+            # check if already queued or complete
+            # TODO: "Queued" is really `get_status_display` of HueyTaskTracker enum
+            if data["build_soln_status"] == "Queued":
+                continue
+            # TODO: "Complete" is really `get_status_display` of HueyTaskTracker enum
+            if data["build_soln_status"] == "Complete" and not data["outdated"]:
+                # is complete and not outdated
+                continue
+            self.queue_single_solution_build(data["paper_num"])
+
+    @transaction.atomic
+    def get_single_solution_pdf_file(self, paper_number: int) -> File:
+        """Get the django-file of the solution pdf for the given paper.
+
+        Args:
+            paper_number (int): The paper number to re-assemble.
+
+        Returns:
+            File: the django-File of the solution pdf.
+
+        Raises:
+            ObjectDoesNotExist: no such paper or solution build chore, or if
+                the solution build is still in-progress.  TODO: maybe we'd
+                like a different exception for the in-progress case.
+        """
+        chore = BuildSolutionPDFChore.objects.get(
+            paper__paper_number=paper_number,
+            obsolete=False,
+            status=BuildSolutionPDFChore.COMPLETE,
+        )
+        return chore.pdf_file
+
+    def try_to_cancel_single_queued_chore(self, paper_num: int) -> None:
+        """Mark a soln pdf build chore as obsolete and try to cancel it if queued in Huey.
+
+        Args:
+            paper_num: The paper number of the chore to cancel.
+
+        Raises:
+            ObjectDoesNotExist: no such paper number or not chore for paper.
+
+        This is a "best-attempt" at catching soln-build chores while they
+        are queued.  It might be possible for a Chore to sneak past from the
+        "Starting" state.  Already "Running" chores are not effected, although
+        they ARE marked as obsolete.
+        """
+        chore = BuildSolutionPDFChore.objects.get(
+            obsolete=False, paper__paper_number=paper_num
+        )
+        chore.set_as_obsolete()
+        if chore.huey_id:
+            queue = get_queue("tasks")
+            queue.revoke_by_id(str(chore.huey_id))
+        if chore.status in (
+            BuildSolutionPDFChore.STARTING,
+            BuildSolutionPDFChore.QUEUED,
+        ):
+            chore.set_as_obsolete_with_error("never ran: forcibly dequeued")
+
+    def try_to_cancel_all_queued_chores(self) -> int:
+        """Loop over all incomplete chores, marking them obsolete and cancelling (if possible) any in Huey.
+
+        This is a "best-attempt" at catching soln-build chores while they
+        are queued.  It might be possible for a Chore to sneak past from the
+        "Starting" state.  Already "Running" chores are not effected, although
+        they ARE marked as obsolete.
+
+        Completed chores are uneffected.
+
+        Returns:
+            The number of chores that we actually tried to revoke.
+        """
+        N = 0
+        queue = get_queue("tasks")
+        with transaction.atomic(durable=True):
+            for chore in BuildSolutionPDFChore.objects.filter(
+                Q(status=BuildSolutionPDFChore.STARTING)
+                | Q(status=BuildSolutionPDFChore.QUEUED)
+            ):
+                if chore.huey_id:
+                    queue.revoke_by_id(str(chore.huey_id))
+                chore.set_as_obsolete_with_error("never ran: forcibly dequeued")
+                N += 1
+        return N
+
+    @transaction.atomic
+    def get_completed_pdf_files(self) -> List[File]:
+        """Get list of paths of pdf-files of solutions that are not obsolete.
+
+        Returns:
+            A list of django-Files of the reassembled pdf.
+        """
+        return [
+            task.pdf_file
+            for task in BuildSolutionPDFChore.objects.filter(
+                obsolete=False, status=HueyTaskTracker.COMPLETE
+            )
+        ]
+
+    @transaction.atomic
+    def get_zipfly_generator(self, short_name: str, *, chunksize: int = 1024 * 1024):
+        paths = [
+            {
+                "fs": pdf_file.path,
+                "n": pdf_file.name,
+            }
+            for pdf_file in self.get_completed_pdf_files()
+        ]
+
+        zfly = zipfly.ZipFly(paths=paths, chunksize=chunksize)
+        return zfly.generator()
+
+
+# The decorated function returns a ``huey.api.Result``
+# ``context=True`` so that the task knows its ID etc.
+# TODO: investigate "preserve=True" here if we want to wait on them?
+@db_task(queue="tasks", context=True)
+def huey_build_soln_for_paper(
+    paper_number: int, *, tracker_pk: int, task=None, _debug_be_flaky: bool = False
+) -> bool:
+    """Build a solution pdf for a single paper, updating the database with progress and resulting PDF.
+
+    Args:
+        paper_number: which paper to build solutions for.
+
+    Keyword Args:
+        tracker_pk: a key into the database for anyone interested in
+            our progress.
+        task: includes our ID in the Huey process queue.
+        _debug_be_flaky: for debugging, all take a while and some
+            percentage will fail.
+
+    Returns:
+        True, no meaning, just as per the Huey docs: "if you need to
+        block or detect whether a task has finished".
+    """
+    try:
+        Paper.objects.get(paper_number=paper_number)
+    except Paper.DoesNotExist:
+        raise ValueError("No paper with that number") from None
+
+    HueyTaskTracker.transition_to_running(tracker_pk, task.id)
+
+    bss = BuildSolutionService()
+    pdf_as_bytes = bss.assemble_solution_for_paper(paper_number)
+    soln_pdf_name = f"solution_{paper_number}.pdf"
+
+    if _debug_be_flaky:
+        for i in range(5):
+            print(f"Huey sleep i={i}/4: {task.id}")
+            time.sleep(1)
+        roll = random.randint(1, 10)
+        if roll % 5 == 0:
+            raise ValueError(
+                f"DEBUG: deliberately failing solution build for {paper_number}"
+            )
+
+    with transaction.atomic():
+        chore = BuildSolutionPDFChore.objects.select_for_update().get(pk=tracker_pk)
+        if not chore.obsolete:
+            chore.pdf_file = File(io.BytesIO(pdf_as_bytes), name=soln_pdf_name)
+            chore.save()
+
+    HueyTaskTracker.transition_to_complete(tracker_pk)
+    return True
---
+++ plom_server/Finish/views/build_soln_pdf.py
@@ -0,0 +1,110 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2023 Andrew Rechnitzer
+# Copyright (C) 2023 Colin B. Macdonald
+
+from django.shortcuts import render
+from django.http import FileResponse, StreamingHttpResponse
+from django.urls import reverse
+from django.utils.text import slugify
+
+from django_htmx.http import HttpResponseClientRedirect
+
+from Base.base_group_views import ManagerRequiredView
+from ..services import BuildSolutionService
+from Papers.services import SpecificationService
+
+
+class BuildSolutionsView(ManagerRequiredView):
+    def get(self, request):
+        # Note: uses the symbolic constants defined in HueyTaskTracker
+        bss = BuildSolutionService()
+        context = self.build_context()
+        all_paper_status = bss.get_all_paper_status_for_solution_build()
+
+        # Compute some counts required for the page
+        n_papers = sum([1 for x in all_paper_status if x["scanned"]])
+        n_not_ready = sum(
+            [1 for x in all_paper_status if x["scanned"] and not x["identified"]]
+        )
+        n_ready = sum(
+            [
+                1
+                for x in all_paper_status
+                if x["identified"] and x["build_soln_status"] == "To Do"
+            ]
+        )
+        n_outdated = sum([1 for x in all_paper_status if x["outdated"]])
+        n_queued = sum(
+            [
+                1
+                for x in all_paper_status
+                if x["build_soln_status"] in ("Starting", "Queued", "Running")
+            ]
+        )  # for display purposes started === queued
+        n_errors = sum(
+            [1 for x in all_paper_status if x["build_soln_status"] == "Error"]
+        )
+        n_complete = sum(
+            [1 for x in all_paper_status if x["build_soln_status"] == "Complete"]
+        )
+
+        context.update(
+            {
+                "papers": all_paper_status,
+                "n_papers": n_papers,
+                "n_not_ready": n_not_ready,
+                "n_ready": n_ready,
+                "n_outdated": n_outdated,
+                "n_errors": n_errors,
+                "n_complete": n_complete,
+                "n_queued": n_queued,
+            }
+        )
+        return render(request, "Finish/build_soln.html", context=context)
+
+
+class StartOneBuildSoln(ManagerRequiredView):
+    def post(self, request, paper_number):
+        BuildSolutionService().queue_single_solution_build(paper_number)
+        return HttpResponseClientRedirect(reverse("build_soln"))
+
+    def delete(self, request, paper_number):
+        BuildSolutionService().reset_single_solution_build(paper_number)
+        return HttpResponseClientRedirect(reverse("build_soln"))
+
+    def get(self, request, paper_number):
+        return FileResponse(
+            BuildSolutionService().get_single_solution_pdf_file(paper_number)
+        )
+
+    def put(self, request, paper_number):  # called by "re-build_soln"
+        BuildSolutionService().reset_single_solution_build(paper_number)
+        BuildSolutionService().queue_single_solution_build(paper_number)
+        return HttpResponseClientRedirect(reverse("build_soln"))
+
+
+class StartAllBuildSoln(ManagerRequiredView):
+    def post(self, request):
+        BuildSolutionService().queue_all_solution_builds()
+        return HttpResponseClientRedirect(reverse("build_soln"))
+
+    def delete(self, request):
+        BuildSolutionService().reset_all_soln_build()
+        return HttpResponseClientRedirect(reverse("build_soln"))
+
+    def get(self, request):
+        # using zipfly python package.  see django example here
+        # https://github.com/sandes/zipfly/blob/master/examples/streaming_django.py
+        short_name = slugify(SpecificationService.get_shortname())
+        zgen = BuildSolutionService().get_zipfly_generator(short_name)
+        response = StreamingHttpResponse(zgen, content_type="application/octet-stream")
+        response[
+            "Content-Disposition"
+        ] = f"attachment; filename={short_name}_solutions.zip"
+        return response
+
+
+class CancelQueuedBuildSoln(ManagerRequiredView):
+    def delete(self, request):
+        BuildSolutionService().try_to_cancel_all_queued_chores()
+        return HttpResponseClientRedirect(reverse("build_soln"))
