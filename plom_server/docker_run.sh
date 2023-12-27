61b80972dc400d211866e3c432e039c06be1c63d
Colin B. Macdonald <cbm@m.fsf.org>
2023-12-27 00:21:18 -0800

    Leaving comment-out out bit about demo

--- plom_server/docker_run.sh
+++ plom_server/docker_run.sh
@@ -3,6 +3,9 @@
 # Copyright (C) 2023 Edith Coates
 # Copyright (C) 2023 Colin B. Macdonald
 
+# A demo
+# python3 manage.py plom_demo --no-waiting
+
 # A basic server
 python3 manage.py plom_init --no-waiting
 python3 manage.py runserver 0.0.0.0:8000
