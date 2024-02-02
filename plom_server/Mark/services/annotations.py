f0de85faac062b2f87984cdc342c0387acbd2792
Colin B. Macdonald <cbm@m.fsf.org>
2024-02-02 01:03:05 -0800

    lint

--- plom_server/Mark/services/annotations.py
+++ plom_server/Mark/services/annotations.py
@@ -52,7 +52,7 @@ def create_new_annotation_in_database(
         annot_img_md5sum,
         annot_img_file,
     )
-    # implementation details abtracted for testing purposes
+    # implementation details abstracted for testing purposes
     return _create_new_annotation_in_database(task, score, time, annotation_image, data)
 
 
