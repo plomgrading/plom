c8e5516c2c4947b877f9b8d6fc848cd818a84454
Colin B. Macdonald <cbm@m.fsf.org>
2024-02-05 10:20:42 -0800

    Comment about static_root

--- plom_server/Web_Plom/settings.py
+++ plom_server/Web_Plom/settings.py
@@ -217,12 +217,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = "static/"
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
+# TODO: maybe incorrect, didn't work on Django 4.0
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
