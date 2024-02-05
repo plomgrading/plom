07d156ee1b72df4c44f0a473eff723470c993890
Colin B. Macdonald <cbm@m.fsf.org>
2024-02-05 10:30:36 -0800

    Run collectstatic on non-debug run

--- plom_server/Web_Plom/settings.py
+++ plom_server/Web_Plom/settings.py
@@ -220,7 +220,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # TODO: maybe incorrect, didn't work on Django 4.0
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
