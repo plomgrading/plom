badf5ef61339467991de4b14c8f97ac919baead1
Colin B. Macdonald <cbm@m.fsf.org>
2024-02-05 09:48:55 -0800

    Fix static files
    Fixes #3257.

--- plom_server/Web_Plom/settings.py
+++ plom_server/Web_Plom/settings.py
@@ -223,7 +223,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
