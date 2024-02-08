c71d8b36f5ade08d65d88cbbb46cfc98b0e839d8
Colin B. Macdonald <cbm@m.fsf.org>
2024-02-07 22:57:32 -0800

    Cleanup the group creation code
    Don't ask permission before creating groups, either use a try-except or
    use `get_or_create`.

--- plom_server/Authentication/management/commands/plom_create_groups.py
+++ plom_server/Authentication/management/commands/plom_create_groups.py
@@ -2,44 +2,45 @@
 # Copyright (C) 2022 Brennen Chiu
 # Copyright (C) 2022 Edith Coates
 # Copyright (C) 2023 Andrew Rechnitzer
-# Copyright (C) 2023 Colin B. Macdonald
+# Copyright (C) 2023-2024 Colin B. Macdonald
 
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, User
+from django.db import transaction
 
 
 class Command(BaseCommand):
     """Create the user groups.
 
-    This is the command for "python manage.py plom_create_groups"
-    It creates admin, manager, marker, and scanner groups. Then,
-    any superusers will be added to the admin group.
+    This is the command for "python manage.py plom_create_groups".
+    It creates "admin", "manager", "marker", and "scanner" groups, as
+    well as "demo" and "lead_marker".
+
+    Any existing superusers will be automatically added to the admin
+    group (but you can add others later).
     """
 
     def handle(self, *args, **options):
         group_list = ["admin", "manager", "marker", "scanner", "demo", "lead_marker"]
-        # get all the existing groups in a list
-        exist_groups = [str(group) for group in Group.objects.all()]
 
-        # get all the groups
         for group in group_list:
-            if group not in exist_groups:
-                Group(name=group).save()
-                self.stdout.write(f"{group} has been added!")
+            _, created = Group.objects.get_or_create(name=group)
+            if created:
+                self.stdout.write(f'Group "{group}" has been added')
             else:
-                self.stderr.write(f"{group} exist already!")
+                self.stderr.write(f'Group "{group}" already exists')
 
-        # now get the admin group
-        admin_group = Group.objects.get(name="admin")
-        # get all superusers
-        for user in User.objects.filter(is_superuser=True):
-            if user.groups.filter(name="admin").exists():
-                self.stderr.write(
-                    f"Superuser {user.username} is already in the 'admin' group."
-                )
-            else:
-                user.groups.add(admin_group)
-                user.save()
-                self.stdout.write(
-                    f"Added superuser {user.username} to the 'admin' group"
-                )
+        with transaction.atomic():
+            admin_group = Group.objects.get(name="admin")
+            # get all existing superusers and ensure they are in the admin group
+            for user in User.objects.filter(is_superuser=True).select_for_update():
+                if user.groups.filter(name="admin").exists():
+                    self.stderr.write(
+                        f"Superuser {user.username} is already in the 'admin' group."
+                    )
+                else:
+                    user.groups.add(admin_group)
+                    user.save()
+                    self.stdout.write(
+                        f"Added superuser {user.username} to the 'admin' group"
+                    )
