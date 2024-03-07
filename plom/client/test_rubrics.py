1c4d17a6e8bd46490ba79893a3261dcef90d5409
Colin B. Macdonald <cbm@m.fsf.org>
2024-03-07 14:37:07 -0800

    A few rubric tests
    Fixes #3295.

---
+++ plom/client/test_rubrics.py
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2024 Colin B. Macdonald
+
+import re
+
+from plom.client.rubrics import diff_rubric, render_rubric_as_html
+
+
+def _make_ex():
+    return {
+        "id": "123456123456",
+        "username": "xenia",
+        "kind": "absolute",
+        "value": 2,
+        "out_of": 4,
+        "display_delta": "2 of 4",
+        "text": "ABC",
+    }
+
+
+def test_basic_render():
+    html = render_rubric_as_html(_make_ex())
+    assert "ABC" in html
+
+
+def test_rubric_diff():
+    p = _make_ex()
+    r = _make_ex()
+    r.update({"text": "DEF"})
+    same, diff = diff_rubric(p, p)
+    assert same
+    assert diff == ""
+    same, diff = diff_rubric(p, r)
+    assert not same
+    assert re.match(r"(?s).*\-.*ABC", diff)
+    assert re.match(r"(?s).*\+.*DEF", diff)
+
+
+def test_rubric_diff_version_change_issue3295():
+    p = _make_ex()
+    r = _make_ex()
+    p.update({"versions": [1]})
+    r.update({"versions": [1, 2]})
+    same, diff = diff_rubric(p, r)
+    assert not same
+    assert "[1, 2]" in diff
