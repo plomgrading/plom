44c49f39e0eca393f1b175a5736d29ab3e7ada66
Colin B. Macdonald <cbm@m.fsf.org>
2024-03-18 00:31:21 -0700

    Copyright update

--- plom_server/SpecCreator/views/template_spec_builder.py
+++ plom_server/SpecCreator/views/template_spec_builder.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2023 Andrew Rechnitzer
+# Copyright (C) 2024 Colin B. Macdonald
 
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
