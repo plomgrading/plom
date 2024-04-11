2acb8ae77aa543613e8cf482c76c3b7a10174b83
Colin B. Macdonald <cbm@m.fsf.org>
2024-04-11 10:52:02 -0700

    Port tests to test_config
    This means its ok to fail when upload is out of version range (which is
    currently unchecked).

--- plom_server/Preparation/tests/test_source_service.py
+++ plom_server/Preparation/tests/test_source_service.py
@@ -15,28 +15,54 @@ from django.core.exceptions import MultipleObjectsReturned
 from django.conf import settings
 from model_bakery import baker
 
+from Base.tests import config_test
 from ..services import SourceService
 from ..models import PaperSourcePDF
 from .. import useful_files_for_testing as useful_files
 
 
 class SourceServiceTests(TestCase):
-    def test_store_source_pdf(self) -> None:
-        upload_path = resources.files(useful_files) / "test_version1.pdf"
-        # TODO: this writes to settings.MEDIA_ROOT
-        # TODO: would one normally use a special "tests" settings?
-        SourceService.store_source_pdf(1, upload_path)
+    @config_test({"test_spec": "config_files/tiny_spec.toml"})
+    def test_store_and_list_source_pdfs(self) -> None:
+        ver1pdf = resources.files(useful_files) / "test_version1.pdf"
+        ver2pdf = resources.files(useful_files) / "test_version2.pdf"
 
+        d = SourceService.get_list_of_sources()
+        assert isinstance(d, list)
+        v1, v2 = d
+        assert v1["version"] == 1
+        assert v2["version"] == 2
+        for src in d:
+            assert not src["uploaded"]
+
+        SourceService.store_source_pdf(1, ver1pdf)
+        d = SourceService.get_list_of_sources()
+        v1, v2 = d
+        assert v1["uploaded"]
+        assert not v2["uploaded"]
+
+        SourceService.store_source_pdf(2, ver2pdf)
+        d = SourceService.get_list_of_sources()
+        v1, v2 = d
+        assert v1["uploaded"]
+        assert v2["uploaded"]
+
+        SourceService.delete_source_pdf(1)
+        d = SourceService.get_list_of_sources()
+        v1, v2 = d
+        assert not v1["uploaded"]
+        assert v2["uploaded"]
         n_sources = SourceService.how_many_source_versions_uploaded()
         self.assertEqual(n_sources, 1)
 
-        with self.assertRaises(MultipleObjectsReturned):
-            SourceService.store_source_pdf(1, upload_path)
-
         SourceService.delete_all_source_pdfs()
+        d = SourceService.get_list_of_sources()
+        for src in d:
+            assert not src["uploaded"]
         n_sources = SourceService.how_many_source_versions_uploaded()
         self.assertEqual(n_sources, 0)
 
+    @config_test({"test_spec": "config_files/tiny_spec.toml"})
     def test_store_source_pdf_location(self) -> None:
         upload_path = resources.files(useful_files) / "test_version1.pdf"
         SourceService.store_source_pdf(1, upload_path)
@@ -48,26 +74,27 @@ class SourceServiceTests(TestCase):
         # This test is sus: DB might store as version1_abc123.pdf
         # assert f.path == pdf_source_path / "version1.pdf"
 
-    def test_delete_existing_source_pdf_by_version(self) -> None:
+    @config_test({"test_spec": "config_files/tiny_spec.toml"})
+    def test_store_source_pdf_already_there(self) -> None:
         upload_path = resources.files(useful_files) / "test_version1.pdf"
         SourceService.store_source_pdf(1, upload_path)
-        SourceService.delete_source_pdf(1)
-        self.assertEqual(SourceService.how_many_source_versions_uploaded(), 0)
+        with self.assertRaises(MultipleObjectsReturned):
+            SourceService.store_source_pdf(1, upload_path)
+        n_sources = SourceService.how_many_source_versions_uploaded()
+        self.assertEqual(n_sources, 1)
 
     def test_delete_non_existing_source_pdf(self) -> None:
         # documented as a non-error
         SourceService.delete_source_pdf(1)
 
-    def test_source_pdf_misc(self) -> None:
+    @config_test({"test_spec": "config_files/tiny_spec.toml"})
+    def test_source_pdf_list_has_hash(self) -> None:
         upload_path = resources.files(useful_files) / "test_version1.pdf"
         SourceService.store_source_pdf(1, upload_path)
         d = SourceService.get_list_of_sources()
-        assert isinstance(d, list)
-        assert len(d) >= 1
-        assert isinstance(d[0], dict)
-        assert d[0]["version"] == 1
         assert len(d[0]["hash"]) > 50
 
+    @config_test({"test_spec": "config_files/tiny_spec.toml"})
     def test_get_as_bytes(self) -> None:
         upload_path = resources.files(useful_files) / "test_version1.pdf"
         original_bytes = upload_path.read_bytes()
