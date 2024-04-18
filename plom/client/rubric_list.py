2087db99a197cc0dc95e021940a9e389ee603dd9
Colin B. Macdonald <cbm@m.fsf.org>
2024-04-18 12:36:03 -0700

    split the conflict dialog to its own file

--- plom/client/rubric_list.py
+++ plom/client/rubric_list.py
@@ -7,7 +7,6 @@
 # Copyright (C) 2021 Forest Kobayashi
 
 from datetime import datetime
-import html
 import logging
 from typing import Any, Dict, List, Optional, Sequence, Tuple, Union
 
@@ -37,6 +36,7 @@ from .useful_classes import BigMessageDialog
 from .rubric_wrangler import RubricWrangler
 from .rubrics import compute_score, diff_rubric, render_rubric_as_html
 from .rubric_add_dialog import AddRubricBox
+from .rubric_conflict_dialog import RubricConflictDialog
 from plom.plom_exceptions import (
     PlomConflict,
     PlomInconsistentRubric,
@@ -1717,55 +1717,12 @@ class RubricWidget(QWidget):
                 ErrorMsg(self, f"{e}").exec()
                 return
             except PlomConflict as e:
-                tmp_rubrics = self._parent.getRubricsFromServer()
+                _tmp_rubrics = self._parent.getRubricsFromServer()
                 (old_rubric,) = (r for r in self.rubrics if r["id"] == new_rubric["id"])
-                (their_rubric,) = (
-                    r for r in tmp_rubrics if r["id"] == new_rubric["id"]
-                )
-                same, their_diff = diff_rubric(old_rubric, their_rubric)
-                # server hasn't seen it to change timestamp, so we'll (temporarily) do
-                # it ourselves.  TODO: might want to revisit once we can choose "ours"
-                new_rubric["last_modified"] = str(arrow.now())
-                same, our_diff = diff_rubric(old_rubric, new_rubric)
-                InfoMsg(
-                    self,
-                    f"""
-                        <h3>Rubric change conflict</h3>
-                        <br />
-                        <quote>
-                        <small>
-                        <tt>{html.escape(str(e))}</tt>
-                        </small>
-                        </quote>
-                        <br />
-                        <table><tr>
-                        <td style="padding-right: 2ex; border-right-width: 1px; border-right-style: solid;">
-                          <h4>Their's</h4>
-                          {render_rubric_as_html(their_rubric)}
-                        </td>
-                        <td style="padding-left: 2ex;">
-                          <h4>Your's</h4>
-                          {render_rubric_as_html(new_rubric)}
-                        </td>
-                        </tr>
-                        <tr>
-                        <td style="padding-right: 2ex; border-right-width: 1px; border-right-style: solid;">
-                          <b>changes</b><br />
-                          {their_diff}
-                        </td>
-                        <td style="padding-left: 2ex;">
-                          <b>changes</b><br />
-                          {our_diff}
-                        </td>
-                        </tr></table>
-                        <p>
-                          This is work-in-progress,
-                          for now we always keep &ldquo;Their's&rdquo;.
-                        </p>
-                    """,
-                    # "Do you want to keep their's or your's?",
+                (theirs,) = (r for r in _tmp_rubrics if r["id"] == new_rubric["id"])
+                RubricConflictDialog(
+                    self, str(e), theirs, new_rubric, old_rubric
                 ).exec()
-                # TODO: future buttons: [Cancel (and keep theirs)], [further edit theirs], [force submit your's], [edit your's]
                 return
             # update the rubric in the current internal rubric list
             # make sure that keys match.
