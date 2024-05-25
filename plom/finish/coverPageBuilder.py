dfcb77eb482db56667c444a61928c0d9e9e15899
Bryan Tanady <btanady@Reincarnated>
2024-05-25 14:41:33 -0700

    Fixed Interchanged sid, sname

--- plom/finish/coverPageBuilder.py
+++ plom/finish/coverPageBuilder.py
@@ -88,7 +88,7 @@ def makeCover(
     tw.append((m, vpos), text, fontsize=big_font)
     bullet = "\N{Bullet}"
     if info:
-        sname, sid = info
+        sid, sname = info
         if sname is None:
             sname = "Not ID'd yet"
         if sid is None:
