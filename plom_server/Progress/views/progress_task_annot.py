89da3c86f84458be6881837d257420158b0b6cb3
Bryan Tanady <btanady@Reincarnated>
2024-06-04 15:07:20 -0700

    Copyright

--- plom_server/Progress/views/progress_task_annot.py
+++ plom_server/Progress/views/progress_task_annot.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2023-2024 Andrew Rechnitzer
 # Copyright (C) 2024 Colin B. Macdonald
+# Copyright (C) 2024 Bryan Tanady
 
 from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse, FileResponse
