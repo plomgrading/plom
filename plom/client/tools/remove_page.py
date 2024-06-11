1350c3919a07568c3bbfb8846f41e735705d090b
Colin B. Macdonald <cbm@m.fsf.org>
2024-06-11 09:37:27 -0700

    Forgot to checkin

---
+++ plom/client/tools/remove_page.py
@@ -0,0 +1,106 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2024 Colin B. Macdonald
+
+from PyQt6.QtCore import QRectF, QObject
+from PyQt6.QtCore import QPropertyAnimation, QAbstractAnimation
+from PyQt6.QtCore import pyqtProperty  # type: ignore[attr-defined]
+from PyQt6.QtGui import QBrush, QPen, QUndoCommand
+from PyQt6.QtWidgets import QGraphicsRectItem
+
+from plom.client.tools import log
+from plom.client.tools import (
+    AnimationPenColour,
+    AnimationPenThickness,
+    AnimationFillColour,
+)
+from .shift_page import Duration
+
+
+class CommandRemovePage(QUndoCommand):
+    """Remove or undo removing a page, not including cleanup moves of annotations."""
+
+    def __init__(self, scene, src_idx: int, page_image_idx: int) -> None:
+        # scene type is plom.client.pagescene.PageScene
+        super().__init__()
+        self.scene = scene
+        self.src_idx = src_idx
+        self.page_image_idx = page_image_idx
+        self.setText("RotatePage")
+
+    def redo(self):
+        # TODO: is this the correct boundary?
+        img = self.scene.underImage.images[self.page_image_idx]
+        r = img.mapRectToScene(img.boundingRect())
+        self.scene._set_visible_page_image(self.src_idx, show=False)
+        # temporary animation, removes itself when done
+        self.scene.addItem(TmpAnimDisappearingRectItem(self.scene, r))
+
+    def undo(self):
+        self.scene._set_visible_page_image(self.src_idx, show=True)
+        # TODO: how to we figure out the index?
+        img = self.scene.underImage.images[self.page_image_idx]
+        r = img.mapRectToScene(img.boundingRect())
+        # temporary animation, removes itself when done
+        self.scene.addItem(TmpAnimDisappearingRectItem(self.scene, r, restore=True))
+
+
+# see comments about this class in `shift_page.py`
+class TmpAnimDisappearingRectItem(QGraphicsRectItem):
+    def __init__(self, scene, r: QRectF, *, restore: bool = False) -> None:
+        super().__init__()
+        self._scene = scene
+        self.saveable = False
+        self.setRect(r)
+        self.setPen(QPen(AnimationPenColour, AnimationPenThickness))
+        self.setBrush(QBrush(AnimationFillColour))
+
+        # Crashes when calling our methods (probably b/c QGraphicsItem
+        # is not QObject).  Instead we use a helper class.
+        self._ctrlr = _AnimatorCtrlr(self)
+        self.anim = QPropertyAnimation(self._ctrlr, b"foo")
+
+        self.anim.setDuration(Duration)
+        if restore:
+            self.anim.setStartValue(1)
+            self.anim.setEndValue(0)
+        else:
+            self.anim.setStartValue(0)
+            self.anim.setEndValue(1)
+        # When the animation finishes, it will destroy itself, whence
+        # we'll get a callback to remove ourself from the scene
+        self.anim.destroyed.connect(self.remove_from_scene)
+        self.anim.start(QAbstractAnimation.DeletionPolicy.DeleteWhenStopped)
+
+    def remove_from_scene(self) -> None:
+        log.debug(f"TmpAnimItem: removing {self} from scene")
+        # TODO: can we be sure that scene survives until the end of the animation?
+        # TODO: also, what if the scene removes the item early?
+        self._scene.removeItem(self)
+
+    def interp(self, t: float) -> None:
+        """Draw a rectangle part way between r and zooming out to nothing.
+
+        Args:
+            t: a value in [0, 1].
+
+        Returns:
+            None
+        """
+        self.setTransformOriginPoint(self.boundingRect().center())
+        self.setScale(1 - t)
+
+
+class _AnimatorCtrlr(QObject):
+    def __init__(self, item: TmpAnimDisappearingRectItem) -> None:
+        super().__init__()
+        self.item = item
+
+    _foo = -1.0  # unused, but the animator expects getter/setter
+
+    @pyqtProperty(float)
+    def foo(self) -> float:
+        return self._foo
+
+    @foo.setter  # type: ignore[no-redef]
+    def foo(self, t: float) -> None:
+        self.item.interp(t)
