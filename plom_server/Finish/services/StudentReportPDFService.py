fa255daca04a697c27f907a79036b02ad7b740ef
Bryan Tanady <btanady@Reincarnated>
2024-06-12 21:16:59 -0700

    Removed tqdm

--- plom_server/Finish/services/StudentReportPDFService.py
+++ plom_server/Finish/services/StudentReportPDFService.py
@@ -7,7 +7,6 @@ from datetime import datetime
 from typing import Any, Dict, Optional
 from textwrap import dedent
 
-from tqdm import tqdm as _tqdm
 from weasyprint import HTML, CSS
 
 from Mark.services import MarkingTaskService
@@ -15,16 +14,11 @@ from Papers.services import SpecificationService
 from . import DataExtractionService, MatplotlibService
 
 
-def _identity_in_first_input(x, *args, **kwargs):
-    return x
-
-
 def pdf_builder(
     versions: bool,
     sid: Optional[str],
     *,
     verbose: Optional[bool] = None,
-    _use_tqdm: bool = False,
 ) -> Dict[str, Any]:
     """Build a Student Report PDF file report and return it as bytes.
 
@@ -34,7 +28,6 @@ def pdf_builder(
 
     Keyword Args:
         verbose: print messages on the stdout.
-        _use_tqdm: even more verbosity: use tqdm for progress bars.
 
     Returns:
         A dictionary with the bytes of a PDF file, a suggested
@@ -45,9 +38,6 @@ def pdf_builder(
             is incomplete, because the pandas library uses NaN for
             missing data.
     """
-    tqdm: Any = _identity_in_first_input
-    if _use_tqdm:
-        tqdm = _tqdm
 
     if verbose:
         print("Building report.")
@@ -91,10 +81,8 @@ def pdf_builder(
     # histogram of grades for each question
     histogram_of_grades_q = []
     marks_for_questions = des._get_marks_for_all_questions()
-    for _q, _ in tqdm(
-        enumerate(marks_for_questions),
-        desc="Histograms of marks by question",
-    ):
+    for _q, _ in enumerate(marks_for_questions):
+
         question_idx = _q + 1  # 1-indexing
         histogram_of_grades_q.append(  # add to the list
             # each base64-encoded image
@@ -115,31 +103,6 @@ def pdf_builder(
         <p style="break-before: page;"></p>
         <h3>{title}</h3>
         """
-        return out
-
-    def _html_for_graphs(list_of_graphs: list) -> str:
-        """Generate HTML for a list of graphs."""
-        out = ""
-        odd = 0
-        for i, graph in enumerate(list_of_graphs):
-            odd = i % 2
-            if not odd:
-                out += """
-                <div class="row">
-                """
-            out += f"""
-            <div class="col" style="margin-left:0mm;">
-            <img src="data:image/png;base64,{graph}" width="50px" height="40px" />
-            </div>
-            """
-            if odd:
-                out += """
-                </div>
-                """
-        if not odd:
-            out += """
-            </div>
-            """
         return out
 
     def _html_for_big_graphs(list_of_graphs: list) -> str:
