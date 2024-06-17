a541d0d7edda9d118a842c306ee2704f2ce0d417
21chanas3 <adenchan@gmail.com>
2024-06-17 07:08:24 -0700

    reset pages

--- plom_server/Base/views.py
+++ plom_server/Base/views.py
@@ -1,9 +1,13 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2024 Colin B. Macdonald
+# Copyright (C) 2024 Aden Chan
 
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views.generic import View
+from Base.base_group_views import ManagerRequiredView
+from .forms import CompleteWipeForm
 
 
 class TroublesAfootGenericErrorView(View):
@@ -28,3 +32,28 @@ class TroublesAfootGenericErrorView(View):
         """
         context = {"hint": hint}
         return render(request, "base/troubles_afoot.html", context)
+    
+class ResetView(ManagerRequiredView):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        return render(request, "base/reset.html")
+
+class ResetConfirmView(ManagerRequiredView):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        context = self.build_context()
+        form = CompleteWipeForm()
+        context.update({"wipe_form": form})
+        return render(request, "base/reset_confirm.html", context=context)
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        context = self.build_context()
+        form = CompleteWipeForm(request.POST)
+        reset_phrase = "I wish to completely delete this Plom instance."
+        _confirm_field = "confirmation_field"
+        if form.is_valid():
+            if form.cleaned_data[_confirm_field] == reset_phrase:
+                #Call the master reset function here
+                messages.success(request, "Plom instance successfully wiped.")
+                return redirect("home")
+            form.add_error(_confirm_field, "Phrase is incorrect")
+        context.update({"wipe_form": form})
+        return render(request, "base/reset_confirm.html", context=context)
