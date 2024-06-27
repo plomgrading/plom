d6d2b91d1469489baeb9f65c89731e561e84d809
21chanas3 <adenchan@gmail.com>
2024-06-27 15:50:33 -0700

    copyright

--- plom_server/Preparation/services/extra_page.py
+++ plom_server/Preparation/services/extra_page.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2023 Andrew Rechnitzer
 # Copyright (C) 2023 Colin B. Macdonald
+# Copyright (C) 2024 Aden Chan
 
 import logging
 from pathlib import Path
