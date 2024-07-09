da163aa66c850d79870565dc62413a499023dab2
Andrew Rechnitzer <andrewr@math.ubc.ca>
2024-07-08 21:36:54 -0700

    Minor fixes - commented out-of-date tests

--- plom_server/Preparation/tests/test_status.py
+++ plom_server/Preparation/tests/test_status.py
@@ -16,39 +16,42 @@ from unittest import skip
 
 # TODO - all of these test need rewriting for the newer
 # method of dependency checking
-@skip
+@skip("These tests need rewriting after the updates for dependency checking.")
 class PapersPrintedSettingTests(TestCase):
-    def test_can_be_set_as_printed(self) -> None:
-        """Test can_status_be_set_true.
+    # Commenting these out until they get rewritten
+    # for the new dependency checking method
 
-        Should return false if the Paper table is empty.
-        """
-        self.assertFalse(PapersPrinted.can_status_be_set_true())
-
-        baker.make(Paper)
-
-        self.assertTrue(PapersPrinted.can_status_be_set_true())
-
-    def test_can_be_set_false(self) -> None:
-        """Test can_status_be_set_false.
-
-        Should return false if the Bundle table isn't empty.
-        """
-        self.assertTrue(PapersPrinted.can_status_be_set_false())
-
-        baker.make(Bundle)
-
-        self.assertFalse(PapersPrinted.can_status_be_set_false())
-
-    def test_papers_have_been_printed(self) -> None:
-        """Test the have_been_printed getter function."""
-        self.assertFalse(PapersPrinted.have_papers_been_printed())
-
-        setting_obj = PapersPrintedSettingModel.load()
-        setting_obj.have_printed_papers = True
-        setting_obj.save()
-
-        self.assertTrue(PapersPrinted.have_papers_been_printed())
+    # def test_can_be_set_as_printed(self) -> None:
+    #     """Test can_status_be_set_true.
+    #
+    #     Should return false if the Paper table is empty.
+    #     """
+    #     self.assertFalse(PapersPrinted.can_status_be_set_true())
+    #
+    #     baker.make(Paper)
+    #
+    #     self.assertTrue(PapersPrinted.can_status_be_set_true())
+    #
+    # def test_can_be_set_false(self) -> None:
+    #     """Test can_status_be_set_false.
+    #
+    #     Should return false if the Bundle table isn't empty.
+    #     """
+    #     self.assertTrue(PapersPrinted.can_status_be_set_false())
+    #
+    #     baker.make(Bundle)
+    #
+    #     self.assertFalse(PapersPrinted.can_status_be_set_false())
+    #
+    # def test_papers_have_been_printed(self) -> None:
+    #     """Test the have_been_printed getter function."""
+    #     self.assertFalse(PapersPrinted.have_papers_been_printed())
+    #
+    #     setting_obj = PapersPrintedSettingModel.load()
+    #     setting_obj.have_printed_papers = True
+    #     setting_obj.save()
+    #
+    #     self.assertTrue(PapersPrinted.have_papers_been_printed())
 
     def test_papers_printed(self) -> None:
         """Test the papers_printed setter function."""
