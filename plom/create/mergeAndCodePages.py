ce4b445db93b18b3e52fce92483a9aa1dbfcbe48
Colin B. Macdonald <cbm@m.fsf.org>
2024-07-11 10:08:07 -0700

    Use MuPDF native font subsetting on recent version
    Fixes #3384.

--- plom/create/mergeAndCodePages.py
+++ plom/create/mergeAndCodePages.py
@@ -16,8 +16,9 @@ from pathlib import Path
 from typing import Any
 
 # import pyqrcode
-import segno
 import fitz
+from packaging import Version
+import segno
 
 from plom.create import paperdir
 from plom.specVerifier import (
@@ -481,14 +482,17 @@ def make_PDF(
             do_subset = True
 
     if do_subset:
-        # TODO: remove this fallback on a future PyMuPDF (see Plom Issue #3384)
-        # Fallback subsetting requires https://pypi.org/project/fonttools
-        try:
-            exam.subset_fonts(fallback=True)
-        except TypeError:
-            # PyMuPDF<=1.23 does not have fallback
-            # Subsetting requires https://pypi.org/project/fonttools
+        if Version(fitz.version[0]) >= Version("1.24.6"):
             exam.subset_fonts()
+        else:
+            # TODO: remove after minimum PyMuPDF is bumped (see Plom Issue #3384)
+            # Fallback subsetting requires https://pypi.org/project/fonttools
+            try:
+                exam.subset_fonts(fallback=True)
+            except TypeError:
+                # PyMuPDF<=1.23 does not have fallback
+                # Subsetting requires https://pypi.org/project/fonttools
+                exam.subset_fonts()
 
     # Add the deflate option to compress the embedded pngs
     # see https://pymupdf.readthedocs.io/en/latest/document/#Document.save
