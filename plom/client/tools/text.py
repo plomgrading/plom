99c6d55babdd998943bac467ed088e82d9eae404
tanadybryan <tanadybryan@gmail.com>
2024-07-18 13:23:32 -0700

    Changed opacity and changed IBeam to Arrow

--- plom/client/tools/text.py
+++ plom/client/tools/text.py
@@ -309,6 +309,7 @@ class GhostText(QGraphicsTextItem):
         font = QFont("Helvetica")
         font.setPixelSize(round(fontsize))
         self.setFont(font)
+        self.setOpacity(0.7)
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
         self.setTextInteractionFlags(Qt.TextInteractionFlag.NoTextInteraction)
         # If displaying png-rendered-latex, store the original text here
