e3ed53e779ecc93e11c1968834e6400df654fd31
Colin B. Macdonald <cbm@m.fsf.org>
2024-07-29 07:48:35 -0700

    minor change to test for more precision

--- plom/test_spec.py
+++ plom/test_spec.py
@@ -68,7 +68,8 @@ def test_spec_question_pages_non_positive():
 
 def test_spec_pages_not_even():
     r = deepcopy(raw)
-    r["numberOfPages"] = 17
+    r["numberOfPages"] = 7
+    r["question"]["3"]["pages"] = [5, 6, 7]
     with raises(ValueError, match="even"):
         SpecVerifier(r).verify()
 
