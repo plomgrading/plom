cc486d7619e8296b815dab01b922ee21c9e8333e
Colin B. Macdonald <cbm@m.fsf.org>
2024-08-05 12:14:45 -0700

    fix up demo path hack

--- plom_server/demo_path_hack.sh
+++ plom_server/demo_path_hack.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2022-2023 Colin B. Macdonald
+# Copyright (C) 2022-2024 Colin B. Macdonald
 # Copyright (C) 2022 Edith Coates
 # Copyright (C) 2022 Brennen Chiu
 # Copyright (C) 2023 Andrew Rechnitzer
@@ -9,6 +9,11 @@
 
 set -e
 
+# able to get the `plom` module even if not installed.
 export PYTHONPATH=".."
 
-python3 manage.py plom_demo $@
+# old way
+# python3 manage.py plom_demo $@
+
+# new way
+./Launcher/launch_scripts/launch_plom_demo_server.py $@
