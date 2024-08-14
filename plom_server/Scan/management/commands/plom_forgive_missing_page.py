8034d178afae579443cd59c48956a4b86027ba25
Andrew Rechnitzer <andrewr@math.ubc.ca>
2024-08-13 21:04:09 -0700

    Got commandline working

--- plom_server/Scan/management/commands/plom_forgive_missing_page.py
+++ plom_server/Scan/management/commands/plom_forgive_missing_page.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2023-204 Andrew Rechnitzer
+# Copyright (C) 2024 Andrew Rechnitzer
 
 from __future__ import annotations
 
@@ -9,5 +9,24 @@ from ...services import ForgiveMissingService
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        """Define arguments for the management command."""
+        parser.add_argument(
+            "username",
+            type=str,
+            help="The username of the user forgiving the missing page",
+        )
+        parser.add_argument(
+            "paper_number", type=int, help="The paper which is missing the page"
+        )
+        parser.add_argument(
+            "page_number", type=int, help="The page missing from the paper"
+        )
+
     def handle(self, *args, **opt) -> None:
-        ForgiveMissingService.forgive_missing_fixed_page_cmd("manager", 4, 5)
+        try:
+            ForgiveMissingService.forgive_missing_fixed_page_cmd(
+                opt["username"], opt["paper_number"], opt["page_number"]
+            )
+        except ValueError as err:
+            raise CommandError(err)
