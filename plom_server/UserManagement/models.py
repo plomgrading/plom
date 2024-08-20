7607272fbd39ee7b377fbfadefe06a2bd8bdc4d6
Colin B. Macdonald <cbm@m.fsf.org>
2024-08-19 19:30:33 -0700

    increase default quota

--- plom_server/UserManagement/models.py
+++ plom_server/UserManagement/models.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2024 Elisa Pan
+# Copyright (C) 2024 Colin B. Macdonald
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -8,7 +9,7 @@ from django.contrib.auth.models import User
 class ProbationPeriod(models.Model):
     """Represents a probation period of a marker with its limit."""
 
-    default_limit = 5
+    default_limit = 12
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     limit = models.IntegerField(default=default_limit)
 
