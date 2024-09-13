1cd3f5b8568a69071055fa4a272089d0f7930d80
Colin B. Macdonald <cbm@m.fsf.org>
2024-09-12 20:48:41 -0700

    two files with missing copyright headers

--- plom_server/Base/apps.py
+++ plom_server/Base/apps.py
@@ -1,3 +1,8 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2022 Brennen Chiu
+# Copyright (C) 2022 Edith Coates
+# Copyright (C) 2024 Colin B. Macdonald
+
 from django.apps import AppConfig
 
 
--- plom_server/Rubrics/apps.py
+++ plom_server/Rubrics/apps.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2023 Edith Coates
+# Copyright (C) 2024 Colin B. Macdonald
+
 from django.apps import AppConfig
 
 
