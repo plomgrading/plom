f2a23e9b299dd1f6eeb8e99b452a9f967ddeb5d9
Colin B. Macdonald <cbm@m.fsf.org>
2024-09-23 14:35:18 -0700

    copyright

--- plom_server/Scan/management/commands/plom_bundle_push_lock.py
+++ plom_server/Scan/management/commands/plom_bundle_push_lock.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2023 Andrew Rechnitzer
+# Copyright (C) 2023-2024 Andrew Rechnitzer
+# Copyright (C) 2024 Colin B. Macdonald
 
 from tabulate import tabulate
 
