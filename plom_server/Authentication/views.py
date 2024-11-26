80ecbbeba12a07601d0ea790a3b97883fd41286a
Aidan Murphy <aidanpmurphy@hotmail.com>
2024-11-26 05:04:59 +0000

    Users - misc tidying/rewrites

--- plom_server/Authentication/views.py
+++ plom_server/Authentication/views.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2022 Edith Coates
 # Copyright (C) 2022-2024 Colin B. Macdonald
 # Copyright (C) 2022 Natalie Balashov
+# Copyright (C) 2024 Aidan Murphy
 
 from __future__ import annotations
 
@@ -20,15 +21,15 @@ from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_str
 from django.views.generic import View
 from braces.views import GroupRequiredMixin
-from bs4 import BeautifulSoup
 
 from Base.base_group_views import (
     RoleRequiredView,
 )
 
 
-# Set User Password
 class SetPassword(View):
+    """Setting or resetting user passwords."""
+
     template_name = "Authentication/set_password.html"
     reset_invalid = "Authentication/activation_invalid.html"
     set_password_complete = "Authentication/set_password_complete.html"
@@ -41,6 +42,7 @@ class SetPassword(View):
     ]
 
     def get(self, request: HttpRequest, uidb64: str, token: str) -> HttpResponse:
+        """The Password setting page."""
         try:
             uid = force_str((urlsafe_base64_decode(uidb64)))
             user = User.objects.get(pk=uid)
@@ -61,68 +63,33 @@ class SetPassword(View):
             return render(request, self.reset_invalid)
 
     def post(self, request: HttpRequest, uidb64: str, token: str) -> HttpResponse:
+        """Attempt to set a user's password."""
         try:
             uid = force_str((urlsafe_base64_decode(uidb64)))
             user = User.objects.get(pk=uid)
         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
             user = None
-        if user is not None and default_token_generator.check_token(user, token):
-            reset_form = SetPasswordForm(user, request.POST)
 
-            # scanner and marker group
-            if (
-                user.groups.filter(name="scanner").exists()
-                or user.groups.filter(name="marker").exists()
-            ):
-                if reset_form.is_valid():
-                    user = reset_form.save()
-                    user.is_active = True
-                    user.profile.signup_confirmation = True
-                    user.save()
-                    return render(request, self.set_password_complete)
-                # display error message
-                else:
-                    tri_quote = '"""'
-                    error_message = tri_quote + str(reset_form.errors) + tri_quote
-                    parsed_error = BeautifulSoup(error_message, "html.parser")
-                    # MyPy unhappy with this code, me too a bit, hardcoded 13?!
-                    assert parsed_error is not None
-                    assert parsed_error.li is not None
-                    error = parsed_error.li.text[13:]
-                    context = {
-                        "username": user.username,
-                        "form": reset_form,
-                        "help_text": SetPassword.help_text,
-                        "error": error,
-                    }
-                    return render(request, self.template_name, context)
-
-            # manager group
-            else:
-                if reset_form.is_valid():
-                    user = reset_form.save()
-                    user.is_active = True
-                    user.profile.signup_confirmation = True
-                    user.save()
-                    return render(request, self.set_password_complete)
-                # display error message
-                else:
-                    tri_quote = '"""'
-                    error_message = tri_quote + str(reset_form.errors) + tri_quote
-                    parsed_error = BeautifulSoup(error_message, "html.parser")
-                    # MyPy unhappy with this code, me too a bit, hardcoded 13?!
-                    assert parsed_error is not None
-                    assert parsed_error.li is not None
-                    error = parsed_error.li.text[13:]
-                    context = {
-                        "username": user.username,
-                        "form": reset_form,
-                        "help_text": SetPassword.help_text,
-                        "error": error,
-                    }
-                    return render(request, self.template_name, context)
-        else:
+        if user is None or not default_token_generator.check_token(user, token):
             return render(request, self.reset_invalid, status=403)
+
+        reset_form = SetPasswordForm(user, request.POST)
+
+        if reset_form.is_valid():
+            user = reset_form.save()
+            user.is_active = True
+            user.profile.signup_confirmation = True
+            user.save()
+            return render(request, self.set_password_complete)
+        else:
+            error_dict = dict(reset_form.errors)
+            context = {
+                "username": user.username,
+                "form": reset_form,
+                "help_text": SetPassword.help_text,
+                "error_dict": error_dict,
+            }
+            return render(request, self.template_name, context)
 
 
 # When user enters their password successfully
@@ -151,32 +118,37 @@ class LoginView(View):
     template_name = "Authentication/login.html"
 
     def get(self, request):
+        """The login page."""
         if request.user.is_authenticated:
             return redirect("home")
         return render(request, self.template_name)
 
     def post(self, request):
+        """Attempt to authenticate and log in."""
         if request.user.is_authenticated:
             return redirect("home")
-        else:
-            username = request.POST.get("username")
-            temp_username = User.objects.filter(username__iexact=username).values()
-            if not temp_username.exists():
-                messages.info(request, "User does not exist!")
-                return render(request, self.template_name)
-            password = request.POST.get("password")
-            user = authenticate(
-                request, username=temp_username[0]["username"], password=password
-            )
-            if user is not None:
-                login(request, user)
-                if "next" in request.POST:
-                    return redirect(request.POST.get("next"))
-                else:
-                    return redirect("home")
-            else:
-                messages.info(request, "Username or Password is incorrect!")
+
+        username = request.POST.get("username")
+        """be wary making db calls for unauthenticated users, see #3733
+        temp_username = User.objects.filter(username__iexact=username).values()
+        if not temp_username.exists():
+            messages.info(request, "User does not exist!")
             return render(request, self.template_name)
+        """
+        user = authenticate(
+            request,
+            username=username,
+            password=request.POST.get("password"),
+        )
+        if user is None:
+            messages.info(request, "Username or Password is incorrect!")
+            return render(request, self.template_name)
+
+        login(request, user)
+        if "next" in request.POST:
+            return redirect(request.POST.get("next"))
+        else:
+            return redirect("home")
 
 
 # Logout User
