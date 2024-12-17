c0621791efa08474ae83a46ba934daadcbbbefba
Colin B. Macdonald <cbm@m.fsf.org>
2024-12-16 21:58:14 -0800

    re-factor HTML/view for choosing questions/all/dnm

--- plom_server/Scan/views/cast_image_state.py
+++ plom_server/Scan/views/cast_image_state.py
@@ -269,16 +269,26 @@ class ExtraliseImageView(ScannerRequiredView):
                 """<span class="alert alert-danger">Invalid paper number</span>"""
             )
 
-        if extra_page_data.get("questionAll", "off") == "all":
+        choice = extra_page_data.get("question_all_dnm", "")
+        if choice == "choose_all":
             # set all the questions
             to_questions = SpecificationService.get_question_indices()
-        else:
+        elif choice == "choose_dnm":
+            # TODO: or explicitly empty list or ...?
+            to_questions = []
+        elif choice == "choose_q":
             # caution: `get` would return just the last entry
             to_questions = [int(q) for q in extra_page_data.getlist("questions")]
             if not to_questions:
                 return HttpResponse(
                     """<span class="alert alert-danger">At least one question</span>"""
                 )
+        else:
+            return HttpResponse(
+                """<span class="alert alert-danger">
+                    Unexpected radio choice: this is a bug; please file an issue!
+                </span>"""
+            )
 
         try:
             ScanCastService().assign_extra_page_from_bundle_pk_and_order(
