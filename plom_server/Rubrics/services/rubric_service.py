d8cda4dd47fc352e69c20d32e57090763a026c29
Colin B. Macdonald <cbm@m.fsf.org>
2025-01-05 12:07:27 -0800

    update copyright

--- plom_server/Rubrics/services/rubric_service.py
+++ plom_server/Rubrics/services/rubric_service.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2022-2023 Edith Coates
 # Copyright (C) 2023 Brennen Chiu
-# Copyright (C) 2019-2024 Colin B. Macdonald
+# Copyright (C) 2019-2025 Colin B. Macdonald
 # Copyright (C) 2019-2024 Andrew Rechnitzer
 # Copyright (C) 2020 Dryden Wiebe
 # Copyright (C) 2021 Nicholas J H Lai
