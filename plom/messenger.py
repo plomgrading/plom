b1c8594e1872dff1a4851b478a6155780fe2ffc7
Colin B. Macdonald <cbm@m.fsf.org>
2025-01-19 19:22:56 -0800

    copyright update

--- plom/messenger.py
+++ plom/messenger.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2018-2020 Andrew Rechnitzer
-# Copyright (C) 2019-2024 Colin B. Macdonald
+# Copyright (C) 2019-2025 Colin B. Macdonald
 # Copyright (C) 2023 Julian Lapenna
 # Copyright (C) 2024 Bryan Tanady
 
