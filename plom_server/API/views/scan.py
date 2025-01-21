db9f0b86dbc8f4acb0e0735826eaca77bf1ada06
Colin B. Macdonald <cbm@m.fsf.org>
2025-01-20 17:20:28 -0800

    fix import

--- plom_server/API/views/scan.py
+++ plom_server/API/views/scan.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2025 Colin B. Macdonald
 
-import hashlib
 from pathlib import Path
 
 from django.core.exceptions import ObjectDoesNotExist
