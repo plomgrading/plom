981669a16c9961d5b5de981b22b481e0f3092c23
Colin B. Macdonald <cbm@m.fsf.org>
2025-02-21 14:11:04 -0800

    local imports

--- plom_server/Demo/tests/test_config.py
+++ plom_server/Demo/tests/test_config.py
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2023 Edith Coates
-# Copyright (C) 2024 Colin B. Macdonald
+# Copyright (C) 2024-2025 Colin B. Macdonald
 
 from django.test import TestCase
 
-from Demo.services import (
+from ..services import (
     PlomServerConfig,
     DemoBundleConfig,
     DemoHWBundleConfig,
