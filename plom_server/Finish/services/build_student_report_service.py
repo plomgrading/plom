488ab310c376e6ef121c6209f05d5b0ae904f952
Andrew Rechnitzer <andrewr@math.ubc.ca>
2025-03-05 13:46:31 -0800

    Remove some unused code

--- plom_server/Finish/services/build_student_report_service.py
+++ plom_server/Finish/services/build_student_report_service.py
@@ -8,10 +8,8 @@ from pathlib import Path
 from statistics import mean, median, mode, stdev, quantiles
 from typing import Any
 
-from Papers.models import Paper
 from Papers.services import SpecificationService
 from ..services import StudentMarkService
-from .StudentReportPDFService import pdf_builder
 from QuestionTags.services import QuestionTagService
 
 
@@ -113,29 +111,6 @@ def brief_report_pdf_builder(
 
 class BuildStudentReportService:
     """Class that contains helper functions for building student report pdf."""
-
-    def build_one_report(self, paper_number: int) -> dict[str, Any]:
-        """Build student report for the given paper number.
-
-        Args:
-            paper_number: the paper_number to be built a report.
-
-        Returns:
-            A dictionary with student report PDF file in bytes.
-        """
-        paper = Paper.objects.get(paper_number=paper_number)
-
-        paper_info = StudentMarkService.get_paper_id_or_none(paper)
-        if paper_info:
-            sid = paper_info[0]
-        else:
-            sid = None
-
-        outdir = Path("student_report")
-        outdir.mkdir(exist_ok=True)
-
-        report = pdf_builder(versions=True, sid=sid)
-        return report
 
     def build_brief_report(
         self,
