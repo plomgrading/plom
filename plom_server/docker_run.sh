1b8f8142986baf226d6aba1570fafe330a47db89
Colin B. Macdonald <cbm@m.fsf.org>
2025-03-10 14:18:00 -0700

    Adjust docker_run.sh for script change
    Fixes #3855.

--- plom_server/docker_run.sh
+++ plom_server/docker_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # SPDX-License-Identifier: FSFAP
 # Copyright (C) 2023-2024 Edith Coates
-# Copyright (C) 2023-2024 Colin B. Macdonald
+# Copyright (C) 2023-2025 Colin B. Macdonald
 # Copyright (C) 2024 Andrew Rechnitzer
 
 # set server binding port
@@ -17,24 +17,24 @@ if ! python3 manage.py plom_database --check-for-database; then
 	echo "a database; it does not yet check anything about the filesystem."
 	echo "Use this hot start feature at your own peril."
 	if [ "x$PLOM_DEBUG" = "x0" ]; then
-		python3 Launcher/launch_scripts/launch_plom_server.py --production --port $PORT --hot-start
+		python3 plom_server/scripts/launch_plom_server.py --production --port $PORT --hot-start
 	else
-		python3 Launcher/launch_scripts/launch_plom_server.py --development --port $PORT --hot-start
+		python3 plom_server/scripts/launch_plom_server.py --development --port $PORT --hot-start
 	fi
 else
 	echo "No existing database; starting from scratch"
 	# start either a canned demo or an empty server
 	if [[ "$PLOM_DEMO" -eq 1 ]]; then
 		if [ "x$PLOM_DEBUG" = "x0" ]; then
-			python3 Launcher/launch_scripts/launch_plom_demo_server.py --production --port $PORT --stop-after bundles-pushed
+			python3 plom_server/scripts/launch_plom_demo_server.py --production --port $PORT --stop-after bundles-pushed
 		else
-			python3 Launcher/launch_scripts/launch_plom_demo_server.py --development --port $PORT --stop-after bundles-pushed
+			python3 plom_server/scripts/launch_plom_demo_server.py --development --port $PORT --stop-after bundles-pushed
 		fi
 	else
 		if [ "x$PLOM_DEBUG" = "x0" ]; then
-			python3 Launcher/launch_scripts/launch_plom_server.py --production --port $PORT
+			python3 plom_server/scripts/launch_plom_server.py --production --port $PORT
 		else
-			python3 Launcher/launch_scripts/launch_plom_server.py --development --port $PORT
+			python3 plom_server/scripts/launch_plom_server.py --development --port $PORT
 		fi
 	fi
 fi
