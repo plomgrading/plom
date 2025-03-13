d8c6c774d726bc046a7527283fb6783652b57280
Colin B. Macdonald <cbm@m.fsf.org>
2025-03-13 14:44:30 -0700

    maint: add a script to wipe and rebuild migrations

---
+++ maint/maint-wipe-rebuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# Copyright (C) 2023-2025 Colin B. Macdonald
+
+# Run this script from the project root (where pyproject.toml is)
+#   ./maint/maint-wipe-rebuild.sh
+#
+# As of 2025-03 we do not use "layered" migrations: we change the
+# database design only between major versions, and we rebuild from
+# scratch.  You can run this script after database edits, or perhaps
+# after Django-upgrades to regenerate the migration files that are
+# used to initialize the database.
+
+python plom_server/scripts/wipe_migrations.py
+
+PYTHONPATH=. python manage.py makemigrations --no-header
