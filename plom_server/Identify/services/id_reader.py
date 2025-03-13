775002e0208a29eb41f011d549d1f6a587d0a998
Andrew Rechnitzer <andrewr@math.ubc.ca>
2025-03-13 10:19:45 -0700

    Minor update to bulk add prename

--- plom_server/Identify/services/id_reader.py
+++ plom_server/Identify/services/id_reader.py
@@ -208,31 +208,26 @@ class IDReaderService:
         """Update the system for changes to papers and prenamed papers.
 
         Args:
-            user: who should new prenames be associated with?
-                TODO: it appears that existing prenames are not updated
-                to this user.  Is that intentional or a bug?  See TODO
-                in code.
-            papers: a list of Paper objects, any papers not in this will
-                not be updated.  TODO: basically lookign for a brief
-                explanation of why this would be anything other than
-                all papers in the system?
-            prenamed_papers: a list of new or updates to which papers
-                TODO: explain whether `prenamed_papers` *must* be a
-                subset of `papers` or any other assumptions between the
-                two args.
+            user: who should new prenames be associated with. Any
+                existing prenames are updated with this user.
+            papers: a list of Paper objects that have been updated
+                (elsewhere in the system - eg ID page uploaded or changed)
+                and so need their prename-predictions updated.
+            prenamed_papers: a list all prenamed papers in the system
+                (ie that the user set when the classlist was uploaded).
+                It does not have to correspond to a subset of papers.
         """
-        paper_numbers = [paper.paper_number for paper in papers]
         # find existing prename-predictions from these papers
         existing_prename_predictions = {}
         for pred in IDPrediction.objects.filter(
-            predictor="prenamed", paper__paper_number__in=paper_numbers
+            predictor="prenanmed", paper__in=papers
         ).prefetch_related("paper"):
             existing_prename_predictions[pred.paper.paper_number] = pred
         # find any existing predictions from these papers
         existing_predictions: dict[int, list[IDPrediction]] = {}  # I like cats
-        for pred in IDPrediction.objects.filter(
-            paper__paper_number__in=paper_numbers
-        ).prefetch_related("paper"):
+        for pred in IDPrediction.objects.filter(paper__in=papers).prefetch_related(
+            "paper"
+        ):
             existing_predictions.get(pred.paper.paper_number, []).append(pred)
 
         # loop over papers making two lists: things to make and things to update
@@ -243,9 +238,9 @@ class IDReaderService:
             if paper.paper_number not in prenamed_papers:
                 continue
             if paper.paper_number in existing_prename_predictions:
-                pred = existing_prename_predictions[paper.paper_numbers]
+                pred = existing_prename_predictions[paper.paper_number]
                 pred.student_id = prenamed_papers[paper.paper_number][0]
-                # TODO: pred.user = user?
+                pred.user = user  # update the associated user too.
                 predictions_to_update.append(pred)
             else:
                 # this does not immediately create a DB entry, we do it in bulk later
@@ -262,9 +257,9 @@ class IDReaderService:
         # note that the updated IDPredictions did not change certainties, so
         # they don't change the associated priorities
         priority_updates = []
-        for idt_obj in PaperIDTask.objects.filter(
-            paper__paper_number__in=paper_numbers
-        ).prefetch_related("paper"):
+        for idt_obj in PaperIDTask.objects.filter(paper__in=papers).prefetch_related(
+            "paper"
+        ):
             if idt_obj.paper.paper_number in existing_prename_predictions:
                 certs = [
                     X.certainty
@@ -277,9 +272,11 @@ class IDReaderService:
             priority_updates.append(idt_obj)
         # Finally actually create + update all the predictions and tasks
         with transaction.atomic():
-            IDPrediction.objects.bulk_update(predictions_to_update, ["student_id"])
+            IDPrediction.objects.bulk_update(
+                predictions_to_update, ["student_id", "user"]
+            )
             IDPrediction.objects.bulk_create(new_predictions)
-
+            # Some existing ID tasks will need their priorities updated too.
             PaperIDTask.objects.bulk_update(priority_updates, ["iding_priority"])
 
     def run_the_id_reader_in_foreground(
