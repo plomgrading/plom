e714208e5a5311ca6d3c9590d818122cca784809
Colin B. Macdonald <cbm@m.fsf.org>
2025-04-15 14:46:02 -0700

    adopt a 6-digit public code

--- plom/tpv_utils.py
+++ plom/tpv_utils.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2019-2020, 2022, 2024 Colin B. Macdonald
+# Copyright (C) 2019-2020, 2022, 2024-2025 Colin B. Macdonald
 # Copyright (C) 2020-2023 Andrew Rechnitzer
 # Copyright (C) 2023 Natalie Balashov
 
@@ -7,7 +7,7 @@
 
 A TPV code is a string of 17 digits of the form
 
-  TTTTTPPPVVVOCCCCC
+  TTTTTPPPVVOCCCCCC
   01234567890123456
 
 This is as much as can be packed into a QR code without
@@ -16,13 +16,13 @@ increasing its size.
 The format consists of:
   * 01234 = test number
   * 567 = page number
-  * 890 = version
-  * 1 = position/orientation code
-  * 23456 = magic code
+  * 89 = version
+  * 0 = position/orientation code
+  * 123456 = magic code
 
 A TPV code can be optionally prefixed with "QR-Code:"
 
-  QR-Code:TTTTTPPPVVVOCCCCC
+  QR-Code:TTTTTPPPVVOCCCCCC
 
 Orientation code:
   * 0 no position information (reserved, unused)
@@ -95,7 +95,7 @@ def isValidTPV(tpv: str) -> bool:
     """
     # string prefix is needed for pyzbar but not zxingcpp
     tpv = tpv.lstrip("QR-Code:")
-    if len(tpv) != len("TTTTTPPPVVVOCCCCC"):  # todo = remove in future.
+    if len(tpv) != len("TTTTTPPPVVOCCCCCC"):  # todo = remove in future.
         return False
     return tpv.isnumeric()
 
@@ -119,23 +119,23 @@ def parseTPV(tpv: str) -> tuple[int, int, int, str, str]:
     """Parse a TPV string (typically from a QR-code).
 
     Args:
-        tpv: a TPV string of the form "TTTTTPPPVVVOCCCCC",
+        tpv: a TPV string of the form "TTTTTPPPVVOCCCCCC",
             typically from a QR-code, possibly with the prefix "QR-Code:".
 
     Returns:
         tn (int): test number, up to 5 digits
         pn (int): page group number, up to 3 digits
-        vn (int): version number, up to 3 digits
-        cn (str): the "magic code", 5 digits zero padded
-        o (str): the orientation code, TODO
+        vn (int): version number, up to 2 digits
+        cn (str): the "magic code", 6 digits zero padded
+        o (str): the orientation code
     """
     # strip prefix is needed for pyzbar but not zxingcpp
     tpv = tpv.lstrip("QR-Code:")  # todo = remove in future.
     tn = int(tpv[0:5])
     pn = int(tpv[5:8])
-    vn = int(tpv[8:11])
-    o = tpv[11]
-    cn = tpv[12:]
+    vn = int(tpv[8:10])
+    o = tpv[10]
+    cn = tpv[11:]
     return tn, pn, vn, cn, o
 
 
@@ -211,22 +211,22 @@ def getPaperPageVersion(tpv: str) -> str:
 
 
 def parse_paper_page_version(ppv: str) -> tuple[int, int, int]:
-    """Parse string "TTTTTPPPVVV" into paper-number, page-number, version triple.
+    """Parse string "TTTTTPPPVV" into paper-number, page-number, version triple.
 
     Args:
-        ppv: a string of the form "TTTTTPPPVVV".
+        ppv: a string of the form "TTTTTPPPVV".
 
     Returns:
         A triple of integers,
         tn: test number, up to 5 digits,
         pn: page group number, up to 3 digits, and
-        vn: version number, up to 3 digits.
+        vn: version number, up to 2 digits.
     """
-    assert len(ppv) == len("TTTTTPPPVVV")
+    assert len(ppv) == len("TTTTTPPPVV")
     return (
         int(ppv[:5]),
         int(ppv[5:8]),
-        int(ppv[8:11]),
+        int(ppv[8:10]),
     )
 
 
@@ -248,25 +248,26 @@ def encodePaperPageVersion(paper_number: str | int, p: str | int, v: str | int) 
     Args:
         paper_number: the test number, 0 ≤ test ≤ 99999
         p: page number, 1 ≤ p ≤ 990
-        v: version number, 1 ≤ v ≤ 909
+        v: version number, 1 ≤ v ≤ 99
 
     Returns:
         A string of the short page-paper-version code
     """
     assert int(paper_number) >= 0
+    # TODO: allow zero
     assert int(v) >= 1
     assert int(p) >= 1
     paper_number = str(paper_number).zfill(5)
     p = str(p).zfill(3)
-    v = str(v).zfill(3)
+    v = str(v).zfill(2)
     assert paper_number.isnumeric()
     assert p.isnumeric()
     assert v.isnumeric()
     assert len(paper_number) == 5
     assert len(p) == 3
-    assert len(v) == 3
+    assert len(v) == 2
     tpv = f"{paper_number}{p}{v}"
-    assert len(tpv) == 11
+    assert len(tpv) == 10
     return tpv
 
 
@@ -278,22 +279,23 @@ def encodeTPV(
     Args:
         test: the test number, 0 ≤ test ≤ 99999
         p: page number, 1 ≤ p ≤ 990
-        v: version number, 1 ≤ v ≤ 909
+        v: version number, 1 ≤ v ≤ 99
         o: position code, 0 ≤ code ≤ 4
-        code: magic code, 0 ≤ code ≤ 99999
+        code: magic code, 0 ≤ code ≤ 999999
 
     Returns:
         A string of the full TPV code.
     """
     assert int(test) >= 0
+    # TODO: allow zero
     assert int(v) >= 1
     assert int(p) >= 1
     assert int(o) >= 0 and int(o) <= 4
     test = str(test).zfill(5)
     p = str(p).zfill(3)
-    v = str(v).zfill(3)
+    v = str(v).zfill(2)
     o = str(o)
-    code = str(code).zfill(5)
+    code = str(code).zfill(6)
     assert test.isnumeric()
     assert p.isnumeric()
     assert v.isnumeric()
@@ -301,9 +303,9 @@ def encodeTPV(
     assert code.isnumeric()
     assert len(test) == 5
     assert len(p) == 3
-    assert len(v) == 3
+    assert len(v) == 2
     assert len(o) == 1
-    assert len(code) == 5
+    assert len(code) == 6
     tpv = f"{test}{p}{v}{o}{code}"
     assert len(tpv) == 17
     return tpv
@@ -317,11 +319,11 @@ def new_magic_code(seed=None) -> str:
             something reasonable (i.e., current time).
 
     Returns:
-        The magic code, a 5-digit string of digits.
+        The magic code, a 6-digit string of digits.
     """
     random.seed(seed)
-    magic = str(random.randrange(0, 10**5)).zfill(5)
-    assert len(magic) == 5
+    magic = str(random.randrange(0, 10**6)).zfill(6)
+    assert len(magic) == 6
     return magic
 
 
