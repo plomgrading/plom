2f5c0843dcca2979a364a8683f40ec69e8613345
Colin B. Macdonald <cbm@m.fsf.org>
2025-04-19 00:05:40 -0700

    Renaming Demo app which isn't really demos
    Fixes #3898, or at least helps.

--- plom_server/TestingSupport/apps.py
+++ plom_server/TestingSupport/apps.py
@@ -7,4 +7,4 @@ from django.apps import AppConfig
 
 class DemoConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "plom_server.Demo"
+    name = "plom_server.TestingSupport"
