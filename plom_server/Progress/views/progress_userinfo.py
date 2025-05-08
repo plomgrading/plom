1cf3905803b2ac387546ea3a39b1571b4a9bf79d
Bryan Tanady <tanadybryan@gmail.com>
2025-05-07 19:22:01 -0700

    added missing context

--- plom_server/Progress/views/progress_userinfo.py
+++ plom_server/Progress/views/progress_userinfo.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2023-2024 Colin B. Macdonald
 # Copyright (C) 2024 Elisa Pan
 # Copyright (C) 2025 Aidan Murphy
+# Copyright (C) 2025 Bryan Tanady
 
 from django.contrib.auth.models import User
 from django.shortcuts import render
@@ -53,9 +54,10 @@ class ProgressUserInfoHome(ManagerRequiredView):
         usernames_with_quota = QuotaService.get_list_of_usernames_with_quotas()
 
         # Fetch user objects
-        users_with_quota_count = User.objects.filter(
+        users_with_quota_as_objects = User.objects.filter(
             username__in=usernames_with_quota
-        ).count()
+        )
+        users_with_quota_count = users_with_quota_as_objects.count()
 
         default_quota_limit = Quota.default_limit
 
@@ -66,6 +68,7 @@ class ProgressUserInfoHome(ManagerRequiredView):
                 "annotation_filter_form": filter_form,
                 "latest_updated_annotation_human_time": latest_annotation_human_time,
                 "default_quota_limit": default_quota_limit,
+                "users_with_quota_as_objects": users_with_quota_as_objects,
                 "users_with_quota_count": users_with_quota_count,
                 "users_progress": UserInfoServices.get_all_user_progress(),
             }
