bdd4ece7db7d1d4b5b7001b12361d278a1036fa6
Bryan Tanady <tanadybryan@gmail.com>
2025-05-13 15:29:01 -0700

    Copyright

--- plom_server/TaskOrder/services/task_ordering_service.py
+++ plom_server/TaskOrder/services/task_ordering_service.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2023 Julian Lapenna
 # Copyright (C) 2023 Edith Coates
 # Copyright (C) 2024-2025 Colin B. Macdonald
-# Copyright (C) 2024 Bryan Tanady
+# Copyright (C) 2024-2025 Bryan Tanady
 
 from plom_server.Mark.models import MarkingTask
 from plom_server.Mark.services import marking_priority
