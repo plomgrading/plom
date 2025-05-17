ce5eb0f5d7aacabd0f116e0bb7ce8ba17793951c
Colin B. Macdonald <cbm@m.fsf.org>
2025-05-16 22:08:31 -0700

    wsgi should use fully-qualified module name?
    A partial fix for Issue #3918.

--- plom_server/wsgi.py
+++ plom_server/wsgi.py
@@ -9,7 +9,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plom_server.settings")
 
 application = get_wsgi_application()
 application = WhiteNoise(application)
