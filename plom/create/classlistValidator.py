0a052c3550db82e9a964753380f495fd7aa6b8ba
Colin B. Macdonald <cbm@m.fsf.org>
2025-05-18 22:55:00 -0700

    Fix typo caught by failing test

--- plom/create/classlistValidator.py
+++ plom/create/classlistValidator.py
@@ -308,7 +308,7 @@ class PlomClasslistValidator:
 
         if len(cl_as_dicts) == 0:
             # Headers were OK, followed by no data. That's degenerate, but valid.
-            e = ("CSV file seems to be empty (headers only)",)
+            e = "CSV file seems to be empty (headers only)"
             werr.append({"warn_or_err": "warn", "werr_line": 0, "werr_text": e})
 
         # collect all errors and warnings before bailing out.
