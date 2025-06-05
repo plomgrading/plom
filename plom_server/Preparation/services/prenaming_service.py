0528563f83c2ba9ce15e3a3128e6a7bedfec9dd3
Philip Loewen <loew@math.ubc.ca>
2025-06-04 21:40:59 -0700

    Update copyright headers

--- plom_server/Preparation/services/prenaming_service.py
+++ plom_server/Preparation/services/prenaming_service.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2022-2024 Andrew Rechnitzer
 # Copyright (C) 2023-2024 Colin B. Macdonald
 # Copyright (C) 2024 Aidan Murphy
+# Copyright (C) 2025 Philip D. Loewen
 
 from django.db import transaction
 
