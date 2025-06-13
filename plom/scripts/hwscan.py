24bcb5e4543b6cde2205d3faaad979a736ff4500
Colin B. Macdonald <cbm@m.fsf.org>
2025-06-13 00:08:34 -0700

    copyright

--- plom/scripts/hwscan.py
+++ plom/scripts/hwscan.py
@@ -2,7 +2,7 @@
 
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2020 Andrew Rechnitzer
-# Copyright (C) 2020-2023 Colin B. Macdonald
+# Copyright (C) 2020-2023, 2025 Colin B. Macdonald
 # Copyright (C) 2021 Elizabeth Xiao
 
 """Plom tools for dealing with student self-submitted scans.
