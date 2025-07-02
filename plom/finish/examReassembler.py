03f3c770d2e16720191f51da1d4c13755dba5e77
Colin B. Macdonald <cbm@m.fsf.org>
2025-07-02 00:03:17 -0700

    update copyright and changelog

--- plom/finish/examReassembler.py
+++ plom/finish/examReassembler.py
@@ -1,9 +1,8 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2018-2020 Andrew Rechnitzer
-# Copyright (C) 2019-2024 Colin B. Macdonald
+# Copyright (C) 2019-2025 Colin B. Macdonald
 # Copyright (C) 2020 Dryden Wiebe
-
-from __future__ import annotations
+# Copyright (C) 2025 Aidan Murphy
 
 import tempfile
 from pathlib import Path
