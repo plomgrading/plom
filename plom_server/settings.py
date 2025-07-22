3f9b7ce969b104ad74f538693b4c04aafc7f6ce5
Philip D. Loewen <loew@math.ubc.ca>
2025-07-21 22:14:35 -0700

    Revert selection of Pacific time zone

--- plom_server/settings.py
+++ plom_server/settings.py
@@ -229,8 +229,11 @@ PASSWORD_RESET_TIMEOUT = 60 * 60 * 24 * 14
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "America/Vancouver"
+
+TIME_ZONE = "UTC"
+
 USE_I18N = True
+
 USE_TZ = True
 
 
