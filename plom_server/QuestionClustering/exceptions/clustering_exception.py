2cec8859049bea15725c160aabf153ae9bb765b8
Bryan Tanady <tanadybryan@gmail.com>
2025-07-25 16:13:46 -0700

    pyproject stuff

--- plom_server/QuestionClustering/exceptions/clustering_exception.py
+++ plom_server/QuestionClustering/exceptions/clustering_exception.py
@@ -5,6 +5,10 @@
 class ClusteringCleanupError(Exception):
     """Base exception for clustering-cleanup related operations."""
 
+    pass
+
 
 class EmptySelectedError(ClusteringCleanupError):
     """Exception when there is no selection in selecting-based operations."""
+
+    pass
