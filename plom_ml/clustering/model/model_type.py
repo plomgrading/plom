72eeac1c40ba2224258cfcb2e95c5496fc0fc134
Bryan Tanady <tanadybryan@gmail.com>
2025-07-30 11:15:41 -0700

    temp silence

--- plom_ml/clustering/model/model_type.py
+++ plom_ml/clustering/model/model_type.py
@@ -5,26 +5,26 @@ from enum import StrEnum
 # from enum import Enum
 
 
-class ClusteringType(StrEnum):
-    """Defines what type of clustering task to tackle on.
-
-    Attributes:
-        MCQ: Multiple choice question (A-F, a-f).
-        HME: Simple handwritten math expression.
-    """
-
-    MCQ = "mcq"
-    HME = "hme"
-
-
-# temporary to test CI Pipeline
-# class ClusteringModelType(Enum):
-#     """Defines what clustering models used in clustering pipeline.
+# class ClusteringType(StrEnum):
+#     """Defines what type of clustering task to tackle on.
 
 #     Attributes:
 #         MCQ: Multiple choice question (A-F, a-f).
 #         HME: Simple handwritten math expression.
 #     """
 
-#     MCQ = 1
-#     HME = 2
+#     MCQ = "mcq"
+#     HME = "hme"
+
+
+# temporary to test CI Pipeline
+class ClusteringType(Enum):
+    """Defines what clustering models used in clustering pipeline.
+
+    Attributes:
+        MCQ: Multiple choice question (A-F, a-f).
+        HME: Simple handwritten math expression.
+    """
+
+    MCQ = 1
+    HME = 2
