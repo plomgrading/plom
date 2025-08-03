45b25f90bc252b0fd220d0db51683326a787154f
Colin B. Macdonald <cbm@m.fsf.org>
2025-08-02 20:55:34 -0700

    update copyright

--- plom/finish/assemble_solutions.py
+++ plom/finish/assemble_solutions.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2021-2022 Andrew Rechnitzer
-# Copyright (C) 2022-2023 Colin B. Macdonald
+# Copyright (C) 2022-2023, 2025 Colin B. Macdonald
 
 from pathlib import Path
 import tempfile
