d5a4ee585d0f675fe7a633b9247d7f4bf05c5721
Colin B. Macdonald <cbm@m.fsf.org>
2025-08-12 12:27:13 -0700

    Document licensing and how-to do the fonts

--- plom/create/fonts/__init__.py
+++ plom/create/fonts/__init__.py
@@ -5,4 +5,14 @@
 # Copyright (C) 2025 Philip D. Loewen
 # Copyright (C) 2025 Lindsey Daniels
 
-"""Fonts based on handwriting samples of the people who wrote this module."""
+"""Fonts based on handwriting samples of the people who wrote this module.
+
+To add a new font based on your own handwriting, we're beening using the
+tool Handwrite (https://pypi.org/project/handwrite).  Its not necessary
+to commit the PDF/jpeg source, just the resulting font.
+
+As indicated at the top of this init file, the fonts are licensed under
+the LGPL-3.0-or-later by default (the license chosen by Elizabeth Xiao
+who did the first one).  If you prefer a different license, speak up
+and we'll document them each separately.
+"""
