881c7e5efa44c9351d2389a2391c9d8b4e88f661
Colin B. Macdonald <cbm@m.fsf.org>
2025-08-19 11:15:52 -0700

    Copyright

--- plom/create/test_build_source_exams.py
+++ plom/create/test_build_source_exams.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2020-2021, 2024 Colin B. Macdonald
+# Copyright (C) 2020-2021, 2024-2025 Colin B. Macdonald
 # Copyright (C) 2020 Dryden Wiebe
 
 import os
--- plom/messenger/test_messengers.py
+++ plom/messenger/test_messengers.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2021, 2023-2024 Colin B. Macdonald
+# Copyright (C) 2021, 2023-2025 Colin B. Macdonald
 
 from pytest import raises
 from plom.messenger import Messenger
