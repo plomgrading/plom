1f20f4e812458d39fe7c677398b8ae12839947af
Colin B. Macdonald <cbm@m.fsf.org>
2025-08-20 18:47:33 -0700

    Put static downloaded stuff into a local dir
    Don't store stuff in what is very likely a readonly directory.
    Fixes #2932.

--- plom_server/Launcher/management/commands/plom_get_static_javascript.py
+++ plom_server/Launcher/management/commands/plom_get_static_javascript.py
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2025 Colin B. Macdonald
 
+from pathlib import Path
+
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
@@ -8,10 +10,13 @@ from plom_server.get_js import download_javascript_and_css_to_static
 
 
 class Command(BaseCommand):
-    """Build the extra-page and scrap-paper PDFs and put them into static storage."""
+    """Download Javascript dependencies and cache them somewhere in static."""
 
     def handle(self, *args, **options):
-        """Build and store the extra-page and scrap paper pdfs."""
-        # TODO: is this the right way to get "the" static source dir?
-        destdir = settings.STATICFILES_DIRS[0]
+        """Download Javascript dependencies and cache them somewhere in static."""
+        # TODO: hardcoding here not great: the first entry is the source code itself
+        # please don't write there (Issue #2932).  2nd entry is relative to CWD, at least
+        # we should have write permission there!
+        destdir = settings.STATICFILES_DIRS[1]
+        Path(destdir).mkdir(exist_ok=True)
         download_javascript_and_css_to_static(destdir)
