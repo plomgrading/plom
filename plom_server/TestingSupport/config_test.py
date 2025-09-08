aa3e705c9d04ee743e0bb4f052220c392bb344f3
Colin B. Macdonald <cbm@m.fsf.org>
2025-09-08 16:56:28 -0700

    Remove unused ConfigTestCase
    I quite like this test config stuff but its too complicated for me to
    remember.  I'm trying to make it homogeneous by having it work the same
    way everywhere.  That means decorator-only, not the subclass route.

--- plom_server/TestingSupport/config_test.py
+++ plom_server/TestingSupport/config_test.py
@@ -9,30 +9,12 @@ from functools import wraps
 from inspect import getfile
 from pathlib import Path
 
-from django.test import TestCase
-
 from .services import (
     ConfigFileService,
     ConfigPreparationService,
     ConfigTaskService,
     PlomServerConfig,
 )
-
-
-class ConfigTestCase(TestCase):
-    """Populate the test database with models following a .toml file."""
-
-    # mypy stumbling over Traverseable?  but abc.Traversable added in Python 3.11
-    # config_file: str | Path | resources.abc.Traversable | None = None
-    config_file: str | Path | None = None
-
-    def setUp(self) -> None:
-        if self.config_file is None:
-            return
-
-        config = ConfigFileService.read_server_config(self.config_file)
-        ConfigPreparationService.create_test_preparation(config)
-        ConfigTaskService.init_all_tasks(config)
 
 
 def config_test(config_input: str | dict | None = None):
@@ -53,11 +35,6 @@ def config_test(config_input: str | dict | None = None):
     def config_test_decorator(method):
         @wraps(method)
         def wrapper_config_test(self, *args, **kwargs):
-            if hasattr(self, "config_file"):
-                raise RuntimeError(
-                    "Using a class-level config in combination with method-level configs is currently not supported."
-                )
-
             if config_input is None:
                 docstring = method.__doc__
                 synopsis, config_description = pydoc.splitdoc(docstring)
