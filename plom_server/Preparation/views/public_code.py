cb3eed322a367a1d07718a36d0f1e99263e31dca
Colin B. Macdonald <cbm@m.fsf.org>
2025-09-21 16:02:29 -0700

    Cleanup view

--- plom_server/Preparation/views/public_code.py
+++ plom_server/Preparation/views/public_code.py
@@ -8,19 +8,13 @@ from django.http import (
 )
 from django.shortcuts import render, redirect
 
-# from django_htmx.http import HttpResponseClientRedirect
-
 from plom_server.Base.base_group_views import ManagerRequiredView
 from plom_server.Papers.services import SpecificationService
 
 
 class PublicCodeView(ManagerRequiredView):
     def get(self, request: HttpRequest) -> HttpResponse:
-        # private_code = SpecificationService.get_private_seed()
-        # print(private_code)
         public_code = SpecificationService.get_public_code()
-        print(public_code)
-
         context = self.build_context()
         context.update({"public_code": public_code})
         return render(request, "Preparation/public_code.html", context)
@@ -30,10 +24,4 @@ class PublicCodeView(ManagerRequiredView):
         if not request.POST.get("confirm_check"):
             return HttpResponseBadRequest("Must confirm by clicking the checkbox")
         SpecificationService.set_public_code(public_code)
-        # HTMX?
-        # return HttpResponseClientRedirect(reverse("public_code"))
         return redirect("public_code")
-        # public_code = SpecificationService.get_public_code()
-        # context = self.build_context()
-        # context.update({"public_code": public_code})
-        # return render(request, "Preparation/public_code.html", context)
