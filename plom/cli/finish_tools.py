d1fcb5d32ce0cdb397cbfa11285f1f7ee6914035
amurph24 <aidanpmurphy@hotmail.com>
2025-10-26 18:07:26 -0700

    remove 'papernum' arg from plom-cli get-marks
    anyone using cli should know how to parse a csv (or at least save it to a file for a spreadsheet editor)

--- plom/cli/finish_tools.py
+++ plom/cli/finish_tools.py
@@ -10,29 +10,16 @@ from plom.cli import with_messenger
 
 
 @with_messenger
-def get_marks_as_csv_string(*, papernum: int | None = None, msgr) -> str:
+def get_marks_as_csv_string(*, msgr) -> str:
     """Get exam paper marks as a csv formatted string.
 
     Keyword Args:
-        papernum: the paper number to retrieve exam paper marks. If
-            unspecified, exam paper marks will be retrieved for all papers.
         msgr: a valid PlomAdminMessenger instance.
 
     Returns:
         A string containing the paper marks formatted as a csv.
     """
     papers_marks_list = msgr.new_server_get_paper_marks()
-
-    # TODO: remove this feature?
-    # if we don't want to remove it, this should be a query param for the API
-    # which can then return the actual error.
-    if papernum is not None:
-        for marks_dict in papers_marks_list:
-            if marks_dict["PaperNumber"] == papernum:
-                papers_marks_list = [marks_dict]
-                break
-        if not len(papers_marks_list) == 1:
-            raise ValueError(f"Paper {papernum} doesn't exist or hasn't been ID'd.")
 
     with StringIO() as stringbuffer:
         writer = DictWriter(stringbuffer, papers_marks_list[0].keys())
