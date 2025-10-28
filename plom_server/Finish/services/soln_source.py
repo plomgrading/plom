5fd30e7f087cd9c670845cd50d17d1056c7a7739
Colin B. Macdonald <cbm@m.fsf.org>
2025-10-27 20:43:16 -0700

    add a TODO about potentially frustrating hash check

--- plom_server/Finish/services/soln_source.py
+++ plom_server/Finish/services/soln_source.py
@@ -135,6 +135,9 @@ class SolnSourceService:
             doc_hash = hashlib.sha256(file_bytes).hexdigest()
             # check if there is an existing solution with that hash
             if SolutionSourcePDF.objects.filter(pdf_hash=doc_hash).exists():
+                # TODO: so what?  maybe my solutions are identical: this should
+                # at most be a warning.  If we're protecting the user from
+                # accidentally uploading the wrong file, we can warn them instead.
                 raise ValueError(
                     f"Another solution pdf with hash {doc_hash} has already been uploaded."
                 )
