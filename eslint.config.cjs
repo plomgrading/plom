ab3730f1c2823e6fb1cf8aac72a3d4a306aaaf58
amurph24 <aidanpmurphy@hotmail.com>
2025-11-22 18:56:41 -0800

    ignore vendored js files

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -8,6 +8,8 @@ const js = require('@eslint/js');
 const stylistic = require('@stylistic/eslint-plugin');
 
 module.exports = [
+  // ignore vendored files
+  { ignores: ['plom_server/plom_extra_static/js3rdparty/**'] },
   {
     files: ['**/*.js', '**/*.cjs', '**/*.jsx', '**/*.ts', '**/*.tsx'],
 
