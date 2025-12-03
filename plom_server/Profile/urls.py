67ea400e884be48a4bda40358f2136b615079f5f
Aidan Murphy <aidanpmurphy@hotmail.com>
2025-12-03 00:50:03 +0000

    User profile for non-self users

--- plom_server/Profile/urls.py
+++ plom_server/Profile/urls.py
@@ -2,13 +2,15 @@
 # Copyright (C) 2022 Brennen Chiu
 # Copyright (C) 2023-2024 Colin B. Macdonald
 # Copyright (C) 2024 Aden Chan
+# Copyright (C) 2025 Aidan Murphy
 
 from django.urls import path
 
-from .views import ProfileView, password_change_redirect
+from .views import ProfileView, PrivateProfileView, password_change_redirect
 
 urlpatterns = [
-    path("profile/", ProfileView.as_view(), name="profile"),
+    path("profile/<str:username>", ProfileView.as_view(), name="profile"),
+    path("profile/", PrivateProfileView.as_view(), name="private_profile"),
     path(
         "profile/password",
         password_change_redirect,
