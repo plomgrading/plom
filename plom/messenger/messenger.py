d703642023baca4d07d908ce7e5868a5d95003aa
Colin B. Macdonald <cbm@m.fsf.org>
2025-12-11 17:22:29 -0800

    messenger: continue dropping support for legacy
    Signed-off-by: Colin B. Macdonald <cbm@m.fsf.org>

--- plom/messenger/messenger.py
+++ plom/messenger/messenger.py
@@ -11,16 +11,11 @@
 from __future__ import annotations
 
 import hashlib
-import json
 import logging
-import mimetypes
-import tempfile
 from io import BytesIO
-from pathlib import Path
 from typing import Any
 
 import requests
-from requests_toolbelt import MultipartEncoder
 
 from plom.plom_exceptions import (
     PlomAuthenticationException,
@@ -173,47 +168,6 @@ class Messenger(BaseMessenger):
     # ------------------------
     # ------------------------
     # Marker stuff
-    def MrequestDoneTasks(self, q: int, v: int) -> list[list[Any]]:
-        """Information about the tasks previously marked by this year.
-
-        Args:
-            q: which question.
-            v: which version.
-
-        Returns:
-            List of info the tasks.  Each entry is a list of the task
-            string (of the form ``0002g3``), the score, the time (in
-            seconds), a list of tags (strings), and an "integrity code".
-            An empty list is returned if nothing has been graded by this
-            user.
-
-        Deprecated: only for supporting legacy servers.
-        """
-        if not self.is_legacy_server():
-            raise PlomNoServerSupportException(
-                "Only legacy servers support list of tasks"
-            )
-
-        self.SRmutex.acquire()
-        try:
-            response = self.get(
-                "/MK/tasks/complete",
-                json={"user": self.user, "token": self.token, "q": q, "v": v},
-            )
-            response.raise_for_status()
-            r = response.json()
-        except requests.HTTPError as e:
-            if response.status_code == 401:
-                raise PlomAuthenticationException() from None
-            raise PlomSeriousException(f"Some other sort of error {e}") from None
-        finally:
-            self.SRmutex.release()
-        # hack off the legacy "q" leading task code char
-        for x in r:
-            assert x[0].startswith("q")
-            x[0] = x[0][1:]
-        return r
-
     def get_tasks(
         self, qidx: int | None = None, v: int | None = None, *, username: str = ""
     ) -> list[list[Any]]:
@@ -234,11 +188,6 @@ class Messenger(BaseMessenger):
             An empty list is returned if there are no tasks.
             TODO: right now it might be a list-of-dicts.
         """
-        if self.is_legacy_server():
-            raise PlomNoServerSupportException(
-                "Legacy server does not support list of tasks"
-            )
-
         with self.SRmutex:
             try:
                 query_params = []
@@ -265,40 +214,6 @@ class Messenger(BaseMessenger):
                 x[0] = x[0][1:]
         return r
 
-    def MprogressCount_legacy(self, q, v) -> list[int]:
-        """Return info about progress on a particular question-version pair.
-
-        Args:
-            q (str/int): a question number.
-            v (str/int): a version number.
-
-        Returns:
-            A list of two integers, indicating the number of questions
-            graded and the total number of questions to be graded of
-            this question-version pair.
-
-        Raises:
-            PlomRangeException: `q` or `v` is out of range.
-            PlomAuthenticationException:
-            PlomSeriousException: something unexpected happened, such as
-                non-integer `q` or `v`.
-        """
-        with self.SRmutex:
-            try:
-                response = self.get(
-                    "/MK/progress",
-                    json={"user": self.user, "token": self.token, "q": q, "v": v},
-                )
-                # throw errors when response code != 200.
-                response.raise_for_status()
-                return response.json()
-            except requests.HTTPError as e:
-                if response.status_code == 401:
-                    raise PlomAuthenticationException(response.reason) from None
-                if response.status_code == 416:
-                    raise PlomRangeException(response.reason) from None
-                raise PlomSeriousException(f"Some other sort of error {e}") from None
-
     def get_marking_progress(self, qidx: int, ver: int) -> dict[str, Any]:
         """Get a dict representing the progress and quota status of current marker.
 
@@ -310,11 +225,6 @@ class Messenger(BaseMessenger):
             Dict with keys "total_tasks", "total_tasks_marked", "user_tasks_claimed",
             "user_tasks_marked", and "user_quota_limit"
         """
-        if self.is_legacy_server():
-            n, m = self.MprogressCount_legacy(qidx, ver)
-            d = {"total_tasks": m, "total_tasks_marked": n, "user_quota_limit": None}
-            return d
-
         with self.SRmutex:
             try:
                 response = self.get_auth(f"/MK/progress/{qidx}/{ver}")
@@ -345,18 +255,13 @@ class Messenger(BaseMessenger):
                 preference for those.  (But we will still accept tasks
                 without any of these tags).
             min_paper_num: paper number must be at least this value.
-                (On legacy servers, this is a soft preference not a hard
-                requirement).
             max_paper_num: paper number must at most this value.
-                (Ignored on legacy servers).
 
         Returns:
             Either the task string of the form "0123g5" or ``None``
             indicated no more tasks available.
 
         Raises:
-            RuntimeError: on legacy servers, can can pass at most
-                one tag.
             PlomAuthenticationException: auth-related troubles.
             PlomSeriousException: something unexpected happened.
 
@@ -364,33 +269,14 @@ class Messenger(BaseMessenger):
         """
         self.SRmutex.acquire()
         try:
-            if not self.is_legacy_server():
-                url = f"/MK/tasks/available?q={q}&v={v}"
-                if tags:
-                    url += f"&tags={','.join(tags)}"
-                if min_paper_num:
-                    url += f"&min_paper_num={min_paper_num}"
-                if max_paper_num:
-                    url += f"&max_paper_num={max_paper_num}"
-                response = self.get_auth(url)
-            else:
-                tag = None
-                if tags:
-                    if len(tags) > 1:
-                        raise RuntimeError("Legacy servers accept at most one tag")
-                    (tag,) = tags
-                response = self.get(
-                    "/MK/tasks/available",
-                    json={
-                        "user": self.user,
-                        "token": self.token,
-                        "q": q,
-                        "v": v,
-                        "above": min_paper_num,
-                        "tag": tag,
-                    },
-                )
-            # throw errors when response code != 200.
+            url = f"/MK/tasks/available?q={q}&v={v}"
+            if tags:
+                url += f"&tags={','.join(tags)}"
+            if min_paper_num:
+                url += f"&min_paper_num={min_paper_num}"
+            if max_paper_num:
+                url += f"&max_paper_num={max_paper_num}"
+            response = self.get_auth(url)
             if response.status_code == 204:
                 return None
             response.raise_for_status()
@@ -430,17 +316,7 @@ class Messenger(BaseMessenger):
             code = "q" + code
         with self.SRmutex:
             try:
-                if self.is_legacy_server():
-                    response = self.patch(
-                        f"/MK/tasks/{code}",
-                        json={
-                            "user": self.user,
-                            "token": self.token,
-                            "version": version,
-                        },
-                    )
-                else:
-                    response = self.patch_auth(f"/MK/tasks/{code}?version={version}")
+                response = self.patch_auth(f"/MK/tasks/{code}?version={version}")
                 response.raise_for_status()
                 return response.json()
             except requests.HTTPError as e:
@@ -613,150 +489,6 @@ class Messenger(BaseMessenger):
             PlomTaskChangedError
             PlomTaskDeletedError
             PlomSeriousException
-        """
-        if self.is_legacy_server():
-            n, m = self._MreturnMarkedTask_legacy(
-                "q" + code,
-                q,
-                ver,
-                score,
-                marking_time,
-                annotated_img,
-                plomfile,
-                rubrics,
-                integrity_check,
-            )
-            d = {"total_tasks": m, "total_tasks_marked": n, "user_quota_limit": None}
-            return d
-        return self._MreturnMarkedTask_webplom(
-            code, q, ver, score, marking_time, annotated_img, plomfile, integrity_check
-        )
-
-    def _MreturnMarkedTask_legacy(
-        self,
-        code,
-        pg,
-        ver,
-        score,
-        marking_time,
-        annotated_img,
-        plomfile,
-        rubrics,
-        integrity_check,
-    ):
-        # legacy expects the src_img_data duplicated outside of plomfile.
-        with open(plomfile, "rb") as f:
-            pdict = json.load(f)
-        image_md5_list = pdict["base_images"]
-
-        # put the scene in legacy format
-        for x in pdict["sceneItems"]:
-            if x[0] == "Rubric":
-                x[0] = "GroupDeltaText"
-                r = x[3]
-                log.debug("Rewriting Rubric %d as legacy GroupDeltaText", r["rid"])
-                x[3] = r["rid"]
-                x.extend(
-                    [
-                        r["kind"],
-                        r["value"],
-                        r["out_of"],
-                        r["display_delta"],
-                        r["text"],
-                        r["tags"],
-                    ]
-                )
-                assert len(x) == 10
-
-        orig_plomfile_name = plomfile.name
-        with tempfile.TemporaryDirectory() as td:
-            hack_pfile = Path(td) / plomfile.name
-            with open(hack_pfile, "w") as f:
-                json.dump(pdict, f, indent="  ")
-                f.write("\n")
-
-            img_mime_type = mimetypes.guess_type(annotated_img)[0]
-            with self.SRmutex:
-                try:
-                    with open(annotated_img, "rb") as fh, open(hack_pfile, "rb") as f2:
-                        # Note this is not passed as JSON: its key-value strings
-                        # (and note you cannot pass json= when using files=)
-                        # doesn't like ints, so convert ints to strings
-                        param = {
-                            "user": self.user,
-                            "token": self.token,
-                            "pg": str(pg),
-                            "ver": str(ver),
-                            "score": str(score),
-                            "mtime": str(round(marking_time)),
-                            "rubrics": rubrics,
-                            "md5sum": hashlib.md5(fh.read()).hexdigest(),
-                            "integrity_check": integrity_check,
-                            "image_md5s": image_md5_list,
-                        }
-                        # reset stream position to start before reading again
-                        fh.seek(0)
-                        dat = MultipartEncoder(
-                            fields={
-                                "param": json.dumps(param),
-                                "annotated": (annotated_img.name, fh, img_mime_type),
-                                "plom": (orig_plomfile_name, f2, "text/plain"),
-                            }
-                        )
-                        # increase read timeout relative to default: Issue #1575
-                        timeout = (self.default_timeout[0], 3 * self.default_timeout[1])
-                        response = self.put(
-                            f"/MK/tasks/{code}",
-                            data=dat,
-                            headers={"Content-Type": dat.content_type},
-                            timeout=timeout,
-                        )
-                    response.raise_for_status()
-                    return response.json()
-                except (requests.ConnectionError, requests.Timeout) as e:
-                    raise PlomTimeoutError(
-                        "Upload timeout/connect error: {}\n\n".format(e)
-                        + "Retries are NOT YET implemented: as a workaround,"
-                        + "you can re-open the Annotator on '{}'.\n\n".format(code)
-                        + "We will now process any remaining upload queue."
-                    ) from None
-                except requests.HTTPError as e:
-                    if response.status_code == 401:
-                        raise PlomAuthenticationException() from None
-                    if response.status_code == 406:
-                        if response.text == "integrity_fail":
-                            raise PlomConflict(
-                                "Integrity fail: can happen if manager altered task while you annotated"
-                            ) from None
-                        raise PlomSeriousException(response.text) from None
-                    if response.status_code == 409:
-                        raise PlomTaskChangedError(
-                            "Task ownership has changed."
-                        ) from None
-                    if response.status_code == 410:
-                        raise PlomTaskDeletedError(
-                            "No such task - it has been deleted from server."
-                        ) from None
-                    if response.status_code == 400:
-                        raise PlomSeriousException(response.text) from None
-                    raise PlomSeriousException(
-                        f"Some other sort of error {e}"
-                    ) from None
-
-    def _MreturnMarkedTask_webplom(
-        self,
-        code,
-        q,
-        ver,
-        score,
-        marking_time,
-        annotated_img,
-        plomfile,
-        integrity_check,
-    ):
-        """Upload annotated image and data to Django server, using it's built-in multipart data handling.
-
-        See :meth:`MreturnMarkedTask` for docs.
         """
         if self.is_server_api_less_than(115):
             assert not code.startswith("q")
