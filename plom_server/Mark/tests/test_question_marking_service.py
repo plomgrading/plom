c688ffec4280617a334c50eaa3ed7d90aacc91cf
Aidan Murphy <aidanpmurphy@hotmail.com>
2025-12-12 18:29:55 -0800

    authorship

--- plom_server/Mark/tests/test_question_marking_service.py
+++ plom_server/Mark/tests/test_question_marking_service.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2023-2024 Colin B. Macdonald
 # Copyright (C) 2023 Julian Lapenna
 # Copyright (C) 2023 Andrew Rechnitzer
+# Copyright (C) 2025 Aidan Murphy
 
 from django.test import TestCase
 from model_bakery import baker
