6873a5909409728e72bafc59b5bb75d9b76470a8
Aidan Murphy <aidanpmurphy@hotmail.com>
2026-01-19 16:40:10 -0800

    authorship

--- plom/cli/finish_tools.py
+++ plom/cli/finish_tools.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2025 Colin B. Macdonald
-# Copyright (C) 2025 Aidan Murphy
+# Copyright (C) 2025-2026 Aidan Murphy
 
 from csv import DictWriter
 from io import StringIO
--- plom_server/API/views/finish.py
+++ plom_server/API/views/finish.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2025 Colin B. Macdonald
-# Copyright (C) 2025 Aidan Murphy
+# Copyright (C) 2025-2026 Aidan Murphy
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import FileResponse
