24704c070f9b754e0632cc8b7a652a61867f2477
Colin B. Macdonald <cbm@m.fsf.org>
2026-03-31 14:22:36 -0700

    copyright update

--- plom_server/Papers/services/SpecificationService.py
+++ plom_server/Papers/services/SpecificationService.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2022-2024 Andrew Rechnitzer
 # Copyright (C) 2022-2023 Edith Coates
-# Copyright (C) 2022-2025 Colin B. Macdonald
+# Copyright (C) 2022-2026 Colin B. Macdonald
 # Copyright (C) 2022 Brennen Chiu
 # Copyright (C) 2024 Aden Chan
 # Copyright (C) 2025 Philip D. Loewen
