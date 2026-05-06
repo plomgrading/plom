3cbd330d2aaf43b9a0c7a0236af2b51e6daf9b62
jax lim <jaxjax1@students.cs.ubc.ca>
2026-05-06 13:35:13 +0800

    Added copyright line to pass CI job

--- plom/scan/fasterQRExtract.py
+++ plom/scan/fasterQRExtract.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2019-2023 Andrew Rechnitzer
 # Copyright (C) 2020-2024 Colin B. Macdonald
 # Copyright (C) 2023 Natalie Balashov
+# Copyright (C) 2026 Jax Lim
 
 from __future__ import annotations
 
