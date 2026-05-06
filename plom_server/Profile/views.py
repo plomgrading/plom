2da86cd797101175c0d19ca5c6fe3431ee794af5
Colin B. Macdonald <cbm@m.fsf.org>
2026-05-06 16:06:15 -0700

    copyright

--- plom_server/Profile/views.py
+++ plom_server/Profile/views.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # Copyright (C) 2022 Brennen Chiu
-# Copyright (C) 2023-2025 Colin B. Macdonald
+# Copyright (C) 2023-2026 Colin B. Macdonald
 # Copyright (C) 2024 Aden Chan
 # Copyright (C) 2025 Bryan Tanady
 # Copyright (C) 2025 Aidan Murphy
