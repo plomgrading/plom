dfb619faf5a0a9d39dbceb0c8b813c578186b872
Aidan Murphy <aidanpmurphy@hotmail.com>
2026-05-07 16:14:04 -0700

    organise utils

--- plom_server/static/js/frontend_utils.js
+++ plom_server/static/js/frontend_utils.js
@@ -3,21 +3,18 @@
     Copyright (C) 2023 Brennen Chiu
     Copyright (C) 2024 Aden Chan
     Copyright (C) 2024 Bryan Tanady
-    Copyright (C) 2025 Aidan Murphy
+    Copyright (C) 2025-2026 Aidan Murphy
     Copyright (C) 2025 Colin B. Macdonald
 */
 
 const firstWordList = ['adorable', 'adventurous', 'aggressive', 'agreeable', 'alert', 'alive', 'amused', 'angry', 'annoyed', 'anxious', 'attractive', 'average', 'bad', 'beautiful', 'better', 'bewildered', 'blue', 'blushing', 'bored', 'brainy', 'brave', 'breakable', 'bright', 'busy', 'calm', 'careful', 'cautious', 'charming', 'cheerful', 'clean', 'clear', 'clever', 'cloudy', 'clumsy', 'colourful', 'combative', 'comfortable', 'concerned', 'confused', 'cooperative', 'crazy', 'curious', 'cute', 'dangerous', 'delightful', 'determined', 'different', 'distinct', 'dizzy', 'eager', 'easy', 'elated', 'elegant', 'energetic', 'enthusiastic', 'excited', 'expensive', 'exuberant', 'fair', 'faithful', 'famous', 'fancy', 'fantastic', 'fine', 'friendly', 'funny', 'gentle', 'gifted', 'glamorous', 'gleaming', 'glorious', 'good', 'gorgeous', 'handsome', 'happy', 'healthy', 'helpful', 'hilarious', 'hungry', 'important', 'innocent', 'jolly', 'kind', 'light', 'lively', 'lovely', 'lucky', 'magnificent', 'misty', 'muddy', 'mushy', 'mysterious', 'naughty', 'nice', 'oldfashioned', 'outstanding', 'perfect', 'powerful', 'precious', 'real', 'relieved', 'rich', 'shiny', 'smiling', 'sparkling', 'successful', 'super', 'thoughtful', 'wandering', 'young'];
 const secondWordList = ['actor', 'actress', 'advertisement', 'airport', 'animal', 'answer', 'apple', 'balloon', 'banana', 'battery', 'bears', 'bird', 'bison', 'breakfast', 'camera', 'candle', 'car', 'cartoon', 'cat', 'chicken', 'computer', 'deer', 'dog', 'dolphin', 'eagle', 'fire', 'fish', 'food', 'ghost', 'gold', 'gorilla', 'grass', 'guitar', 'hamburger', 'helicopter', 'horse', 'ice', 'jackal', 'jelly', 'juice', 'kangaroo', 'king', 'lawyer', 'lion', 'lizard', 'llama', 'lobster', 'machine', 'magician', 'monkey', 'mosquito', 'panda', 'parrot', 'pig', 'pizza', 'planet', 'pony', 'potato', 'queen', 'rabbit', 'rainbow', 'shark', 'snake', 'tiger', 'tomato', 'train', 'truck', 'turkey', 'whale', 'wolf'];
+
+// generate a random username
+/* *********************************************************** */
+document.addEventListener('DOMContentLoaded', generateRandomUsername);
+
 let generateUsernameBtn = document.getElementById('generate-username');
-let copyBtn = document.getElementById('copy-btn');
-
-let togglePassword = document.getElementById('togglePassword');
-let newPassword1 = document.getElementById('id_new_password1');
-let newPassword2 = document.getElementById('id_new_password2');
-
-// login.html
-let showPassword = document.getElementById('check-password');
 
 function generateRandomUsername() {
   generateUsernameBtn.addEventListener('click', () => {
@@ -27,6 +24,13 @@ function generateRandomUsername() {
     document.getElementById('id_username').value = firstUserWord + secondUserWord + randomNumAsString;
   });
 }
+/* *********************************************************** */
+
+// copy password reset link
+/* *********************************************************** */
+document.addEventListener('DOMContentLoaded', copyToClipboard);
+
+let copyBtn = document.getElementById('copy-btn');
 
 function copyToClipboard() {
   const passwordResetLink = copyBtn.getAttribute('data-passwordResetLink');
@@ -34,6 +38,15 @@ function copyToClipboard() {
     navigator.clipboard.writeText(passwordResetLink);
   });
 }
+/* *********************************************************** */
+
+// toggle password visibility
+/* *********************************************************** */
+document.addEventListener('DOMContentLoaded', viewPassword);
+
+let togglePassword = document.getElementById('togglePassword');
+let newPassword1 = document.getElementById('id_new_password1');
+let newPassword2 = document.getElementById('id_new_password2');
 
 function viewPassword() {
   togglePassword.addEventListener('click', () => {
@@ -44,8 +57,14 @@ function viewPassword() {
     togglePassword.setAttribute('class', classAttribute);
   });
 }
+/* *********************************************************** */
 
-// login.html
+// toggle password visibility (again)
+/* *********************************************************** */
+document.addEventListener('DOMContentLoaded', showLoginPassword);
+
+let showPassword = document.getElementById('check-password');
+
 function showLoginPassword() {
   let loginPasswordInput = document.getElementById('typePasswordX');
   showPassword.addEventListener('click', () => {
@@ -53,8 +72,4 @@ function showLoginPassword() {
     loginPasswordInput.setAttribute('type', passwordType);
   });
 }
-
-document.addEventListener('DOMContentLoaded', generateRandomUsername);
-document.addEventListener('DOMContentLoaded', copyToClipboard);
-document.addEventListener('DOMContentLoaded', viewPassword);
-document.addEventListener('DOMContentLoaded', showLoginPassword);
+/* *********************************************************** */
