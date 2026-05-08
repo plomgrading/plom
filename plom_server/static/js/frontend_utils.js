100bd9fbbc8e5c384f68e959d4833668c350dd1b
Aidan Murphy <aidanpmurphy@hotmail.com>
2026-05-08 13:37:40 -0700

    refactor show password from checkbox to button
    also adjust javascript to be more reusable

--- plom_server/static/js/frontend_utils.js
+++ plom_server/static/js/frontend_utils.js
@@ -10,6 +10,15 @@
 const firstWordList = ['adorable', 'adventurous', 'aggressive', 'agreeable', 'alert', 'alive', 'amused', 'angry', 'annoyed', 'anxious', 'attractive', 'average', 'bad', 'beautiful', 'better', 'bewildered', 'blue', 'blushing', 'bored', 'brainy', 'brave', 'breakable', 'bright', 'busy', 'calm', 'careful', 'cautious', 'charming', 'cheerful', 'clean', 'clear', 'clever', 'cloudy', 'clumsy', 'colourful', 'combative', 'comfortable', 'concerned', 'confused', 'cooperative', 'crazy', 'curious', 'cute', 'dangerous', 'delightful', 'determined', 'different', 'distinct', 'dizzy', 'eager', 'easy', 'elated', 'elegant', 'energetic', 'enthusiastic', 'excited', 'expensive', 'exuberant', 'fair', 'faithful', 'famous', 'fancy', 'fantastic', 'fine', 'friendly', 'funny', 'gentle', 'gifted', 'glamorous', 'gleaming', 'glorious', 'good', 'gorgeous', 'handsome', 'happy', 'healthy', 'helpful', 'hilarious', 'hungry', 'important', 'innocent', 'jolly', 'kind', 'light', 'lively', 'lovely', 'lucky', 'magnificent', 'misty', 'muddy', 'mushy', 'mysterious', 'naughty', 'nice', 'oldfashioned', 'outstanding', 'perfect', 'powerful', 'precious', 'real', 'relieved', 'rich', 'shiny', 'smiling', 'sparkling', 'successful', 'super', 'thoughtful', 'wandering', 'young'];
 const secondWordList = ['actor', 'actress', 'advertisement', 'airport', 'animal', 'answer', 'apple', 'balloon', 'banana', 'battery', 'bears', 'bird', 'bison', 'breakfast', 'camera', 'candle', 'car', 'cartoon', 'cat', 'chicken', 'computer', 'deer', 'dog', 'dolphin', 'eagle', 'fire', 'fish', 'food', 'ghost', 'gold', 'gorilla', 'grass', 'guitar', 'hamburger', 'helicopter', 'horse', 'ice', 'jackal', 'jelly', 'juice', 'kangaroo', 'king', 'lawyer', 'lion', 'lizard', 'llama', 'lobster', 'machine', 'magician', 'monkey', 'mosquito', 'panda', 'parrot', 'pig', 'pizza', 'planet', 'pony', 'potato', 'queen', 'rabbit', 'rainbow', 'shark', 'snake', 'tiger', 'tomato', 'train', 'truck', 'turkey', 'whale', 'wolf'];
 
+// toggle password visibility
+/* *********************************************************** */
+window.toggleVisibility = function (passwordInputId) {
+  let passwordInput = document.getElementById(passwordInputId);
+  const newType = passwordInput.getAttribute('type') == 'password' ? 'text' : 'password';
+  passwordInput.setAttribute('type', newType);
+};
+/* *********************************************************** */
+
 // generate a random username
 /* *********************************************************** */
 document.addEventListener('DOMContentLoaded', generateRandomUsername);
@@ -38,34 +47,4 @@ function copyToClipboard() {
     navigator.clipboard.writeText(passwordResetLink);
   });
 }
-/* *********************************************************** */
-
-// toggle password visibility
-/* *********************************************************** */
-document.addEventListener('DOMContentLoaded', viewPassword);
-
-let togglePassword = document.getElementById('togglePassword');
-let newPassword1 = document.getElementById('id_new_password1');
-let newPassword2 = document.getElementById('id_new_password2');
-
-function viewPassword() {
-  togglePassword.addEventListener('click', () => {
-    const type = newPassword1.getAttribute('type') === 'password' ? 'text' : 'password';
-    const classAttribute = togglePassword.getAttribute('class') === 'bi-eye' ? 'bi-eye-slash' : 'bi-eye';
-    newPassword1.setAttribute('type', type);
-    newPassword2.setAttribute('type', type);
-    togglePassword.setAttribute('class', classAttribute);
-  });
-}
-/* *********************************************************** */
-
-// toggle password visibility (again)
-/* *********************************************************** */
-let showPasswordCheckbox = document.getElementById('showPasswordCheckbox');
-let loginPasswordInput = document.getElementById('passwordInput');
-
-showPasswordCheckbox.addEventListener('change', function () {
-  const newType = this.checked ? 'text' : 'password';
-  loginPasswordInput.setAttribute('type', newType);
-});
 /* *********************************************************** */
