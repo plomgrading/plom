0d3db24c9440594c344aa7ee1706d2611abc6caa
amurph24 <aidanpmurphy@hotmail.com>
2026-05-09 22:42:06 -0700

    docstrings for unit tests

--- plom_server/Authentication/tests/test_base_link.py
+++ plom_server/Authentication/tests/test_base_link.py
@@ -7,42 +7,50 @@ from ..services import AuthService
 
 
 class AuthService__assemble_base_link(TestCase):
+    """Test AuthService._assemble_base_link()."""
 
     def test_scheme_domain(self) -> None:
+        """Check scheme and domain inputs."""
         link = AuthService._assemble_base_link(scheme="http", domain="www.example.com")
         self.assertEqual(link, "http://www.example.com/")
 
     def test_scheme_domain_port(self) -> None:
+        """Check port inputs."""
         link = AuthService._assemble_base_link(
             scheme="http", domain="www.example.com", port="8000"
         )
         self.assertEqual(link, "http://www.example.com:8000/")
 
     def test_scheme_domain_prefix(self) -> None:
+        """Check prefix inputs."""
         link = AuthService._assemble_base_link(
             scheme="http", domain="www.example.com", prefix="bluepanda"
         )
         self.assertEqual(link, "http://www.example.com/bluepanda/")
 
     def test_scheme_domain_prefix_trailing_slash(self) -> None:
+        """Check prefix inputs with trailing slash."""
         link = AuthService._assemble_base_link(
             scheme="http", domain="www.example.com", prefix="bluepanda/"
         )
         self.assertEqual(link, "http://www.example.com/bluepanda/")
 
     def test_scheme_domain_prefix_leading_slash(self) -> None:
+        """Check prefix inputs with leading slash."""
         link = AuthService._assemble_base_link(
             scheme="http", domain="www.example.com", prefix="/bluepanda"
         )
         self.assertEqual(link, "http://www.example.com/bluepanda/")
 
     def test_scheme_domain_port_prefix(self) -> None:
+        """Check port and prefix inputs simultaneously."""
         link = AuthService._assemble_base_link(
             scheme="http", domain="www.example.com", port="8000", prefix="bluepanda"
         )
         self.assertEqual(link, "http://www.example.com:8000/bluepanda/")
 
     def test_trailing_slash(self) -> None:
+        """Check that return string always has a trailing slash."""
         link = AuthService._assemble_base_link(scheme="http", domain="www.example.com")
         self.assertEqual(link[-1], "/")
         link = AuthService._assemble_base_link(
