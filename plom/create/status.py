24750fa739548648a0ce0dea7705b0e3fdd8ed38
Colin B. Macdonald <cbm@m.fsf.org>
2026-07-09 23:39:29 -0700

    fix a CI test for moved files, copyright

--- plom/create/status.py
+++ plom/create/status.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2022-2025 Colin B. Macdonald
+# Copyright (C) 2022-2026 Colin B. Macdonald
 # Copyright (C) 2022 Natalia Accomazzo Scotti
 
 from textwrap import wrap
