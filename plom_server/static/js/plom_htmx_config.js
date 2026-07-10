90702579ef0e173d04f8a3325367ec582e5f2985
Aidan Murphy <aidanpmurphy@hotmail.com>
2026-07-10 21:28:58 +0000

    Apply 2 suggestion(s) to 1 file(s)
    Co-authored-by: Colin Macdonald <cbm@m.fsf.org>

--- plom_server/static/js/plom_htmx_config.js
+++ plom_server/static/js/plom_htmx_config.js
@@ -2,10 +2,12 @@
     SPDX-License-Identifier: AGPL-3.0-or-later
     Copyright (C) 2026 Aidan Murphy
 */
+
 /* global htmx */
 
-// response-targets changes event.detail.successful
-// to true on error responses (!!!)
+// OOTB, response-targets changes event.detail.successful
+// to true on error responses, which is surprising behavior,
+// so we adopt a more sensible default.
 // see "Configure" in the response-targets docs:
 // https://htmx.org/extensions/response-targets/
 htmx.config.responseTargetUnsetsError = false;
